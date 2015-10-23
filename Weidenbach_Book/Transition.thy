d386e6759407b9b92ee50963980dcfc7f9c7874b
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2015-10-23 13:38:36 +0200

    adding missing file

---
+++ Weidenbach_Book/Transition.thy
@@ -0,0 +1,19 @@
+theory Transition
+imports Main
+
+begin
+
+text \<open>We are interested in the stated after applying conflict and propagate\<close>
+abbreviation "no_step step S \<equiv> (\<forall>S'. \<not>step S S')"
+
+definition full :: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" ("_\<^sup>+\<^sup>\<down>") where
+"full transf = (\<lambda>S S'. tranclp transf S S' \<and> (\<forall>S''. \<not> transf S' S''))"
+
+definition full0:: "('a \<Rightarrow> 'a \<Rightarrow> bool) \<Rightarrow> 'a \<Rightarrow> 'a \<Rightarrow> bool" ("_\<^sup>\<down>") where
+"full0 transf = (\<lambda>S S'. rtranclp transf S S' \<and> (\<forall>S''. \<not> transf S' S''))"
+
+lemma full0_unfold:
+  "full0 r S S' \<longleftrightarrow> ((S = S' \<and> (\<forall>S''. \<not> r S' S'')) \<or> full r S S')"
+  unfolding full0_def full_def by (auto simp add: Nitpick.rtranclp_unfold)
+
+end
