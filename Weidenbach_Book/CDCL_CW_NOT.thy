bdc82d2bd86d7a7bbd8af78254fe7f2531d9383d
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2015-11-23 09:41:49 +0100

    more lemmas

--- Weidenbach_Book/CDCL_CW_NOT.thy
+++ Weidenbach_Book/CDCL_CW_NOT.thy
@@ -4,17 +4,17 @@ begin
 sledgehammer_params[verbose]
 
 lemma rtranclp_skip_backtrack_backtrack:
-  assumes 
+  assumes
     "skip\<^sup>*\<^sup>* S T" and
     "backtrack T W" and
     "cdcl_all_inv_mes S"
   shows "backtrack S W"
-  using assms 
+  using assms
 proof (induction)
   case base
   thus ?case by simp
 next
-  case (step T V) note st = this(1) and skip = this(2) and IH = this(3) and bt = this(4) and 
+  case (step T V) note st = this(1) and skip = this(2) and IH = this(3) and bt = this(4) and
     inv = this(5)
   obtain M N k M1 M2 K i D L U where
     V: "V = (M, N, U, k, C_Clause (D + {#L#}))" and
@@ -32,14 +32,14 @@ next
     "-L' \<notin># ?D" and
     "?D \<noteq> {#}"
     using skip unfolding V by fastforce
-  let ?M =  "Propagated L' C' # M"   
+  let ?M =  "Propagated L' C' # M"
   have inv': "cdcl_all_inv_mes T"
-    by (metis (no_types, hide_lams) cdcl_o.bj cdcl_bj.skip inv mono_rtranclp other 
+    by (metis (no_types, hide_lams) cdcl_o.bj cdcl_bj.skip inv mono_rtranclp other
       rtranclp_cdcl_all_inv_mes_inv st)
   have M_lev: "cdcl_M_level_inv T" using inv' unfolding cdcl_all_inv_mes_def by auto
   hence n_d': "no_dup ?M"
     unfolding cdcl_M_level_inv_def T by auto
-    
+
   have "k > 0"
     using decomp M_lev unfolding cdcl_M_level_inv_def T by auto
   hence "atm_of L \<in> atm_of ` lit_of `set M"
@@ -51,47 +51,93 @@ next
   have "?M \<Turnstile>as CNot ?D"
     using inv' unfolding cdcl_conflicting_def cdcl_all_inv_mes_def T by auto
   hence "L' \<notin># ?D"
-    using L_L' L'_M unfolding true_annots_def by (auto simp add: true_annot_def true_cls_def 
-      atm_of_in_atm_of_set_iff_in_set_or_uminus_in_set Ball_mset_def 
+    using L_L' L'_M unfolding true_annots_def by (auto simp add: true_annot_def true_cls_def
+      atm_of_in_atm_of_set_iff_in_set_or_uminus_in_set Ball_mset_def
       split: split_if_asm)
-    
-  obtain M2' where  
+
+  obtain M2' where
     "(Marked K (i+1) # M1, M2') \<in> set (get_all_marked_decomposition ?M)"
-    using decomp by (cases "hd (get_all_marked_decomposition M)", 
+    using decomp by (cases "hd (get_all_marked_decomposition M)",
       cases "get_all_marked_decomposition M") auto
-  moreover 
+  moreover
     from L_L'
-    have "get_level L ?M = k" 
+    have "get_level L ?M = k"
     using lev_l \<open>-L' \<notin># ?D\<close> by (auto split: split_if_asm)
-  moreover 
+  moreover
     have "atm_of L' \<notin> atms_of D"
-      using \<open>L' \<notin># ?D\<close> \<open>-L' \<notin># ?D\<close> by (simp add: atm_of_in_atm_of_set_iff_in_set_or_uminus_in_set 
+      using \<open>L' \<notin># ?D\<close> \<open>-L' \<notin># ?D\<close> by (simp add: atm_of_in_atm_of_set_iff_in_set_or_uminus_in_set
         atms_of_def)
     hence "get_level L ?M = get_maximum_level (D+{#L#}) ?M"
-      using lev_l_D L_L' by simp 
+      using lev_l_D L_L' by simp
   moreover have "i = get_maximum_level D ?M"
     using i \<open>atm_of L' \<notin> atms_of D\<close> by auto
   ultimately show ?thesis using inv
     by (metis (no_types, lifting) T Un_insert_right W backtracking i step.IH sup_bot.comm_neutral)
 qed
 
-inductive cdcl_s_sr where
-"full cdcl_cp S T \<Longrightarrow> cdcl_s_sr S T" |
-"decided S T \<Longrightarrow> full cdcl_cp T U \<Longrightarrow> cdcl_s_sr S U" |
-"no_step cdcl_cp S \<Longrightarrow> full (\<lambda>S T. skip S T \<or> resolve S T \<or> backtrack S T) S T 
-  \<Longrightarrow> full cdcl_cp T U 
-  \<Longrightarrow> cdcl_s_sr S U"
-
-lemma 
-  "cdcl_s\<^sup>*\<^sup>* S T \<Longrightarrow> no_step cdcl_s T \<Longrightarrow> cdcl_s_sr\<^sup>*\<^sup>* S T"
-  apply (induction rule: rtranclp_induct)
-  apply simp
-  apply simp
-oops
-
-interpretation cdcl_CW: dpll_state trail "\<lambda>S. clauses S \<union> learned_clauses S" 
-  "\<lambda> M (_, N, U, k, D). (M, N, U, k, D)" "\<lambda> C (M, N, U, k, D). (M, N, insert C U, k, D)" 
+interpretation cdcl_CW: dpll_state trail "\<lambda>S. clauses S \<union> learned_clauses S"
+  "\<lambda> M (_, N, U, k, D). (M, N, U, k, D)" "\<lambda> C (M, N, U, k, D). (M, N, insert C U, k, D)"
   "\<lambda> C (M, N, U, k, D). (M, N - {C}, U - {C}, k, D)"
   by unfold_locales auto
-  
+
+inductive cdcl_fw :: "'v cdcl_state \<Rightarrow> 'v cdcl_state \<Rightarrow> bool" where
+fw_propagate: "propagate S S' \<Longrightarrow> cdcl_fw S S'" |
+fw_conflict: "conflict S T \<Longrightarrow> full cdcl_bj T U \<Longrightarrow> cdcl_fw S U" |
+fw_other: "decided S S' \<Longrightarrow> cdcl_fw S S'"|
+fw_rf: "cdcl_rf S S' \<Longrightarrow> cdcl_fw S S'"
+
+lemma cdcl_fw_cdcl:
+  assumes "cdcl_fw S T"
+  shows "cdcl\<^sup>*\<^sup>* S T"
+  using assms
+proof (induction)
+  case (fw_conflict S T U) note confl = this(1) and bj = this(2)
+  have "cdcl S T" using confl by (simp add: cdcl.intros r_into_rtranclp)
+  moreover
+    have "cdcl_bj\<^sup>*\<^sup>* T U" using bj unfolding full_def by auto
+    hence "cdcl\<^sup>*\<^sup>* T U" by (metis cdcl_o.bj mono_rtranclp other)
+  ultimately show ?case by auto
+qed (simp_all add: cdcl_o.intros cdcl.intros r_into_rtranclp)
+
+lemma cdcl_bj_decomp_resolve_skip_and_bj:
+  assumes "cdcl_bj\<^sup>*\<^sup>* S T"
+  shows "((\<lambda>S T. skip S T \<or> resolve S T)\<^sup>*\<^sup>* S T
+    \<or> (\<exists>U. (\<lambda>S T. skip S T \<or> resolve S T)\<^sup>*\<^sup>* S U \<and> backtrack U T))"
+  using assms
+proof induction
+  case base
+  thus ?case by simp
+next
+  case (step T U) note st = this(1) and bj = this(2) and IH = this(3)
+  have IH: "(\<lambda>S T. skip S T \<or> resolve S T)\<^sup>*\<^sup>* S T"
+    proof -
+      { assume "(\<exists>U. (\<lambda>S T. skip S T \<or> resolve S T)\<^sup>*\<^sup>* S U \<and> backtrack U T)"
+        then obtain V where
+          "backtrack V T"
+          by blast
+        with bj have False by induction fastforce+
+      }
+      thus ?thesis using IH by blast
+    qed
+  show ?case
+    using bj
+    proof (cases rule: cdcl_bj.cases)
+      case backtrack
+      thus ?thesis using IH by blast
+    qed (metis (no_types, lifting) IH rtranclp.simps)+
+qed
+
+(* TODO prove that  full cdcl_bj is deterministic *)
+lemma full_cdcl_bj_deterministic:
+  assumes "full cdcl_bj S T" and "full cdcl_bj S U"
+  shows "T = U"
+sorry
+
+lemma
+  assumes "cdcl\<^sup>*\<^sup>* S T" and "full cdcl_bj S U"
+  shows "cdcl\<^sup>*\<^sup>* T U"
+  using assms apply (induction arbitrary: )
+    defer
+    apply simp
+oops
 end
