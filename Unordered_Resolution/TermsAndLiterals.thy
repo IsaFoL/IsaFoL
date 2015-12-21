1ae4c277751b716e74886d237156f550a472fcb6
Anders Sch <s103467@student.dtu.dk>
2015-12-21 13:04:51 +0100

    I try to obtain some bijections by proving countability

--- Unordered_Resolution/TermsAndLiterals.thy
+++ Unordered_Resolution/TermsAndLiterals.thy
@@ -47,7 +47,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 \end{verbatim}
 *}
 
-theory TermsAndLiterals imports Main begin
+theory TermsAndLiterals imports Main "~~/src/HOL/Library/Countable_Set" begin
 
 
 type_synonym var_sym  = string
@@ -72,200 +72,59 @@ datatype 't literal =
   sign: Pos (get_pred: pred_sym) (get_terms: "'t list")
 | Neg (get_pred: pred_sym) (get_terms: "'t list")
 
-(* begin Berghofer *)
-subsection {* Enumerating datatypes *}
-
-(* text {* \label{sec:enumeration} In the following section, we will show that elements of datatypes can be enumerated. This will be done by specifying functions that map natural numbers to elements of datatypes and vice versa. *}*)
-subsubsection {* Enumerating Pairs of Natural Numbers *}
-
-(*text {* \begin{figure} \begin{center} \includegraphics[scale=0.6]{diag} \end{center} \caption{Cantor's method for enumerating sets of pairs}\label{fig:diag} \end{figure} As a starting point, we show that pairs of natural numbers are enumerable. For this purpose, we use a method due to Cantor, which is illustrated in \figref{fig:diag}. The function for mapping natural numbers to pairs of natural numbers can be characterized recursively as follows: *}*)
-primrec diag :: "nat \<Rightarrow> (nat \<times> nat)"
-where
-  "diag 0 = (0, 0)"
-| "diag (Suc n) =
-     (let (x, y) = diag n
-      in case y of
-          0 \<Rightarrow> (0, Suc x)
-        | Suc y \<Rightarrow> (Suc x, y))"
-
-theorem diag_le1: "fst (diag (Suc n)) < Suc n"
-  by (induct n) (simp_all add: Let_def split_def split add: nat.split)
-
-theorem diag_le2: "snd (diag (Suc (Suc n))) < Suc (Suc n)"
-  apply (induct n)
-  apply (simp_all add: Let_def split_def split add: nat.split nat.split_asm)
-  apply (rule impI)
-  apply (case_tac n)
-  apply simp
-  apply hypsubst
-  apply (rule diag_le1)
-  done
-
-theorem diag_le3: "fst (diag n) = Suc x \<Longrightarrow> snd (diag n) < n"
-  apply (case_tac n)
-  apply simp
-  apply (case_tac nat)
-  apply (simp add: Let_def)
-  apply hypsubst
-  apply (rule diag_le2)
-  done
-
-theorem diag_le4: "fst (diag n) = Suc x \<Longrightarrow> x < n"
-  apply (case_tac n)
-  apply simp
-  apply (case_tac nat)
-  apply (simp add: Let_def)
-  apply hypsubst_thin
-  apply (drule sym)
-  apply (drule ord_eq_less_trans)
-  apply (rule diag_le1)
-  apply simp
-  done
-
-function undiag :: "nat \<times> nat \<Rightarrow> nat"
-where
-  "undiag (0, 0) = 0"
-| "undiag (0, Suc y) = Suc (undiag (y, 0))"
-| "undiag (Suc x, y) = Suc (undiag (x, Suc y))"
-  by pat_completeness auto
-
-termination
-  by (relation "measure (\<lambda>(x, y). ((x + y) * (x + y + 1)) div 2 + x)") auto
-
-theorem diag_undiag [simp]: "diag (undiag (x, y)) = (x, y)"
-  by (rule undiag.induct) (simp add: Let_def)+
-
-subsubsection {* Enumerating Trees *}
-(* text {* When writing enumeration functions for datatypes, it is useful to note that all datatypes are some kind of trees. In order to avoid re-inventing the wheel, we therefore write enumeration functions for trees once and for all. In applications, we then only have to write functions for converting between trees and concrete datatypes. *}*)
-
-datatype btree = Leaf nat | Branch btree btree
-
-function diag_btree :: "nat \<Rightarrow> btree"
-where
-  "diag_btree n = (case fst (diag n) of
-       0 \<Rightarrow> Leaf (snd (diag n))
-     | Suc x \<Rightarrow> Branch (diag_btree x) (diag_btree (snd (diag n))))"
-  by auto
-
-termination
-  by (relation "measure (\<lambda>x. x)") (auto intro: diag_le3 diag_le4)
-
-primrec undiag_btree :: "btree \<Rightarrow> nat"
-where
-  "undiag_btree (Leaf n) = undiag (0, n)"
-| "undiag_btree (Branch t1 t2) =
-     undiag (Suc (undiag_btree t1), undiag_btree t2)"
-
-theorem diag_undiag_btree [simp]: "diag_btree (undiag_btree t) = t"
-  by (induct t) (simp_all add: Let_def)
-
-declare diag_btree.simps [simp del] undiag_btree.simps [simp del]
-
-subsubsection {* Enumerating Lists *}
-
-fun list_of_btree :: "(nat \<Rightarrow> 'a) \<Rightarrow> btree \<Rightarrow> 'a list"
-where
-  "list_of_btree f (Leaf x) = []"
-| "list_of_btree f (Branch (Leaf n) t) = f n # list_of_btree f t"
-
-primrec btree_of_list :: "('a \<Rightarrow> nat) \<Rightarrow> 'a list \<Rightarrow> btree"
-where
-  "btree_of_list f [] = Leaf 0"
-| "btree_of_list f (x # xs) = Branch (Leaf (f x)) (btree_of_list f xs)"
-
-definition diag_list :: "(nat \<Rightarrow> 'a) \<Rightarrow> nat \<Rightarrow> 'a list" where
-  "diag_list f n = list_of_btree f (diag_btree n)"
-
-definition undiag_list :: "('a \<Rightarrow> nat) \<Rightarrow> 'a list \<Rightarrow> nat" where
-  "undiag_list f xs = undiag_btree (btree_of_list f xs)"
-
-theorem diag_undiag_list [simp]:
-  "(\<And>x. d (u x) = x) \<Longrightarrow> diag_list d (undiag_list u xs) = xs"
-  by (induct xs) (simp_all add: diag_list_def undiag_list_def)
+subsection {* Enumeration *}
 
 subsubsection {* Enumerating hterms *}
 
-fun term_of_btree :: "(nat \<Rightarrow> string) \<Rightarrow> btree \<Rightarrow> hterm"
-and term_list_of_btree :: "(nat \<Rightarrow> string) \<Rightarrow> btree \<Rightarrow> hterm list"
-where
-  "term_of_btree f (Leaf m) = HFun (f m) []"
-| "term_of_btree f (Branch (Leaf m) t) =
-     HFun (f m) (term_list_of_btree f t)"
-| "term_list_of_btree f (Leaf m) = []"
-| "term_list_of_btree f (Branch t1 t2) =
-     term_of_btree f t1 # term_list_of_btree f t2"
-
-primrec btree_of_term :: "(string \<Rightarrow> nat) \<Rightarrow> hterm \<Rightarrow> btree"
-    and btree_of_term_list :: "(string \<Rightarrow> nat) \<Rightarrow> hterm list \<Rightarrow> btree"
-where
-  "btree_of_term f (HFun m ts) = (if ts=[] then Leaf (f m) else Branch (Leaf (f m)) (btree_of_term_list f ts))"
-| "btree_of_term_list f [] = Leaf 0"
-| "btree_of_term_list f (t # ts) = Branch (btree_of_term f t) (btree_of_term_list f ts)"
-
-theorem term_btree: assumes du: "\<And>x. d (u x) = x"
-  shows "term_of_btree d (btree_of_term u t) = t"
-  and "term_list_of_btree d (btree_of_term_list u ts) = ts"
-  by (induct t and ts rule: btree_of_term.induct btree_of_term_list.induct) (simp_all add: du)
-
-definition diag_term :: "(nat \<Rightarrow> string) \<Rightarrow> nat \<Rightarrow> hterm" where
-  "diag_term f n = term_of_btree f (diag_btree n)"
-
-definition undiag_term :: "(string \<Rightarrow> nat) \<Rightarrow> hterm \<Rightarrow> nat" where
-  "undiag_term f t = undiag_btree (btree_of_term f t)"
-
-theorem diag_undiag_term [simp]:
-  "(\<And>x. d (u x) = x) \<Longrightarrow> diag_term d (undiag_term u t) = t"
-  by (simp add: diag_term_def undiag_term_def term_btree)
-(* end Berghofer *)
-
-subsubsection {* Enumerating chars *}
-
-definition diag_char :: "nat \<Rightarrow> char" where 
-  "diag_char == char_of_nat"
-
-definition undiag_char :: "char \<Rightarrow> nat" where 
-  "undiag_char == nat_of_char"
-
-theorem diag_undiag_char [simp]: "diag_char (undiag_char c) = c"
-  unfolding diag_char_def undiag_char_def by auto
-
-subsubsection {* Enumerating strings *}
-definition diag_string :: "nat \<Rightarrow> string" where
-  "diag_string \<equiv> diag_list diag_char"
-
-definition undiag_string :: "string \<Rightarrow> nat" where
-  "undiag_string \<equiv> undiag_list undiag_char"
-
-theorem diag_undiag_string [simp]: 
-  "diag_string (undiag_string s) = s"
-  unfolding diag_string_def undiag_string_def by auto
-
-subsubsection {* Really enumerating hterms *}
-
-definition diag_hterm :: "nat \<Rightarrow> hterm" where
-  "diag_hterm \<equiv> diag_term diag_string"
-
-definition undiag_hterm :: "hterm \<Rightarrow> nat" where
-  "undiag_hterm \<equiv> undiag_term undiag_string"
-
-theorem diag_undiag_hterm[simp]: 
-  "diag_hterm (undiag_hterm t) = t"
-  unfolding diag_hterm_def undiag_hterm_def by auto
+instantiation hterm :: countable begin
+instance by countable_datatype
+end
 
 subsubsection {* Enumerating hatoms *}
 
-definition undiag_hatom :: "hterm literal \<Rightarrow> nat" where
-  "undiag_hatom a \<equiv> undiag (undiag_string (get_pred a), undiag_list undiag_hterm (get_terms a))"
+term "to_nat (HFun '''' [])"
+term "from_nat 0 = (HFun '''' [])"
+term "from_nat::nat \<Rightarrow> hterm"
+term "from_nat::nat \<Rightarrow> string"
 
-definition diag_hatom :: "nat \<Rightarrow> hterm literal" where
-  "diag_hatom a \<equiv> 
-     (let (p,ts) = diag a in
-       (Pos (diag_string p) (diag_list diag_hterm ts))
-     )"
+instantiation literal :: (countable) countable begin
+instance by countable_datatype
+end
+
+lemma Pos_count: "countable {l :: hterm literal. sign l = True}" by auto
+
+definition undiag_hatom :: "hterm literal \<Rightarrow> nat" where
+  "undiag_hatom \<equiv> (SOME f. bij_betw f {l :: hterm literal. sign l = True} (UNIV::nat set))"
+
+lemma Pos_inf: "\<not>(finite {l :: hterm literal. sign l = True})" sorry
+
+lemma bij_undiag_hatom: "bij_betw undiag_hatom {l :: hterm literal. sign l = True} (UNIV :: nat set)"
+proof -
+ from Pos_count Pos_inf obtain f where "bij_betw f {l :: hterm literal. sign l = True} (UNIV :: nat set)" 
+   using countableE_infinite[of "{l :: hterm literal. sign l = True}"] by auto
+ then show ?thesis unfolding undiag_hatom_def using someI_ex by metis
+qed
+
+definition diag_hatom where "diag_hatom \<equiv> inv undiag_hatom"
+
+lemma "bij_betw f A B \<Longrightarrow> inj_on (inv f) A" unfolding bij_betw_def unfolding inj_on_def
+proof (rule; rule)
+
+lemma "bij_betw f A B \<Longrightarrow> bij_betw (inv f) B A"
+unfolding bij_betw_def unfolding inj_on_def apply (rule conjI)
+proof
+
+lemma bij_diag_hatom: "bij_betw diag_hatom (UNIV :: nat set) {l :: hterm literal. sign l = True}" 
+  unfolding diag_hatom_def using bij_undiag_hatom 
 
 theorem diag_undiag_hatom[simp]: 
-  "sign a = True \<Longrightarrow> diag_hatom (undiag_hatom a) = a"
-  unfolding diag_hatom_def undiag_hatom_def by auto
+  assumes "sign a = True"
+  shows "diag_hatom (undiag_hatom a) = a"
+proof -
+  have "surj
+  
+  show "diag_hatom (undiag_hatom a) = a" by auto
+qed
 
 subsubsection {* Enumerating ground terms *}
 
