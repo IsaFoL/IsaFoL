a6da57de52ce0586de5a40baf0fbc3adb3f444d8
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2016-02-11 17:00:42 +0100

    whitespace tuning; including file in ROOT

--- Weidenbach_Book/CDCL_Two_Watched_Literals_Implementation.thy
+++ Weidenbach_Book/CDCL_Two_Watched_Literals_Implementation.thy
@@ -1,10 +1,11 @@
-(*  Title: CDCL with Two Watched Literals
+(*  Title: Implementation of CDCL with Two Watched Literals
     Author: Mathias Fleury <mathias.fleury@mpi-inf.mpg.de>
 *)
+
+section \<open>Implementation for 2 Watched-Literals\<close>
 
 theory CDCL_Two_Watched_Literals_Implementation
 imports CDCL_Two_Watched_Literals
 begin
-
 
 end
