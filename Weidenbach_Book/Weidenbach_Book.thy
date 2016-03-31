f5396d6cee775d6e7f951bee98469d48e921033d
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2016-03-31 15:41:24 +0200

    adding theory import to Weidenbach_Book theory

--- Weidenbach_Book/Weidenbach_Book.thy
+++ Weidenbach_Book/Weidenbach_Book.thy
@@ -2,14 +2,14 @@ section \<open>Importing all Theories\<close>
 
 theory Weidenbach_Book
 imports
-  Prop_Normalisation
+  Prop_Normalisation Prop_Logic_Multiset
 
   Prop_Resolution
 
   Prop_Superposition
 
   CDCL_NOT DPLL_NOT DPLL_W_Implementation CDCL_W_Implementation CDCL_W_Incremental
-  CDCL_WNOT CDCL_Two_Watched_Literals
+  CDCL_WNOT CDCL_Two_Watched_Literals CDCL_Two_Watched_Literals_Invariant
 
 begin
 text \<open>This theory imports all the other theories\<close>
