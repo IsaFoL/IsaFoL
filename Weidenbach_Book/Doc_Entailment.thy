0efa72df0e0a944ea677c6779310fab2bfb46966
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2016-04-16 17:26:18 +0200

    adding files for documentation

---
+++ Weidenbach_Book/Doc_Entailment.thy
@@ -0,0 +1,8 @@
+chapter \<open>Definition of Entailment\<close>
+(*<*)
+theory Doc_Entailment
+imports Main
+begin
+(*>*)
+text \<open>This chapter defines various form of entailment.\<close>
+end
---
+++ Weidenbach_Book/Doc_Libraries.thy
@@ -0,0 +1,10 @@
+chapter \<open>More Standard Theorems\<close>
+
+(*<*)
+theory Doc_Libraries
+imports Main
+begin
+(*>*)
+
+text \<open>This chapter contains additional lemmas built on top of HOL.\<close>
+end
