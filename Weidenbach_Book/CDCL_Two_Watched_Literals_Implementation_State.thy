3429b9dd30810aa61b4fbb7bf2c98e9ef6c41ada
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2016-04-28 09:52:35 +0200

    renaming + adding lift_definition

--- Weidenbach_Book/CDCL_Two_Watched_Literals_Implementation_State.thy
+++ Weidenbach_Book/CDCL_Two_Watched_Literals_Implementation_State.thy
@@ -2,10 +2,9 @@ theory CDCL_Two_Watched_Literals_Implementation_State
 imports Partial_Annotated_Clausal_Logic "~~/src/HOL/Library/RBT"
 begin
 
-section \<open>Definition of the State\<close>
+section \<open>RBT\<close>
 
-subsection \<open>Trail\<close>
-
+subsection \<open>More RBT\<close>
 lemma "RBT.keys t = [] \<Longrightarrow> t = RBT.empty"
   using non_empty_keys by auto
 
@@ -38,66 +37,88 @@ lemma keys_nil[simp]: "RBT.keys t = [] \<longleftrightarrow> t = RBT.empty"
     defer apply auto[]
   using non_empty_keys by blast
 
-definition wf_trail_tr :: "(nat, 'a) RBT.rbt \<Rightarrow> bool" where
-"wf_trail_tr t \<longleftrightarrow> set (RBT.keys t) = {1..length (RBT.keys t)}"
 
-typedef 't trail = "{t::(nat, 't) RBT.rbt. wf_trail_tr t}"
-proof 
-  show "RBT.empty \<in> ?trail" by (auto simp: wf_trail_tr_def)
-qed
+subsection \<open>Array-like type based on RBT\<close>
 
-fun cons_trail_tr :: "'a \<Rightarrow> (nat, 'a) RBT.rbt \<Rightarrow> (nat, 'a) RBT.rbt"  where
-"cons_trail_tr L t = RBT.insert (length (RBT.keys t)+1) L t"
+definition wf_rbt_array :: "(nat, 'a) RBT.rbt \<Rightarrow> bool" where
+"wf_rbt_array t \<longleftrightarrow> set (RBT.keys t) = {1..length (RBT.keys t)}"
 
-fun tl_trail_tr :: "(nat, 'a) RBT.rbt \<Rightarrow> (nat, 'a) RBT.rbt"  where
-"tl_trail_tr t = RBT.delete (length (RBT.keys t)) t"
+fun rbt_array_append :: "'a \<Rightarrow> (nat, 'a) RBT.rbt \<Rightarrow> (nat, 'a) RBT.rbt"  where
+"rbt_array_append L t = RBT.insert (length (RBT.keys t)+1) L t"
 
-lemma wf_trail_tr_cons_trail_tr:
-  assumes wf: "wf_trail_tr t"
-  shows "wf_trail_tr (cons_trail_tr L t)"
+fun rbt_array_rem_last :: "(nat, 'a) RBT.rbt \<Rightarrow> (nat, 'a) RBT.rbt"  where
+"rbt_array_rem_last t = RBT.delete (length (RBT.keys t)) t"
+
+lemma wf_rbt_array_rbt_array_append:
+  assumes wf: "wf_rbt_array t"
+  shows "wf_rbt_array (rbt_array_append L t)"
 proof -
   have H: "set (RBT.keys t) = {1..length (RBT.keys t)}"
-    using wf by (auto simp: wf_trail_tr_def)
+    using wf by (auto simp: wf_rbt_array_def)
   have "card (set (RBT.keys t))+1 \<notin> set (RBT.keys t)"
     by (subst H, subst (1) H) auto
   then have "length (RBT.keys (RBT.insert (length (RBT.keys t) + 1) L t)) = Suc (length (RBT.keys t))"
-    by (auto simp: wf_trail_tr_def distinct_card[symmetric] keys_insert)
+    by (auto simp: wf_rbt_array_def distinct_card[symmetric] keys_insert)
   moreover 
     have "insert (Suc (length (RBT.keys t))) (set (RBT.keys t)) = {Suc 0..Suc (length (RBT.keys t))}"
       by (subst H)  auto
   ultimately show ?thesis
-    unfolding wf_trail_tr_def
-    by (simp add: keys_insert wf_trail_tr_def)
+    unfolding wf_rbt_array_def
+    by (simp add: keys_insert wf_rbt_array_def)
 qed
 
-lemma wf_trail_tl_trail_tr:
-  assumes wf: "wf_trail_tr t"
-  shows "wf_trail_tr (tl_trail_tr t)"
+lemma wf_trail_rbt_array_rem_last:
+  assumes wf: "wf_rbt_array t"
+  shows "wf_rbt_array (rbt_array_rem_last t)"
 proof -
   have H: "set (RBT.keys t) = {1..length (RBT.keys t)}"
-    using wf by (auto simp: wf_trail_tr_def)
+    using wf by (auto simp: wf_rbt_array_def)
   show ?thesis
     proof (cases "card (set (RBT.keys t)) \<ge> 1")
       case True
       then have "card (set (RBT.keys t)) \<in> set (RBT.keys t)"
         by (subst H, subst (1) H) (auto simp: distinct_card[symmetric])
       then have "length (RBT.keys (RBT.delete (length (RBT.keys t)) t)) = length (RBT.keys t) - 1"
-        by (auto simp: wf_trail_tr_def distinct_card[symmetric] keys_delete Set.remove_def)
+        by (auto simp: wf_rbt_array_def distinct_card[symmetric] keys_delete Set.remove_def)
       moreover 
         have "Set.remove (length (RBT.keys t)) (set (RBT.keys t)) =
           {Suc 0..length (RBT.keys t) - Suc 0}"
           by (subst H) auto
       ultimately show ?thesis
-       unfolding wf_trail_tr_def by (simp add: keys_delete)
+       unfolding wf_rbt_array_def by (simp add: keys_delete)
     next
       case False
       then have "t = RBT.empty"
         by (metis One_nat_def RBT.distinct_keys Suc_leI distinct_card length_greater_0_conv 
           non_empty_keys)
       then show ?thesis
-        unfolding wf_trail_tr_def by (auto simp: keys_delete) 
+        unfolding wf_rbt_array_def by (auto simp: keys_delete) 
     qed
 qed
+
+definition rel_rbt :: "('a \<Rightarrow> 'b \<Rightarrow> bool) \<Rightarrow> 'a set \<Rightarrow> 'b set \<Rightarrow> bool"
+  where "rel_rbt R = (\<lambda>A B. (\<forall>x\<in>A. \<exists>y\<in>B. R x y) \<and> (\<forall>y\<in>B. \<exists>x\<in>A. R x y))"
+
+typedef 't rbt_array = "{t::(nat, 't) RBT.rbt. wf_rbt_array t}"
+proof 
+  show "RBT.empty \<in> ?rbt_array" by (auto simp: wf_rbt_array_def)
+qed
+
+setup_lifting type_definition_rbt_array
+
+lift_definition array_rem_last :: "'t rbt_array \<Rightarrow> 't rbt_array" is "rbt_array_rem_last"
+by (rule wf_trail_rbt_array_rem_last)
+
+lift_definition array_append :: "'t \<Rightarrow> 't rbt_array \<Rightarrow> 't rbt_array" is "rbt_array_append" 
+by (rule wf_rbt_array_rbt_array_append)
+
+lift_definition array_lookup :: "'t rbt_array \<Rightarrow> nat \<Rightarrow> 't option" is "RBT.lookup" .
+
+
+section \<open>Definition of the State\<close>
+
+subsection \<open>Trail\<close>
+
 
 fun get_part_trail :: "(nat, 'a) RBT.rbt \<Rightarrow> nat \<Rightarrow> 'a list"  where
 "get_part_trail C 0 = []" |
@@ -119,22 +140,22 @@ lemma "tl (get_part_trail t (length (RBT.keys t))) =  get_part_trail t (length (
   by (cases "(t, length (RBT.keys t))" rule: get_part_trail.cases) auto
 
 lemma tl_get_part_trail:
-  assumes wf: "wf_trail_tr t"
+  assumes wf: "wf_rbt_array t"
   shows 
     "tl (get_part_trail t (length (RBT.keys t))) = 
       get_part_trail (RBT.delete (length (RBT.keys t)) t) (length (RBT.keys t) - Suc 0)"
 proof -
-  have "wf_trail_tr (tl_trail_tr t)"
-    using local.wf wf_trail_tl_trail_tr by blast
+  have "wf_rbt_array (rbt_array_rem_last t)"
+    using local.wf wf_trail_rbt_array_rem_last by blast
   then have H: "set (RBT.keys t) = {1..length (RBT.keys t)}"
-    using wf by (auto simp: wf_trail_tr_def)
+    using wf by (auto simp: wf_rbt_array_def)
   show ?thesis
     proof (cases "card (set (RBT.keys t)) \<ge> 1")
       case True
       then have "card (set (RBT.keys t)) \<in> set (RBT.keys t)"
         by (subst H, subst (1) H) (auto simp: distinct_card[symmetric])
       then have l: "length (RBT.keys t) = Suc (length (RBT.keys (RBT.delete (length (RBT.keys t)) t)))"
-        using True by (auto simp: wf_trail_tr_def distinct_card[symmetric] keys_delete Set.remove_def)
+        using True by (auto simp: wf_rbt_array_def distinct_card[symmetric] keys_delete Set.remove_def)
 
       have r: "Set.remove (length (RBT.keys t)) (set (RBT.keys t)) =
         {Suc 0..length (RBT.keys t) - Suc 0}"
@@ -146,7 +167,7 @@ proof -
         by (metis RBT.distinct_keys distinct_card eq_iff fun_upd_apply keys_delete l le_imp_less_Suc
           lookup_delete not_less)    
       show ?thesis
-        unfolding wf_trail_tr_def apply (subst (1) l) 
+        unfolding wf_rbt_array_def apply (subst (1) l) 
         by (auto simp add:distinct_card[symmetric] T keys_delete)
     next
       case False
@@ -154,18 +175,18 @@ proof -
         by (metis One_nat_def RBT.distinct_keys Suc_leI distinct_card length_greater_0_conv 
           non_empty_keys)
       then show ?thesis
-        unfolding wf_trail_tr_def by (auto simp: keys_delete) 
+        unfolding wf_rbt_array_def by (auto simp: keys_delete) 
     qed    
 qed
 
-lemma get_trail_tl_trail_tr_tl:
-  assumes wf: "wf_trail_tr t"
-  shows "get_trail (tl_trail_tr t) = tl (get_trail t)"
+lemma get_trail_rbt_array_rem_last_tl:
+  assumes wf: "wf_rbt_array t"
+  shows "get_trail (rbt_array_rem_last t) = tl (get_trail t)"
 proof -
-  have "wf_trail_tr (tl_trail_tr t)"
-    using local.wf wf_trail_tl_trail_tr by blast
+  have "wf_rbt_array (rbt_array_rem_last t)"
+    using local.wf wf_trail_rbt_array_rem_last by blast
   then have H: "set (RBT.keys t) = {1..length (RBT.keys t)}"
-    using wf by (auto simp: wf_trail_tr_def)
+    using wf by (auto simp: wf_rbt_array_def)
   show ?thesis
     proof (cases "card (set (RBT.keys t)) \<ge> 1")
       case True
@@ -173,33 +194,33 @@ proof -
         by (subst H, subst (1) H) (auto simp: distinct_card[symmetric])
       then have [simp]: "length (RBT.keys (RBT.delete (length (RBT.keys t)) t)) =
         length (RBT.keys t) - 1"
-        by (auto simp: wf_trail_tr_def distinct_card[symmetric] keys_delete Set.remove_def)
+        by (auto simp: wf_rbt_array_def distinct_card[symmetric] keys_delete Set.remove_def)
       moreover 
         have "Set.remove (length (RBT.keys t)) (set (RBT.keys t)) = 
           {Suc 0..length (RBT.keys t) - Suc 0}"
           by (subst H) auto
       ultimately show ?thesis
-       unfolding wf_trail_tr_def by (simp add: keys_delete tl_get_part_trail assms)
+       unfolding wf_rbt_array_def by (simp add: keys_delete tl_get_part_trail assms)
     next
       case False
       then have "t = RBT.empty"
         by (metis One_nat_def RBT.distinct_keys Suc_leI distinct_card length_greater_0_conv 
           non_empty_keys)
       then show ?thesis
-        unfolding wf_trail_tr_def by (auto simp: keys_delete) 
+        unfolding wf_rbt_array_def by (auto simp: keys_delete) 
     qed    
 qed
 
-lemma get_trail_cons_trail_trail_tr_cons:
-  assumes wf: "wf_trail_tr t"
-  shows "get_trail (cons_trail_tr L t) = L # get_trail t"
+lemma get_trail_rbt_array_appendail_tr_cons:
+  assumes wf: "wf_rbt_array t"
+  shows "get_trail (rbt_array_append L t) = L # get_trail t"
 proof -
   have H: "set (RBT.keys t) = {1..length (RBT.keys t)}"
-    using wf by (auto simp: wf_trail_tr_def)
+    using wf by (auto simp: wf_rbt_array_def)
   have "card (set (RBT.keys t))+1 \<notin> set (RBT.keys t)"
     by (subst H, subst (1) H) auto
   then have "length (RBT.keys (RBT.insert (length (RBT.keys t) + 1) L t)) = Suc (length (RBT.keys t))"
-    by (auto simp: wf_trail_tr_def distinct_card[symmetric] keys_insert)
+    by (auto simp: wf_rbt_array_def distinct_card[symmetric] keys_insert)
   moreover 
     have i: "insert (Suc (length (RBT.keys t))) (set (RBT.keys t)) = {Suc 0..Suc (length (RBT.keys t))}"
       by (subst H)  auto
@@ -210,8 +231,9 @@ proof -
           apply (simp add: i distinct_card)
         by simp
   ultimately show ?thesis
-    unfolding wf_trail_tr_def
-    by (simp add: keys_insert wf_trail_tr_def)
+    unfolding wf_rbt_array_def
+    by (simp add: keys_insert wf_rbt_array_def)
 qed
+
 
 end
