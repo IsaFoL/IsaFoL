6ca02311f2845d9a606c8e7d8b909659ff55eb56
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2016-11-15 07:45:02 +0100

    more abourt cached watched clauses

--- Weidenbach_Book/CDCL_Two_Watched_Literals_List_Watched.thy
+++ Weidenbach_Book/CDCL_Two_Watched_Literals_List_Watched.thy
@@ -5,14 +5,15 @@ begin
 section \<open>Third Refinement: Remembering watched\<close>
 
 subsection \<open>Types\<close>
-typ working_queue_l
 
 type_synonym working_queue_wl = "nat multiset"
+type_synonym watched = "nat multiset"
+type_synonym 'v lit_queue_wl = "'v literal multiset"
 
 type_synonym 'v twl_st_wl =
   "('v, nat) ann_lits \<times> 'v clause_l list \<times> nat \<times>
-    'v clause_l option \<times> 'v clauses \<times> 'v clauses \<times> working_queue_wl \<times> 'v lit_queue \<times>
-    ('v literal \<Rightarrow> nat multiset)"
+    'v clause_l option \<times> 'v clauses \<times> 'v clauses \<times> working_queue_wl \<times> 'v lit_queue_wl \<times>
+    ('v literal \<Rightarrow> watched)"
 
 
 fun working_queue_wl :: "'v twl_st_wl \<Rightarrow> working_queue_wl" where
@@ -24,46 +25,55 @@ fun get_trail_wl :: "'v twl_st_wl \<Rightarrow> ('v, nat) ann_lit list" where
 fun set_working_queue_wl :: "working_queue_wl \<Rightarrow> 'v twl_st_wl \<Rightarrow> 'v twl_st_wl" where
   \<open>set_working_queue_wl WS (M, N, U, D, NP, UP, _, Q) = (M, N, U, D, NP, UP, WS, Q)\<close>
 
-fun pending_wl :: "'v twl_st_l \<Rightarrow> 'v literal multiset" where
-  \<open>pending_wl (_, _, _, _, _, _, _, Q) = Q\<close>
+fun pending_wl :: "'v twl_st_wl \<Rightarrow> 'v lit_queue_wl" where
+  \<open>pending_wl (_, _, _, _, _, _, _, Q, _) = Q\<close>
 
-fun set_pending_wl :: "'v literal multiset \<Rightarrow> 'v twl_st_l \<Rightarrow> 'v twl_st_l" where
-  \<open>set_pending_wl Q (M, N, U, D, NP, UP, WS, _) = (M, N, U, D, NP, UP, WS, Q)\<close>
+fun set_pending_wl :: "'v lit_queue_wl \<Rightarrow> 'v twl_st_wl \<Rightarrow> 'v twl_st_wl" where
+  \<open>set_pending_wl Q (M, N, U, D, NP, UP, WS, _, W) = (M, N, U, D, NP, UP, WS, Q, W)\<close>
 
 fun get_conflict_wl :: "'v twl_st_wl \<Rightarrow> 'v clause_l option" where
   \<open>get_conflict_wl (_, _, _, D, _, _, _, _) = D\<close>
 
 fun correct_watching :: \<open>'v twl_st_wl \<Rightarrow> bool\<close> where
   \<open>correct_watching (M, N, U, D, NP, UP, WS, Q, W) \<longleftrightarrow>
-    (\<forall>L \<in># \<Union>#(mset `# mset N). W L = clause_to_update L (M, N, U, D, NP, UP, {#}, Q))\<close>
-  
-fun watched_by where
+    (\<forall>L \<in># \<Union>#(mset `# mset N). W L = clause_to_update L (M, N, U, D, NP, UP, {#}, {#}))\<close>
+
+fun correct_watching_except :: \<open>'v twl_st_wl \<Rightarrow> 'v literal \<Rightarrow> bool\<close> where
+  \<open>correct_watching_except (M, N, U, D, NP, UP, WS, Q, W) L \<longleftrightarrow>
+    (\<forall>L \<in> (\<Union>(set ` set N)) - {L}. W L = clause_to_update L (M, N, U, D, NP, UP, {#}, {#}))\<close>
+
+fun watched_by :: \<open>'v twl_st_wl \<Rightarrow> 'v literal \<Rightarrow> watched\<close> where
   \<open>watched_by (M, N, U, D, NP, UP, WS, Q, W) L = W L\<close>
 
-definition unit_propagation_inner_loop_body_wl :: "nat \<times> nat \<Rightarrow>
-  'v twl_st_wl \<Rightarrow> 'v twl_st_wl nres"  where
-  \<open>unit_propagation_inner_loop_body_wl C' S = do {
+fun update_watched :: \<open>'v literal \<Rightarrow> watched \<Rightarrow> 'v twl_st_wl \<Rightarrow> 'v twl_st_wl\<close> where
+  \<open>update_watched L WL (M, N, U, D, NP, UP, WS, Q, W) = (M, N, U, D, NP, UP, WS, Q, W(L:= WL))\<close>
+
+text \<open>We here also update the list of watched clauses \<^term>\<open>WL\<close>. It would be more memory efficient
+  to directly iterate over \<^term>\<open>W L\<close>, but this is not compatible with multisets.\<close>
+definition unit_propagation_inner_loop_body_wl :: "'v literal \<Rightarrow> nat \<Rightarrow> watched \<Rightarrow>
+  'v twl_st_wl \<Rightarrow> (watched \<times> 'v twl_st_wl) nres"  where
+  \<open>unit_propagation_inner_loop_body_wl L C WL S = do {
     let (M, N, U, D, NP, UP, WS, Q, W) = S;
-    let (i, C::nat) = C';
+    let i = (if (watched_l (N!C)) ! 0 = L then 0 else 1);
     let L = (watched_l (N!C)) ! i;
     let L' = (watched_l (N!C)) ! (1 - i);
     ASSERT(L' \<in># mset (watched_l (N!C)) - {#L#});
     ASSERT (mset (watched_l (N!C)) = {#L, L'#});
     val_L' \<leftarrow> valued M L';
     if val_L' = Some True
-    then RETURN S
+    then RETURN (WL, S)
     else do {
       f \<leftarrow> find_unwatched M (N!C);
       ASSERT (fst f = None \<longleftrightarrow> (\<forall>L\<in>#mset (unwatched_l (N!C)). - L \<in> lits_of_l M));
       if fst f = None
       then
         if val_L' = Some False
-        then do {RETURN (M, N, U, Some (N!C), NP, UP, {#}, {#}, W)}
-        else do {RETURN (Propagated L' C # M, N, U, D, NP, UP, WS, add_mset (-L') Q, W)}
+        then do {RETURN (add_mset C WL, (M, N, U, Some (N!C), NP, UP, {#}, {#}, W))}
+        else do {RETURN (add_mset C WL, (Propagated L' C # M, N, U, D, NP, UP, WS, add_mset (-L') Q, W))}
       else do {
         let K = (N!C) ! (snd f);
         let N' = list_update N C (swap (N!C) i (snd f));
-        RETURN (M, N', U, D, NP, UP, WS, Q, W)
+        RETURN (WL, (M, N', U, D, NP, UP, WS, Q, W))
       }
     }
    }
@@ -72,21 +82,35 @@ definition unit_propagation_inner_loop_body_wl :: "nat \<times> nat \<Rightarrow
 fun st_l_of_wl :: \<open>'v twl_st_wl  \<Rightarrow> 'v twl_st_l\<close> where
   \<open>st_l_of_wl (M, N, C, D, NP, UP, WS, Q, W) = (M, N, C, D, NP, UP, WS, Q)\<close>
 
-fun twl_st_of_l :: \<open>'v literal option \<Rightarrow> 'v twl_st_wl  \<Rightarrow> 'v twl_st\<close> where
-  \<open>twl_st_of_l L S = twl_st_of L (st_l_of_wl S)\<close>
+fun twl_st_of_wl :: \<open>'v literal option \<Rightarrow> 'v twl_st_wl  \<Rightarrow> 'v twl_st\<close> where
+  \<open>twl_st_of_wl L S = twl_st_of L (st_l_of_wl S)\<close>
+
+definition unit_propagation_inner_loop_wl :: "'v literal \<Rightarrow> 'v twl_st_wl \<Rightarrow> 'v twl_st_wl nres" where
+  \<open>unit_propagation_inner_loop_wl L S\<^sub>0 = do {
+    (WL, S) \<leftarrow> WHILE\<^sub>T\<^bsup>\<lambda>(WL, S). twl_struct_invs (twl_st_of_wl (Some L) S) \<and> twl_stgy_invs (twl_st_of_wl (Some L) S) \<and>
+    cdcl_twl_cp\<^sup>*\<^sup>* (twl_st_of_wl (Some L) S\<^sub>0) (twl_st_of_wl (Some L) S)\<^esup>
+      (\<lambda>(WL, S). working_queue_wl S \<noteq> {#})
+      (\<lambda>(WL, S). do {
+        C \<leftarrow> SPEC (\<lambda>C. C \<in># working_queue_wl S);
+        let S' = set_working_queue_wl (working_queue_wl S - {#C#}) S;
+        unit_propagation_inner_loop_body_wl L C WL S'
+      })
+      ({#}, S\<^sub>0);
+      RETURN (update_watched L WL S)
+  }
+  \<close>
 
 definition unit_propagation_outer_loop_wl :: "'v twl_st_wl \<Rightarrow> 'v twl_st_wl nres"  where
   \<open>unit_propagation_outer_loop_wl S\<^sub>0 =
-    WHILE\<^sub>T\<^bsup>\<lambda>S. twl_struct_invs (twl_st_of_l S) \<and> twl_stgy_invs (twl_st_of_l S) \<and>
+    WHILE\<^sub>T\<^bsup>\<lambda>S. twl_struct_invs (twl_st_of_wl None S) \<and> twl_stgy_invs (twl_st_of_wl None S) \<and>
       working_queue_wl S = {#}\<^esup>
       (\<lambda>S. pending_wl S \<noteq> {#})
       (\<lambda>S. do {
         L \<leftarrow> SPEC (\<lambda>L. L \<in># pending_wl S);
-        let S' = set_working_queue_wl (watched_by S L)
-           (set_pending_l (pending_wl S - {#L#}) S);
+        let S' = set_working_queue_wl (watched_by S L) (set_pending_wl (remove1_mset L (pending_wl S)) S);
         unit_propagation_inner_loop_wl L S'
       })
       S\<^sub>0
 \<close>
- 
+
 end
