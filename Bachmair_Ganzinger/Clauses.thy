74a4c30dbadd8537b96c858401bb90268834145b
Anders Sch <andschl@dtu.dk>
2016-12-16 22:04:52 +0100

    added missing file

---
+++ Bachmair_Ganzinger/Clauses.thy
@@ -0,0 +1,44 @@
+theory Clauses imports "../lib/Clausal_Logic" begin
+
+section {* Main and Side Clauses *}
+
+subsection {* Main and side clauses *} (* Should maybe be in Clausal_Logic *)
+
+type_synonym 'a main_clause = "'a clause * 'a list"
+type_synonym 'a side_clause = "'a clause * 'a multiset"
+
+definition "main_clause \<equiv> (\<lambda>(D,As). D + negs (mset As))"
+abbreviation "side_clause \<equiv> (\<lambda>(C,As). C + poss As)"
+definition "side_clauses Cs \<equiv> mset (map side_clause Cs)"
+
+(* "'b" represents "'a multiset" and "'a list" *)
+abbreviation get_C :: "'a clause * 'b \<Rightarrow> 'a clause" where 
+  "get_C \<equiv> fst"
+
+abbreviation get_As :: "'a clause * 'b \<Rightarrow> 'b" where 
+  "get_As \<equiv> snd"
+
+definition atms_of_mcls :: "'a main_clause \<Rightarrow> 'a set" where
+  "atms_of_mcls DAs = atms_of (get_C DAs) \<union> set (get_As DAs)"
+  
+definition atms_of_scls :: "'a side_clause \<Rightarrow> 'a set" where
+  "atms_of_scls CA = atms_of (get_C CA) \<union> set_mset (get_As CA)"
+  
+  
+definition "Union_Cs CAs \<equiv> \<Union># (mset (map get_C CAs))"
+
+lemma get_C_get_As_side_clauses:
+ "CA \<in> set CAs \<Longrightarrow> (get_C CA) + poss (get_As CA) \<in># side_clauses CAs"
+  unfolding side_clauses_def by auto
+
+lemma get_C_Union_Cs[simp]: "x \<in> set CAs \<Longrightarrow> get_C x \<subseteq># Union_Cs CAs"
+proof -
+  assume "x \<in> set CAs"
+  then have "get_C x \<in># (mset (map get_C CAs))" by auto
+  then show "get_C x \<subseteq># Union_Cs CAs" unfolding Union_Cs_def
+    by (simp add: sum_mset.remove)
+qed
+
+  
+
+end
