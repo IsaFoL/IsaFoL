6f2645471f5ce130e03e1a645c6a39e76e82bc3b
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2017-01-05 07:34:19 +0100

    adding theory for watched code

---
+++ Weidenbach_Book/CDCL_Two_Watched_Literals_List_Watched_Code.thy
@@ -0,0 +1,416 @@
+theory CDCL_Two_Watched_Literals_List_Watched_Code
+  imports CDCL_Two_Watched_Literals_List_Watched
+begin
+
+instance literal :: (heap) heap
+proof standard
+  obtain f :: \<open>'a \<Rightarrow> nat\<close> where f: \<open>inj f\<close>
+    by blast
+  then have Hf: \<open>f x = f s \<longleftrightarrow> x = s\<close> for s x
+    unfolding inj_on_def Ball_def comp_def by blast
+  let ?f = \<open>\<lambda>L. (is_pos L, f (atm_of L))\<close>
+  have \<open>OFCLASS(bool \<times> nat, heap_class)\<close>
+   by standard
+  then obtain g :: \<open>bool \<times> nat \<Rightarrow> nat\<close> where g: \<open>inj g\<close>
+    by blast
+  then have H: \<open>g (x, y) = g (s, t) \<longleftrightarrow> x = s \<and> y = t\<close> for s t x y
+    unfolding inj_on_def Ball_def comp_def by blast
+  have \<open>inj (g o ?f)\<close>
+    using f g unfolding inj_on_def Ball_def comp_def H Hf
+    apply (intro allI impI)
+    apply (rename_tac x y, case_tac x; case_tac y)
+    by auto
+  then show \<open>\<exists>to_nat:: 'a literal \<Rightarrow> nat. inj to_nat\<close>
+    by blast
+qed
+
+instance ann_lit :: (heap, heap) heap
+proof standard
+  let ?f = \<open>\<lambda>L:: ('a, 'b) ann_lit. (lit_of L, if is_decided L then None else Some (mark_of L))\<close>
+  have f: \<open>inj ?f\<close>
+    unfolding inj_on_def Ball_def
+    apply (intro allI impI)
+    apply (rename_tac x y, case_tac x; case_tac y)
+    by auto
+  then have Hf: \<open>?f x = ?f s \<longleftrightarrow> x = s\<close> for s x
+    unfolding inj_on_def Ball_def comp_def by blast
+  have \<open>OFCLASS('a literal \<times> 'b option, heap_class)\<close>
+   by standard
+  then obtain g :: \<open>'a literal \<times> 'b option \<Rightarrow> nat\<close> where g: \<open>inj g\<close>
+    by blast
+  then have H: \<open>g (x, y) = g (s, t) \<longleftrightarrow> x = s \<and> y = t\<close> for s t x y
+    unfolding inj_on_def Ball_def comp_def by blast
+  have \<open>inj (g o ?f)\<close>
+    using f g unfolding inj_on_def Ball_def comp_def H Hf
+    apply (intro allI impI)
+    apply (rename_tac x y, case_tac x; case_tac y)
+    by auto
+  then show \<open>\<exists>to_nat:: ('a, 'b) ann_lit \<Rightarrow> nat. inj to_nat\<close>
+    by blast
+qed
+
+section \<open>Code Generation\<close>
+
+subsection \<open>Literals as Natural Numbers\<close>
+
+text \<open>
+  Modeling \<^typ>\<open>nat literal\<close> via the transformation associating \<^term>\<open>2*n\<close> or \<^term>\<open>2*n+1\<close>
+  has some advantages over the transformation to positive or negative integers: 0 is not an issue.\<close>
+
+fun nat_of_lit :: \<open>nat literal \<Rightarrow> nat\<close> where
+  \<open>nat_of_lit (Pos L) = 2*L\<close>
+| \<open>nat_of_lit (Neg L) = 2*L + 1\<close>
+
+lemma nat_of_lit_def: \<open>nat_of_lit L = (if is_pos L then 2*atm_of L else 2*atm_of L + 1)\<close>
+  by (cases L) auto
+
+fun lit_of_nat :: \<open>nat \<Rightarrow> nat literal\<close> where
+  \<open>lit_of_nat n = (if even n then Pos (n div 2) else Neg (n div 2))\<close>
+
+lemma lit_of_nat_nat_of_lit[simp]: \<open>lit_of_nat (nat_of_lit L) = L\<close>
+  by (cases L) auto
+
+lemma nat_of_lit_lit_of_nat[simp]: \<open>nat_of_lit (lit_of_nat n) = n\<close>
+  by auto
+
+lemma atm_of_lit_of_nat: \<open>atm_of (lit_of_nat n) = n div 2\<close>
+  by auto
+
+text \<open>There is probably a more-``closed'' form from the following theorem, but it is unclear if 
+  that is useful or not.\<close>
+lemma uminus_lit_of_nat:
+  \<open>- (lit_of_nat n) = (if even n then lit_of_nat (n+1) else lit_of_nat (n-1))\<close>
+  by (auto elim!: oddE)
+
+definition lit_of_natP where
+  \<open>lit_of_natP L L' \<longleftrightarrow> lit_of_nat L = L'\<close>
+  
+definition nat_nat_lit_assn :: "nat literal \<Rightarrow> nat \<Rightarrow> assn" where
+  \<open>nat_nat_lit_assn = pure (p2rel lit_of_natP)\<close>
+
+fun pair_of_ann_lit :: "('a, 'b) ann_lit \<Rightarrow> 'a literal \<times> 'b option" where
+  \<open>pair_of_ann_lit (Propagated L D) = (L, Some D)\<close>
+| \<open>pair_of_ann_lit (Decided L) = (L, None)\<close>
+
+fun ann_lit_of_pair :: "'a literal \<times> 'b option \<Rightarrow> ('a, 'b) ann_lit" where
+  \<open>ann_lit_of_pair (L, Some D) = Propagated L D\<close>
+| \<open>ann_lit_of_pair (L, None) = Decided L\<close>
+
+lemma ann_lit_of_pair_pair_of_ann_lit: \<open>ann_lit_of_pair (pair_of_ann_lit L) = L\<close>
+  by (cases L) auto
+
+lemma pair_of_ann_lit_ann_lit_of_pair: \<open>pair_of_ann_lit (ann_lit_of_pair L) = L\<close>
+  by (cases L; cases \<open>snd L\<close>) auto
+
+definition pair_nat_ann_lit_assn :: "(nat, nat) ann_lit \<Rightarrow> (nat \<times> nat option) \<Rightarrow> assn" where
+  \<open>pair_nat_ann_lit_assn = pure ({(a, b). b = ann_lit_of_pair ((\<lambda>(a,b). (lit_of_nat a, b)) a)})\<close>
+
+
+subsection \<open>State Conversion\<close>
+
+subsubsection \<open>Refinement of the Watched Function\<close>
+
+definition watched_rel :: "('a \<Rightarrow> 'b) \<Rightarrow> nat clauses_l \<Rightarrow> ('a list \<times> (nat literal \<Rightarrow> 'b)) set" where
+  \<open>watched_rel R N = 
+    (br (\<lambda>W. (\<lambda>L. R (W!(nat_of_lit L)))) 
+        (\<lambda>W. \<forall>L \<in># lits_of_atms_of_mm (mset `# mset N). nat_of_lit L < length W))\<close>
+
+term \<open>br (\<lambda>W. (\<lambda>L. R (W!(nat_of_lit L))))\<close>
+
+text \<open>Some functions and types:\<close>
+  
+abbreviation nat_lit_assn :: "nat literal \<Rightarrow> nat literal \<Rightarrow> assn" where
+  \<open>nat_lit_assn \<equiv> (id_assn :: nat literal \<Rightarrow> _)\<close>
+
+abbreviation nat_ann_lit_assn :: "(nat, nat) ann_lit \<Rightarrow> (nat, nat) ann_lit \<Rightarrow> assn" where
+  \<open>nat_ann_lit_assn \<equiv> (id_assn :: (nat, nat) ann_lit \<Rightarrow> _)\<close>
+
+type_synonym ann_lits_l = \<open>(nat, nat) ann_lits\<close>
+type_synonym working_queue_ll = "nat list"
+type_synonym lit_queue_l = "nat literal list"
+type_synonym nat_trail = \<open>(nat \<times> nat option) array_list\<close>
+type_synonym clause_wl = \<open>nat array_list\<close>
+type_synonym clauses_wl = \<open>nat array_list array_list\<close>
+type_synonym watched_wl = \<open>nat array_list array\<close>
+
+abbreviation nat_ann_lits_assn :: "ann_lits_l \<Rightarrow> ann_lits_l \<Rightarrow> assn" where
+  \<open>nat_ann_lits_assn \<equiv> list_assn nat_ann_lit_assn\<close>
+
+abbreviation nat_lits_trail_assn :: "ann_lits_l \<Rightarrow> (nat \<times> nat option) array_list \<Rightarrow> assn" where
+  \<open>nat_lits_trail_assn \<equiv> arl_assn (pair_nat_ann_lit_assn :: (nat, nat) ann_lit \<Rightarrow> _)\<close>
+
+abbreviation clause_ll_assn :: "nat clause_l \<Rightarrow> clause_wl \<Rightarrow> assn" where
+  \<open>clause_ll_assn \<equiv> arl_assn nat_nat_lit_assn\<close>
+
+abbreviation clauses_ll_assn :: "nat clauses_l \<Rightarrow> clauses_wl \<Rightarrow> assn" where
+  \<open>clauses_ll_assn \<equiv> arl_assn clause_ll_assn\<close>
+
+abbreviation clause_l_assn :: "nat clause \<Rightarrow> nat clause_l \<Rightarrow> assn" where
+  \<open>clause_l_assn \<equiv> list_mset_assn nat_lit_assn\<close>
+
+abbreviation clauses_l_assn :: "nat clauses \<Rightarrow> nat clauses_l \<Rightarrow> assn" where
+  \<open>clauses_l_assn \<equiv> list_mset_assn clause_l_assn\<close>
+
+abbreviation working_queue_l_assn :: "nat multiset \<Rightarrow> nat list \<Rightarrow> assn" where
+  \<open>working_queue_l_assn \<equiv> list_mset_assn nat_assn\<close>
+
+abbreviation working_queue_ll_assn :: "nat list \<Rightarrow> nat list \<Rightarrow> assn" where
+  \<open>working_queue_ll_assn \<equiv> list_assn nat_assn\<close>
+
+type_synonym nat_clauses_l = \<open>nat list list\<close>
+
+type_synonym twl_st_wll =
+  "nat_trail \<times> clauses_wl \<times> nat \<times>
+      clause_wl option \<times> nat clauses_l \<times> nat clauses_l \<times> lit_queue_l \<times> watched_wl"
+
+notation prod_assn (infixr "*assn" 90)
+term \<open>array_watched_assn N\<close>
+
+locale test = 
+  fixes N :: \<open>nat literal list list\<close>
+begin
+
+definition map_fun_rel :: "(nat \<times> nat literal) set \<Rightarrow> ('b \<times> 'a) set \<Rightarrow> ('b list \<times> (nat literal \<Rightarrow> 'a)) set" where
+  \<open>map_fun_rel D R = {(m, f). \<forall>(i, j)\<in>D. i < length m \<and> (m ! i, f j) \<in> R}\<close>
+
+definition map_fun_rel_assn :: "(nat \<times> nat literal) set \<Rightarrow> ('a \<Rightarrow> 'b \<Rightarrow> assn) \<Rightarrow> (nat literal \<Rightarrow> 'a) \<Rightarrow> 'b list \<Rightarrow> assn" where
+  \<open>map_fun_rel_assn D R = pure (\<langle>the_pure R\<rangle>map_fun_rel D)\<close>
+
+lemma [safe_constraint_rules]: \<open>is_pure (map_fun_rel_assn D R)\<close>
+  unfolding map_fun_rel_assn_def by auto
+
+term array_assn
+term \<open>hr_comp is_array_list (\<langle>the_pure A\<rangle>list_rel)\<close>
+term \<open>\<lambda>Q. hr_comp Q (the_pure (map_fun_rel_assn D (arl_assn R)))\<close>
+term \<open>\<lambda>Q. hr_comp (map_fun_rel_assn D (arl_assn R)) Q\<close>
+
+term \<open>hr_comp (array_assn (arl_assn id_assn)) (\<langle>Id\<rangle>map_fun_rel D)\<close>
+  term \<open>array_assn\<close>
+term \<open>the_pure (list_assn (arl_assn R))\<close>
+term \<open>hr_comp (list_assn (hr_comp is_array_list (\<langle>the_pure A\<rangle>list_rel)))\<close>
+term \<open>\<lambda>Q. hr_comp ((hr_comp is_array (\<langle>the_pure (hr_comp is_array_list (\<langle>the_pure A\<rangle>list_rel))\<rangle>list_rel)))
+  (the_pure Q) a b\<close>
+term \<open>map_fun_rel D (the_pure (arl_assn R))\<close>
+term \<open>the_pure (array_assn G)\<close>
+definition max_index where
+\<open>max_index = MMax (nat_of_lit `# lits_of_atms_of_mm (mset `# mset N))\<close>
+
+(* abbreviation array_watched_assn :: "(nat literal \<Rightarrow> nat list) \<Rightarrow> 
+    nat array_list array \<Rightarrow> assn" where
+  \<open>array_watched_assn W \<equiv>
+      array_assn (arl_assn nat_assn) (map (W o lit_of_nat) [0..<max_index])\<close> *)
+abbreviation D where
+  \<open>D \<equiv> (\<lambda>L. (nat_of_lit L, L)) ` set_mset (lits_of_atms_of_mm (mset `# mset N))\<close>
+abbreviation array_watched_assn :: "_" where
+  \<open>array_watched_assn \<equiv>
+      hr_comp (array_assn (arl_assn nat_assn)) (\<langle>Id\<rangle>map_fun_rel D)\<close>
+
+term hashmap_assn
+definition twl_st_l_assn :: \<open>nat twl_st_wl \<Rightarrow> twl_st_wll \<Rightarrow> assn\<close> where
+\<open>twl_st_l_assn \<equiv>
+  (nat_lits_trail_assn *assn clauses_ll_assn *assn nat_assn *assn
+  option_assn clause_ll_assn *assn
+  clauses_l_assn *assn
+  clauses_l_assn *assn
+  clause_l_assn *assn
+  array_watched_assn
+  )\<close>
+
+term to_assn
+definition truc_muche :: \<open>nat twl_st_wl \<Rightarrow> nat twl_st_wl nres\<close> where
+  \<open>truc_muche S = do {RETURN S}\<close>
+
+sepref_register \<open>watched_by :: nat twl_st_wl \<Rightarrow> nat literal \<Rightarrow> watched\<close> 
+   :: \<open>nat twl_st_wl \<Rightarrow> nat literal \<Rightarrow> watched\<close>
+
+definition watched_by_nth :: \<open>nat twl_st_wl \<Rightarrow> nat literal \<Rightarrow> nat \<Rightarrow> nat\<close> where
+  \<open>watched_by_nth = (\<lambda>(M, N, U, D, NP, UP, Q, W) L i. W L ! i)\<close>
+
+definition watched_by_nth_wll :: \<open>twl_st_wll \<Rightarrow> nat \<Rightarrow> nat \<Rightarrow> _\<close> where
+  \<open>watched_by_nth_wll = (\<lambda>(M, N, U, D, NP, UP, Q, W) L i. 
+      do {
+        WL \<leftarrow> Array.nth W L;
+        j \<leftarrow> arl_get WL i;
+        return j
+      })\<close>
+
+ lemma CONSTRAINT_is_pureE:
+  assumes "CONSTRAINT is_pure A"
+  obtains R where "A=pure R"
+  using assms by (auto simp: is_pure_conv)
+term fun_rel
+term mset_rel
+term list_rel
+term list_mset_assn
+
+(* 
+lemma 
+  assumes \<open>A \<Turnstile> arl_assn (arl_assn nat_assn) (map (W \<circ> lit_of_nat) [0..<max_index]) (x1, x2)\<close>
+  shows \<open>Array.length h x1 = max_index\<close>
+proof -
+  have [iff]: \<open>(\<forall>x x'. nat_assn x x' = \<up> ((x', x) \<in> P')) \<longleftrightarrow> P' = Id\<close>
+    for P'
+    by (auto simp: pure_def)
+  have [simp]: \<open>min a b = b\<close> if \<open>b \<le> a\<close> for a b :: nat
+    using that by (auto simp: min_def)
+  show ?thesis
+    using assms
+    apply (simp add: arl_assn_def hr_comp_def (* is_array_list_def *) (* list_rel_def *)
+      Array.length_def (* snga_assn_def *) (* list_all2_iff *) comp_def (* the_pure_def *))
+    apply (auto simp add: list_rel_def list_all2_iff)
+    apply (auto simp add: arl_assn_def hr_comp_def is_array_list_def list_rel_def
+      Array.length_def (* snga_assn_def *) list_all2_iff comp_def the_pure_def)
+    apply (auto simp: snga_assn_def min_def split: if_splits)
+
+    thm arl_get_hnr_mop arl_get_hnr_aux
+    sorry
+  oops
+
+lemma
+  fixes N'
+  assumes \<open>bc < max_index\<close>
+  shows
+   \<open><twl_st_l_assn (M, N', U, D, NP, UP, Q, W)
+         ((a, b), (aa, ba), ab, ac, ad, ae, af, W') *
+        nat_nat_lit_assn bf bc>
+       Array.get W' bc
+       <\<lambda>r. arl_assn id_assn (W (lit_of_nat bc)) r>\<close> 
+   using assms
+  apply (sep_auto simp: arl_get_def is_array_list_def twl_st_l_assn_def
+      split: prod.split)
+  unfolding hoare_triple_def
+   apply (auto simp: Let_def elim!: run_elims mod_starE)
+             apply (simp add: arl_assn_def)
+  oops
+    thm runE
+*)
+term p2rel
+sepref_decl_intf i_my_watched is "nat literal \<Rightarrow> nat list"
+thm map_type_eqI[of "TYPE(nat literal literal \<Rightarrow> nat list)" "TYPE(i_my_watched)"]
+
+definition watched_app :: \<open>(nat literal \<Rightarrow> nat list) \<Rightarrow> nat literal \<Rightarrow> nat list\<close> where
+  \<open>watched_app M L \<equiv> M L\<close>
+
+sepref_decl_op watched_app: \<open>watched_app ::(nat literal \<Rightarrow> nat list) \<Rightarrow> nat literal \<Rightarrow> nat list\<close> ::
+  \<open>(Id :: ((nat literal \<Rightarrow> nat list) \<times> _) set) \<rightarrow> (Id :: (nat literal \<times> _) set) \<rightarrow> 
+     (Id :: (nat list \<times> _) set)\<close>
+  .
+
+lemma [def_pat_rules]:
+  \<open>watched_app $ M $ L \<equiv> op_watched_app $ M $ L\<close>
+  by (auto simp: watched_app_def)
+
+lemma \<open>(uncurry Array.nth, uncurry (RETURN oo watched_app)) \<in>
+   [\<lambda>(W, L). L \<in> snd ` D]\<^sub>a array_watched_assn\<^sup>k *\<^sub>a nat_nat_lit_assn\<^sup>k \<rightarrow> arl_assn id_assn\<close>
+  apply sepref_to_hoare  
+  apply (sep_auto simp: map_fun_rel_def[abs_def] nat_nat_lit_assn_def lit_of_natP_def relAPP_def)
+  apply (rule cons_rule[OF _ _ nth_rule])
+    apply (auto simp: hr_comp_def entails_def (* snga_assndef *) array_assn_def relAPP_def
+      arl_assn_def)
+    apply (auto simp: hr_comp_def[abs_def])
+    apply (subgoal_tac \<open>(aa, b) \<Turnstile> ai \<mapsto>\<^sub>a (Array.get aa ai)\<close>)
+     defer
+     apply sep_auto
+  oops
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+lemma 
+  \<open>(uncurry2 watched_by_nth_wll, uncurry2 (RETURN ooo watched_by_nth)) \<in> 
+     [\<lambda>(((M, N, U, D, NP, UP, Q, W), L), i). i < length (W L)]\<^sub>a
+      twl_st_l_assn\<^sup>d *\<^sub>a nat_nat_lit_assn\<^sup>k *\<^sub>a nat_assn\<^sup>k \<rightarrow> nat_assn\<close> 
+  unfolding watched_by_nth_wll_def watched_by_nth_def
+  unfolding watched_by_nth_def twl_st_l_assn_def
+  apply (subst watched_app_def[symmetric])
+  apply sepref_dbg_keep
+  supply [[goals_limit=1]] -- \<open>There will be many subgoals during translation, and printing them takes very long with Isabelle :(\<close>
+   apply sepref_dbg_trans_keep
+               apply sepref_dbg_trans_step_keep
+    oops
+    apply (simp only: prod.case)
+   apply sepref_dbg_trans_step_keep
+      apply (sepref_dbg_side_keep)
+    apply  ( ( elim CONSTRAINT_is_pureE;  (simp only: list_assn_pure_conv the_pure_pure)?)?;
+    sep_auto 
+      simp: pure_def hn_ctxt_def invalid_assn_def list_assn_aux_eqlen_simp 
+      intro!: hn_refineI[THEN hn_refine_preI] hfrefI 
+      elim!: mod_starE
+      intro: (* list_set_entails_aux list_set_hd_tl_aux list_set_last_butlast_aux
+             list_swap_aux list_rotate1_aux list_rev_aux *)
+    ;
+    ((rule entails_preI; sep_auto simp: list_assn_aux_eqlen_simp | (parametricity; simp; fail))?)
+  )
+  apply sepref_dbg_keep
+  supply [[goals_limit=1]] -- \<open>There will be many subgoals during translation, and printing them takes very long with Isabelle :(\<close>
+   apply sepref_dbg_trans_keep
+               apply sepref_dbg_trans_step_keep
+    apply (simp only: prod.case)
+   apply sepref_dbg_trans_step_keep
+      apply (sepref_dbg_side_keep)
+
+  
+  thm HOL_list_prepend_hnr
+  
+  
+  
+  
+ *)  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+sepref_definition watched_by_nth_impl is \<open>uncurry2 (RETURN ooo watched_by_nth)\<close>
+  :: \<open>[\<lambda>(((M, N, U, D, NP, UP, Q, W), L), i). i < length (W L)]\<^sub>a
+      twl_st_l_assn\<^sup>k *\<^sub>a nat_nat_lit_assn\<^sup>k *\<^sub>a nat_assn\<^sup>k \<rightarrow> nat_assn\<close>
+  unfolding watched_by_nth_def twl_st_l_assn_def
+  apply sepref_dbg_keep
+  supply [[goals_limit=1]] -- \<open>There will be many subgoals during translation, and printing them takes very long with Isabelle :(\<close>
+   apply sepref_dbg_trans_keep
+    apply auto[]
+   apply sepref_dbg_trans_step_keep
+      apply (sepref_dbg_side_keep)
+  
+definition watched_by_impl :: \<open>twl_st_wll \<Rightarrow> nat \<Rightarrow> _\<close> where
+  \<open>watched_by_impl S L =
+     do {
+        let (M, N, U, D, NP, UP, Q, W) = S;
+        RETURN (Array.nth W L)
+     }\<close>
+
+sepref_definition truc_muche is truc_muche :: 
+    \<open>twl_st_l_assn\<^sup>d \<rightarrow>\<^sub>a twl_st_l_assn\<close>
+  unfolding truc_muche_def
+  apply sepref_dbg_keep
+  supply [[goals_limit=1]] -- \<open>There will be many subgoals during translation, and printing them takes very long with Isabelle :(\<close>
+   apply sepref_dbg_trans_keep
+    apply auto
+   apply sepref_dbg_trans_step_keep
+      apply (sepref_dbg_side_keep)
+  
+end
