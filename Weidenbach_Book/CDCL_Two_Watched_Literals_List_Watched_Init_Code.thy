84dc6c493296057818349a16006a456400c71cb7
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2017-02-19 21:40:49 +0100

    more

--- Weidenbach_Book/CDCL_Two_Watched_Literals_List_Watched_Init_Code.thy
+++ Weidenbach_Book/CDCL_Two_Watched_Literals_List_Watched_Init_Code.thy
@@ -76,7 +76,7 @@ proof -
   moreover have \<open>(\<exists>\<^sub>Axa. arl_assn R xa r * true * \<up> (xa = x)) = (arl_assn R x r * true)\<close> for x r
     using ex_assn_up_eq by force
   ultimately show ?thesis
-    by (sepref_to_hoare) (sep_auto simp: array_of_list_def array_assn_def[symmetric])
+    by sepref_to_hoare (sep_auto simp: array_of_list_def array_assn_def[symmetric])
 qed
 
 lemma array_of_list_mset[sepref_fr_rules]:
@@ -101,5 +101,56 @@ sepref_thm init_dt_step_l
   apply (rewrite at \<open>(_, \<hole>)\<close> lms_fold_custom_empty)+
   supply [[goals_limit = 1]]
   by sepref
+
+definition extract_lits_cls :: \<open>'a clause_l \<Rightarrow> 'a literal list \<Rightarrow> 'a literal list\<close> where
+  \<open>extract_lits_cls C N\<^sub>0 = fold (\<lambda>L N\<^sub>0. if atm_of L \<in> atms_of (mset N\<^sub>0) then N\<^sub>0 else L # N\<^sub>0) C N\<^sub>0\<close>
+
+definition extract_lits_clss:: \<open>'a clauses_l \<Rightarrow> 'a literal list \<Rightarrow> 'a literal list\<close>  where
+  \<open>extract_lits_clss N N\<^sub>0 = fold extract_lits_cls N N\<^sub>0\<close>
+
+lemma lits_of_atms_of_mm_empty[simp]: \<open>lits_of_atms_of_mm {#} = {#}\<close>
+  by (auto simp: lits_of_atms_of_mm_def)
+
+lemma lits_of_atms_of_m_empty[simp]: \<open>lits_of_atms_of_m {#} = {#}\<close>
+  by (auto simp: lits_of_atms_of_m_def)
+
+lemma extract_lits_cls_Cons:
+  \<open>extract_lits_cls (L # C) N\<^sub>0 = extract_lits_cls C (if atm_of L \<in> atms_of (mset N\<^sub>0) then N\<^sub>0 else L # N\<^sub>0)\<close>
+  unfolding extract_lits_cls_def fold.simps by simp
+
+lemma extract_lits_cls_Nil[simp]:
+  \<open>extract_lits_cls [] N\<^sub>0 = N\<^sub>0\<close>
+  unfolding extract_lits_cls_def fold.simps by simp
+
+lemma extract_lits_clss_Cons[simp]:
+  \<open>extract_lits_clss (C # Cs) N = extract_lits_clss Cs (extract_lits_cls C N)\<close>
+  by (simp add: extract_lits_clss_def)
+
+lemma lits_of_atms_of_m_extract_lits_cls: \<open>set_mset (lits_of_atms_of_m (mset (extract_lits_cls C N\<^sub>0))) =
+   set_mset (lits_of_atms_of_m (mset C) + lits_of_atms_of_m (mset N\<^sub>0))\<close>
+  apply (induction C arbitrary: N\<^sub>0)
+  subgoal by simp
+  subgoal by (simp add: extract_lits_cls_Cons lits_of_atms_of_m_add_mset
+        in_lits_of_atms_of_m_ain_atms_of_iff insert_absorb)
+  done
+
+lemma is_N\<^sub>1_extract_lits_clss: \<open>twl_array_code.is_N\<^sub>1 (map nat_of_lit (extract_lits_clss N N\<^sub>0))
+  (lits_of_atms_of_mm (mset `# mset N) + lits_of_atms_of_m (mset N\<^sub>0))\<close>
+proof -
+  have is_N\<^sub>1_add: \<open>twl_array_code.is_N\<^sub>1 N\<^sub>0 (A + B) \<longleftrightarrow> set_mset A \<subseteq> set_mset (twl_array_code.N\<^sub>1 N\<^sub>0)\<close>
+    if \<open>twl_array_code.is_N\<^sub>1 N\<^sub>0 B\<close> for A B N\<^sub>0
+    using that unfolding twl_array_code.is_N\<^sub>1_def by auto
+  show ?thesis
+    apply (induction N arbitrary: N\<^sub>0)
+    subgoal by (auto simp: extract_lits_cls_def extract_lits_clss_def  twl_array_code.is_N\<^sub>1_def
+          twl_array_code.N\<^sub>1_def in_lits_of_atms_of_m_ain_atms_of_iff twl_array_code.N\<^sub>0''_def
+          twl_array_code.N\<^sub>0'_def atm_of_eq_atm_of
+          simp del: nat_of_lit.simps literal_of_nat.simps)
+    subgoal premises H for C Cs N\<^sub>0
+      using H[of \<open>extract_lits_cls C N\<^sub>0\<close>, unfolded twl_array_code.is_N\<^sub>1_def, symmetric]
+      by (simp add: lits_of_atms_of_mm_add_mset twl_array_code.is_N\<^sub>1_def
+          lits_of_atms_of_m_extract_lits_cls ac_simps)
+    done
+qed
 
 end
