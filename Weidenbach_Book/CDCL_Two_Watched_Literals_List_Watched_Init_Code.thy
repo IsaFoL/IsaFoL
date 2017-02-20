cb6ad078f0914542ee35c002bf880fbbb5380c11
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2017-02-20 19:41:13 +0100

    tuning; some proofs

--- Weidenbach_Book/CDCL_Two_Watched_Literals_List_Watched_Init_Code.thy
+++ Weidenbach_Book/CDCL_Two_Watched_Literals_List_Watched_Init_Code.thy
@@ -7,51 +7,51 @@ type_synonym 'v twl_st_wl' =
     'v cconflict \<times> 'v clauses \<times> 'v clauses \<times> 'v lit_queue_wl"
 
 type_synonym twl_st_wll' =
-  "nat_trail \<times> clauses_wl \<times> nat \<times> nat array_list option \<times>  unit_lits_wl \<times> unit_lits_wl \<times>
+  "nat_trail \<times> clauses_wl \<times> nat \<times> nat array_list option \<times> unit_lits_wl \<times> unit_lits_wl \<times>
     lit_queue_l"
 
-definition init_dt_step_wl :: \<open>_ \<Rightarrow> nat clause_l \<Rightarrow> nat twl_st_wl \<Rightarrow> (nat twl_st_wl) nres\<close> where
+definition init_dt_step_wl :: \<open>nat list \<Rightarrow> nat clause_l \<Rightarrow> nat twl_st_wl \<Rightarrow> (nat twl_st_wl) nres\<close> where
   \<open>init_dt_step_wl N\<^sub>1 C S = do {
-   (let (M, N, U, D, NP, UP, Q, WS) = S in
-   (case D of
-    None \<Rightarrow>
-    if length C = 1
-    then do {
-      ASSERT (no_dup M);
-      ASSERT (C \<noteq> []);
-      let L = hd C;
-      let val_L = valued M L;
-      if val_L = None
-      then do {RETURN (Propagated L 0 # M, N, U, None, add_mset {#L#} NP, UP, add_mset (-L) Q, WS)}
-      else
-        if val_L = Some True
-        then do {RETURN (M, N, U, None, add_mset {#L#} NP, UP, Q, WS)}
-        else do {RETURN (M, N, U, Some (mset C), add_mset {#L#} NP, UP, {#}, WS)}
-      }
-    else do {
-      ASSERT(C \<noteq> []);
-      ASSERT(tl C \<noteq> []);
-      ASSERT(hd C \<in> snd ` twl_array_code.D\<^sub>0 N\<^sub>1);
-      ASSERT(hd (tl C) \<in> snd ` twl_array_code.D\<^sub>0 N\<^sub>1);
-      let U = length N;
-      let WS = WS((hd C) := WS (hd C) @ [U]);
-      let WS = WS((hd (tl C)) := WS (hd (tl C)) @ [U]);
-      RETURN (M, N @ [op_array_of_list C], U, None, NP, UP, Q, WS)}
-  | Some D \<Rightarrow>
-    if length C = 1
-    then do {
-      ASSERT (C \<noteq> []);
-      let L = hd C;
-      RETURN (M, N, U, Some D, add_mset {#L#} NP, UP, {#}, WS)}
-    else do {
-      ASSERT(C \<noteq> []);
-      ASSERT(tl C \<noteq> []);
-      ASSERT(hd C \<in> snd ` twl_array_code.D\<^sub>0 N\<^sub>1);
-      ASSERT(hd (tl C) \<in> snd ` twl_array_code.D\<^sub>0 N\<^sub>1);
-      let U = length N;
-      let WS = WS((hd C) := WS (hd C) @ [U]);
-      let WS = WS((hd (tl C)) := WS (hd (tl C)) @ [U]);
-      RETURN (M, N @ [op_array_of_list C], U, Some D, NP, UP, {#}, WS)}))
+     let (M, N, U, D, NP, UP, Q, WS) = S in
+     case D of
+      None \<Rightarrow>
+      if length C = 1
+      then do {
+        ASSERT (no_dup M);
+        ASSERT (C \<noteq> []);
+        let L = hd C;
+        let val_L = valued M L;
+        if val_L = None
+        then do {RETURN (Propagated L 0 # M, N, U, None, add_mset {#L#} NP, UP, add_mset (-L) Q, WS)}
+        else
+          if val_L = Some True
+          then do {RETURN (M, N, U, None, add_mset {#L#} NP, UP, Q, WS)}
+          else do {RETURN (M, N, U, Some (mset C), add_mset {#L#} NP, UP, {#}, WS)}
+        }
+      else do {
+        ASSERT(C \<noteq> []);
+        ASSERT(tl C \<noteq> []);
+        ASSERT(hd C \<in> snd ` twl_array_code.D\<^sub>0 N\<^sub>1);
+        ASSERT(hd (tl C) \<in> snd ` twl_array_code.D\<^sub>0 N\<^sub>1);
+        let U = length N;
+        let WS = WS((hd C) := WS (hd C) @ [U]);
+        let WS = WS((hd (tl C)) := WS (hd (tl C)) @ [U]);
+        RETURN (M, N @ [op_array_of_list C], U, None, NP, UP, Q, WS)}
+    | Some D \<Rightarrow>
+      if length C = 1
+      then do {
+        ASSERT (C \<noteq> []);
+        let L = hd C;
+        RETURN (M, N, U, Some D, add_mset {#L#} NP, UP, {#}, WS)}
+      else do {
+        ASSERT(C \<noteq> []);
+        ASSERT(tl C \<noteq> []);
+        ASSERT(hd C \<in> snd ` twl_array_code.D\<^sub>0 N\<^sub>1);
+        ASSERT(hd (tl C) \<in> snd ` twl_array_code.D\<^sub>0 N\<^sub>1);
+        let U = length N;
+        let WS = WS(hd C := WS (hd C) @ [U]);
+        let WS = WS((hd (tl C)) := WS (hd (tl C)) @ [U]);
+        RETURN (M, N @ [op_array_of_list C], U, Some D, NP, UP, {#}, WS)}
   }\<close>
 
 definition array_of_list :: "'a::heap list \<Rightarrow> 'a array_list Heap" where
@@ -138,6 +138,79 @@ proof -
       by (simp add: lits_of_atms_of_mm_add_mset twl_array_code.is_N\<^sub>1_def
           lits_of_atms_of_m_extract_lits_cls ac_simps)
     done
+qed
+
+fun correct_watching_init :: \<open>nat literal multiset \<Rightarrow> nat twl_st_wl \<Rightarrow> bool\<close> where
+  \<open>correct_watching_init N\<^sub>1 (M, N, U, D, NP, UP, Q, W) \<longleftrightarrow>
+    (\<forall>L \<in># lits_of_atms_of_m N\<^sub>1. mset (W L) = clause_to_update L (M, N, U, D, NP, UP, {#}, {#}))\<close>
+
+definition HH :: \<open>nat literal multiset \<Rightarrow> (nat twl_st_wl \<times> nat twl_st_l) set\<close> where
+  \<open>HH N\<^sub>1 = {((M', N', U', D', NP', UP', Q', WS'), (M, N, U, D, NP, UP, WS, Q)).
+               M = M' \<and> N = N' \<and> U = U' \<and> D = D' \<and> NP = NP' \<and> UP = UP' \<and> Q = Q' \<and> WS = {#} \<and>
+               U = length N - 1 \<and> UP = {#} \<and> N \<noteq> [] \<and>
+        correct_watching_init N\<^sub>1 (M', N', U', D', NP', UP', Q', WS')}\<close>
+
+lemma clause_to_update_append: \<open>N \<noteq> [] \<Longrightarrow> clause_to_update La (M, N @ [C], U, D, NP, UP, WS, Q) =
+     clause_to_update La (M, N, U, D, NP, UP, WS, Q) +
+    (if La \<in> set (watched_l C) then {#length N#} else {#})\<close>
+  unfolding clause_to_update_def get_clauses_l.simps
+  apply (auto simp: clause_to_update_def nth_append)
+  by meson
+
+lemma
+  fixes N\<^sub>0 :: \<open>nat list\<close>
+  defines \<open>N\<^sub>1 \<equiv> mset (map literal_of_nat N\<^sub>0)\<close>
+  assumes \<open>(S', S) \<in> HH N\<^sub>1\<close> and \<open>twl_array_code.literals_are_in_N\<^sub>0 N\<^sub>0 (mset C)\<close> and
+    \<open>set_mset (lits_of_atms_of_mm (cdcl\<^sub>W_restart_mset.clauses (convert_to_state (twl_st_of_wl None S')))) \<subseteq>
+     set_mset N\<^sub>1\<close> and \<open>distinct C\<close>
+  shows \<open>init_dt_step_wl N\<^sub>0 C S' \<le> \<Down> (HH N\<^sub>1) (init_dt_step_l C S)\<close>
+proof -
+  have [simp]: \<open>clause_to_update L (M, N, U, D, NP, UP, WS, Q) =
+       clause_to_update L (M', N', U', D', NP', UP', WS', Q')\<close>
+    if \<open>N = N'\<close>
+    for M N U D NP UP WS Q M' N' U' D' NP' UP' WS' Q' L
+    by (auto simp: clause_to_update_def that)
+  show ?thesis
+  supply literal_of_nat.simps[simp del]
+  using assms
+  unfolding init_dt_step_wl_def init_dt_step_l_def
+  apply refine_rcg
+  subgoal by (auto simp: HH_def)
+  subgoal by fast
+  subgoal by (auto simp: HH_def)
+  subgoal by (auto simp: HH_def)
+  subgoal
+    by (cases C) (auto simp: HH_def correct_watching.simps clause_to_update_def
+        lits_of_atms_of_mm_add_mset lits_of_atms_of_m_add_mset
+        twl_array_code.literals_are_in_N\<^sub>0_def clauses_def mset_take_mset_drop_mset')
+  subgoal by (auto simp: HH_def)
+
+  subgoal by (clarsimp simp: HH_def correct_watching.simps clause_to_update_def
+        lits_of_atms_of_mm_add_mset lits_of_atms_of_m_add_mset
+        twl_array_code.literals_are_in_N\<^sub>0_def clauses_def mset_take_mset_drop_mset')
+  subgoal by (clarsimp simp: HH_def correct_watching.simps clause_to_update_def
+        lits_of_atms_of_mm_add_mset lits_of_atms_of_m_add_mset
+        twl_array_code.literals_are_in_N\<^sub>0_def clauses_def mset_take_mset_drop_mset')
+  subgoal by (cases C) (clarsimp_all simp: HH_def
+        lits_of_atms_of_mm_add_mset lits_of_atms_of_m_add_mset image_image
+        twl_array_code.literals_are_in_N\<^sub>0_def clauses_def mset_take_mset_drop_mset')
+
+  subgoal by (cases C; cases \<open>tl C\<close>) (clarsimp_all simp: HH_def
+        lits_of_atms_of_mm_add_mset lits_of_atms_of_m_add_mset image_image
+        twl_array_code.literals_are_in_N\<^sub>0_def clauses_def mset_take_mset_drop_mset')
+  subgoal by (cases C; cases \<open>tl C\<close>) (auto simp: HH_def Let_def clause_to_update_append
+        clauses_def mset_take_mset_drop_mset')
+  subgoal by fast
+  subgoal by (auto simp: HH_def)
+  subgoal by (cases C; cases \<open>tl C\<close>) (clarsimp_all simp: HH_def
+        lits_of_atms_of_mm_add_mset lits_of_atms_of_m_add_mset image_image
+        twl_array_code.literals_are_in_N\<^sub>0_def clauses_def mset_take_mset_drop_mset')
+  subgoal by (cases C; cases \<open>tl C\<close>) (auto simp: HH_def Let_def clause_to_update_append
+        clauses_def mset_take_mset_drop_mset' image_image lits_of_atms_of_m_add_mset
+        twl_array_code.literals_are_in_N\<^sub>0_def)
+  subgoal by (cases C; cases \<open>tl C\<close>) (auto simp: HH_def Let_def clause_to_update_append
+        clauses_def mset_take_mset_drop_mset')
+  done
 qed
 
 end
