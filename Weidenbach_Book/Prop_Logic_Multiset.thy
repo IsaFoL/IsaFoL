d2c2370a623f8c3b87c1a3ae926d78c2b2c6bf09
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2017-03-15 10:22:58 +0100

    adding conversion multiset to CNF formula

--- Weidenbach_Book/Prop_Logic_Multiset.thy
+++ Weidenbach_Book/Prop_Logic_Multiset.thy
@@ -153,4 +153,193 @@ next
     unfolding is_cnf_def by (auto simp: is_conj_with_TF_FNot)
 qed
 
+function formula_of_mset :: "'a clause \<Rightarrow> 'a propo" where
+  \<open>formula_of_mset \<phi> =
+     (if \<phi> = {#} then FF
+      else
+         let v = (SOME v. v \<in># \<phi>);
+             v' = (if is_pos v then FVar (atm_of v) else FNot (FVar (atm_of v))) in
+         if remove1_mset v \<phi> = {#} then v'
+         else FOr v' (formula_of_mset (remove1_mset v \<phi>)))\<close>
+  by auto
+termination
+  apply (relation \<open>measure size\<close>)
+   apply (auto simp: size_mset_remove1_mset_le_iff)
+  by (meson multiset_nonemptyE someI_ex)
+
+lemma formula_of_mset_empty[simp]: \<open>formula_of_mset {#} = FF\<close>
+  by (auto simp: Let_def)
+
+lemma formula_of_mset_empty_iff[iff]: \<open>formula_of_mset \<phi> = FF \<longleftrightarrow> \<phi> = {#}\<close>
+  by (induction \<phi>) (auto simp: Let_def)
+
+declare formula_of_mset.simps[simp del]
+
+function formula_of_msets :: "'a literal multiset set \<Rightarrow> 'a propo" where
+  \<open>formula_of_msets \<phi>s =
+     (if \<phi>s = {} \<or> infinite \<phi>s then FT
+      else
+         let v = (SOME v. v \<in> \<phi>s);
+             v' = formula_of_mset v in
+         if  \<phi>s - {v} = {} then v'
+         else FAnd v' (formula_of_msets (\<phi>s - {v})))\<close>
+  by auto
+termination
+  apply (relation \<open>measure card\<close>)
+   apply (auto simp: some_in_eq)
+  by (metis all_not_in_conv card_gt_0_iff diff_less lessI)
+
+declare formula_of_msets.simps[simp del]
+
+lemma remove1_mset_empty_iff:
+  \<open>remove1_mset v \<phi> = {#} \<longleftrightarrow> (\<phi> = {#} \<or> \<phi> = {#v#})\<close>
+  using remove1_mset_eqE by force
+
+definition fun_of_set where
+  \<open>fun_of_set A x = (if Pos x \<in> A then True else if Neg x \<in> A then False else undefined)\<close>
+
+lemma grouped_by_COr_formula_of_mset: \<open>grouped_by COr (formula_of_mset \<phi>)\<close>
+proof (induction \<open>size \<phi>\<close> arbitrary: \<phi>)
+  case 0
+  then show ?case by (subst formula_of_mset.simps) (auto simp: Let_def)
+next
+  case (Suc n) note IH = this(1) and s = this(2)
+  then have \<open>n = size (remove1_mset (SOME v. v \<in># \<phi>) \<phi>)\<close> if \<open>\<phi> \<noteq> {#}\<close>
+    using that by (auto simp: size_Diff_singleton_if some_in_eq)
+  then show ?case
+    using IH[of \<open>remove1_mset (SOME v. v \<in># \<phi>) \<phi>\<close>]
+    by(subst formula_of_mset.simps) (auto simp: Let_def grouped_by_COr_FOr)
+qed
+lemma no_T_F_formula_of_mset: \<open>no_T_F (formula_of_mset \<phi>)\<close> if \<open>formula_of_mset \<phi> \<noteq> FF\<close> for \<phi>
+  using that
+proof (induction \<open>size \<phi>\<close> arbitrary: \<phi>)
+  case 0
+  then show ?case by (subst formula_of_mset.simps) (auto simp: Let_def no_T_F_def
+        all_subformula_st_def)
+next
+  case (Suc n) note IH = this(1) and s = this(2) and FF = this(3)
+  then have \<open>n = size (remove1_mset (SOME v. v \<in># \<phi>) \<phi>)\<close> if \<open>\<phi> \<noteq> {#}\<close>
+    using that by (auto simp: size_Diff_singleton_if some_in_eq)
+  moreover have \<open>no_T_F (FVar (atm_of (SOME v. v \<in># \<phi>)))\<close>
+    by (auto simp: no_T_F_def)
+  ultimately show ?case
+    using IH[of \<open>remove1_mset (SOME v. v \<in># \<phi>) \<phi>\<close>] FF
+    by(subst formula_of_mset.simps) (auto simp: Let_def grouped_by_COr_FOr)
+qed
+
+lemma mset_of_conj_formula_of_mset[simp]: \<open>mset_of_conj(formula_of_mset \<phi>) = \<phi>\<close> for \<phi>
+proof (induction \<open>size \<phi>\<close> arbitrary: \<phi>)
+  case 0
+  then show ?case by (subst formula_of_mset.simps) (auto simp: Let_def no_T_F_def
+        all_subformula_st_def)
+next
+  case (Suc n) note IH = this(1) and s = this(2)
+  then have \<open>n = size (remove1_mset (SOME v. v \<in># \<phi>) \<phi>)\<close> if \<open>\<phi> \<noteq> {#}\<close>
+    using that by (auto simp: size_Diff_singleton_if some_in_eq)
+  moreover have \<open>no_T_F (FVar (atm_of (SOME v. v \<in># \<phi>)))\<close>
+    by (auto simp: no_T_F_def)
+  ultimately show ?case
+    using IH[of \<open>remove1_mset (SOME v. v \<in># \<phi>) \<phi>\<close>]
+    by(subst formula_of_mset.simps) (auto simp: some_in_eq Let_def grouped_by_COr_FOr remove1_mset_empty_iff)
+qed
+
+lemma mset_of_formula_formula_of_mset [simp]: \<open>mset_of_formula (formula_of_mset \<phi>) = {\<phi>}\<close> for \<phi>
+proof (induction \<open>size \<phi>\<close> arbitrary: \<phi>)
+  case 0
+  then show ?case by (subst formula_of_mset.simps) (auto simp: Let_def no_T_F_def
+        all_subformula_st_def)
+next
+  case (Suc n) note IH = this(1) and s = this(2)
+  then have \<open>n = size (remove1_mset (SOME v. v \<in># \<phi>) \<phi>)\<close> if \<open>\<phi> \<noteq> {#}\<close>
+    using that by (auto simp: size_Diff_singleton_if some_in_eq)
+  moreover have \<open>no_T_F (FVar (atm_of (SOME v. v \<in># \<phi>)))\<close>
+    by (auto simp: no_T_F_def)
+  ultimately show ?case
+    using IH[of \<open>remove1_mset (SOME v. v \<in># \<phi>) \<phi>\<close>]
+    by(subst formula_of_mset.simps) (auto simp: some_in_eq Let_def grouped_by_COr_FOr remove1_mset_empty_iff)
+qed
+
+lemma formula_of_mset_is_cnf: \<open>is_cnf (formula_of_mset \<phi>)\<close>
+  by (auto simp: is_cnf_def is_conj_with_TF_def grouped_by_COr_formula_of_mset no_T_F_formula_of_mset
+        intro!: grouped_is_super_grouped)
+
+lemma eval_clss_iff:
+  assumes \<open>consistent_interp A\<close> and \<open>total_over_set A UNIV\<close>
+  shows \<open>eval (fun_of_set A) (formula_of_mset \<phi>) \<longleftrightarrow> Partial_Clausal_Logic.true_clss A {\<phi>}\<close>
+  apply (subst cnf_eval_true_clss[OF formula_of_mset_is_cnf])
+  using assms
+  apply (auto simp add: true_cls_def fun_of_set_def consistent_interp_def total_over_set_def)
+  apply (case_tac L)
+  by (fastforce simp add: true_cls_def fun_of_set_def consistent_interp_def total_over_set_def)+
+
+lemma is_conj_with_TF_Fand_iff:
+  "is_conj_with_TF (FAnd \<phi>1 \<phi>2) \<longleftrightarrow> is_conj_with_TF \<phi>1 \<and> is_conj_with_TF \<phi>2 "
+  unfolding is_conj_with_TF_def by (subst super_grouped_by.simps) auto
+
+lemma is_CNF_Fand:
+  \<open>is_cnf (FAnd \<phi> \<psi>) \<longleftrightarrow> (is_cnf \<phi> \<and> no_T_F \<phi>) \<and> is_cnf \<psi> \<and> no_T_F \<psi>\<close>
+  by (auto simp: is_cnf_def is_conj_with_TF_Fand_iff)
+
+lemma no_T_F_formula_of_mset_iff: \<open>no_T_F (formula_of_mset \<phi>) \<longleftrightarrow> \<phi> \<noteq> {#}\<close>
+proof (induction \<open>size \<phi>\<close> arbitrary: \<phi>)
+  case 0
+  then show ?case by (subst formula_of_mset.simps) (auto simp: Let_def no_T_F_def
+        all_subformula_st_def)
+next
+  case (Suc n) note IH = this(1) and s = this(2)
+  then have \<open>n = size (remove1_mset (SOME v. v \<in># \<phi>) \<phi>)\<close> if \<open>\<phi> \<noteq> {#}\<close>
+    using that by (auto simp: size_Diff_singleton_if some_in_eq)
+  moreover have \<open>no_T_F (FVar (atm_of (SOME v. v \<in># \<phi>)))\<close>
+    by (auto simp: no_T_F_def)
+  ultimately show ?case
+    using IH[of \<open>remove1_mset (SOME v. v \<in># \<phi>) \<phi>\<close>]
+    by(subst formula_of_mset.simps) (auto simp: some_in_eq Let_def grouped_by_COr_FOr remove1_mset_empty_iff)
+qed
+
+lemma no_T_F_formula_of_msets:
+  assumes \<open>finite \<phi>\<close> and \<open>{#} \<notin> \<phi>\<close> and \<open>\<phi> \<noteq> {}\<close>
+  shows \<open>no_T_F (formula_of_msets (\<phi>))\<close>
+  using assms apply (induction \<open>card \<phi>\<close> arbitrary: \<phi>)
+  subgoal by (subst formula_of_msets.simps) (auto simp: no_T_F_def all_subformula_st_def)[]
+  subgoal
+    apply (subst formula_of_msets.simps)
+    apply (auto split:  simp: Let_def formula_of_mset_is_cnf is_CNF_Fand
+        no_T_F_formula_of_mset_iff some_in_eq)
+    apply (metis (mono_tags, lifting) some_eq_ex)
+    done
+  done
+
+lemma is_cnf_formula_of_msets:
+  assumes \<open>finite \<phi>\<close> and \<open>{#} \<notin> \<phi>\<close>
+  shows \<open>is_cnf (formula_of_msets (\<phi>))\<close>
+  using assms apply (induction \<open>card \<phi>\<close> arbitrary: \<phi>)
+  subgoal by (subst formula_of_msets.simps) (auto simp: is_cnf_def is_conj_with_TF_def)[]
+  subgoal
+    apply (subst formula_of_msets.simps)
+    apply (auto split:  simp: Let_def formula_of_mset_is_cnf is_CNF_Fand
+        no_T_F_formula_of_mset_iff some_in_eq intro: no_T_F_formula_of_msets)
+    apply (metis (mono_tags, lifting) some_eq_ex)
+    done
+  done
+
+lemma mset_of_formula_formula_of_msets:
+  assumes \<open>finite \<phi>\<close>
+  shows \<open>mset_of_formula (formula_of_msets \<phi>) = \<phi>\<close>
+  using assms apply (induction \<open>card \<phi>\<close> arbitrary: \<phi>)
+  subgoal by (subst formula_of_msets.simps) (auto simp: is_cnf_def is_conj_with_TF_def)[]
+  subgoal
+    apply (subst formula_of_msets.simps)
+    apply (auto split:  simp: Let_def formula_of_mset_is_cnf is_CNF_Fand
+        no_T_F_formula_of_mset_iff some_in_eq intro: no_T_F_formula_of_msets)
+    done
+  done
+
+lemma
+  assumes \<open>consistent_interp A\<close> and \<open>total_over_set A UNIV\<close> and \<open>finite \<phi>\<close> and \<open>{#} \<notin> \<phi>\<close>
+  shows \<open>eval (fun_of_set A) (formula_of_msets \<phi>) \<longleftrightarrow> Partial_Clausal_Logic.true_clss A \<phi>\<close>
+  apply (subst cnf_eval_true_clss[OF is_cnf_formula_of_msets[OF assms(3-4)]])
+  using assms(3) unfolding mset_of_formula_formula_of_msets[OF assms(3)]
+  by (induction \<phi>)
+    (use eval_clss_iff[OF assms(1,2)] in \<open>simp_all add: cnf_eval_true_clss formula_of_mset_is_cnf\<close>)
+
 end
