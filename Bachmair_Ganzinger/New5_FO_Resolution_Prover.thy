7a49a3db29971c9e09e08cfd377398bf5ed5fb8c
anderssch <andschl@dtu.dk>
2017-07-04 19:27:15 +0200

    proving more of from_N_to_P_or_Q

--- Bachmair_Ganzinger/New5_FO_Resolution_Prover.thy
+++ Bachmair_Ganzinger/New5_FO_Resolution_Prover.thy
@@ -2336,7 +2336,7 @@ proof -
   qed
 qed
 
-lemma from_N_to_P:
+lemma from_N_to_P_or_Q:
   assumes 
     deriv: "derivation (op \<leadsto>) Sts" and
     fair: "fair_state_seq Sts" and
@@ -2345,7 +2345,7 @@ lemma from_N_to_P:
     c: "C \<in> llimit Ns - src.Rf (llimit Ns)" and
     d: "D \<in> getN (lnth Sts i)" "enat i < llength Sts" "subsumes D C" and
     d_least: "\<forall>E \<in> {E. E \<in> (clss_of_state (sup_state Sts)) \<and> subsumes E C}. \<not>properly_subsumes E D"
-  shows "\<exists>l D' \<sigma>'. D' \<in> getP (lnth Sts l) \<and> enat l < llength Sts \<and> (\<forall>E \<in> {E. E \<in> (clss_of_state (sup_state Sts)) \<and> subsumes E C}. \<not>properly_subsumes E D') \<and> D' \<cdot> \<sigma>' = C \<and> is_ground_subst \<sigma>' \<and> subsumes D' C"
+  shows "\<exists>l D' \<sigma>'. D' \<in> getP (lnth Sts l) \<union> getQ (lnth Sts l) \<and> enat l < llength Sts \<and> (\<forall>E \<in> {E. E \<in> (clss_of_state (sup_state Sts)) \<and> subsumes E C}. \<not>properly_subsumes E D') \<and> D' \<cdot> \<sigma>' = C \<and> is_ground_subst \<sigma>' \<and> subsumes D' C"
 proof -
   let ?Ns = "\<lambda>i. getN (lnth Sts i)"
   let ?Ps = "\<lambda>i. getP (lnth Sts i)"
@@ -2426,14 +2426,27 @@ proof -
       using l_p by auto
     from forward_subsumption obtain D' where D'_p: "D' \<in> P \<union> Q \<and> subsumes D' D"
       using twins by auto
-    then have "\<not>properly_subsumes D' D"
-      using d_least sorry
-    then have "subsumes D D'" 
+    then have subs: "subsumes D' C"
+      using d(3) subsumes_trans by auto
+    moreover
+    have "D' \<in> clss_of_state (sup_state Sts)"
+      using twins D'_p l_p unfolding clss_of_state_def sup_state_def apply simp
+      by (metis contra_subsetD llength_lmap lnth_lmap lnth_subset_lSup) 
+    ultimately
+    have "\<not>properly_subsumes D' D"
+      using d_least by auto
+    then have "subsumes D D'"
       unfolding properly_subsumes_def using D'_p by auto
-    then have "\<forall>E\<in>{E \<in> clss_of_state (sup_state Sts). subsumes E C}. \<not> properly_subsumes E D'" 
+    then have mini: "\<forall>E\<in>{E \<in> clss_of_state (sup_state Sts). subsumes E C}. \<not> properly_subsumes E D'" 
       using d_least sorry (* I think it's something like this. Maybe. Well if nothing else the argument is that D and D' must simply be renamings of each other. *)
-    then show ?case 
-      sorry
+
+    obtain \<sigma>' where \<sigma>'_p: "D' \<cdot> \<sigma>' = C \<and> is_ground_subst \<sigma>'"
+      sorry (* Well if nothing else the argument is that D and D' must simply be renamings of each other. *)
+
+    show ?case
+      using D'_p twins l_p subs mini \<sigma>'_p
+      apply auto
+      done
   next
     case (backward_subsumption_P N D_twin P Q)
     then have False
@@ -2524,15 +2537,15 @@ proof
   {
     assume a: "D \<in> ?Ns i"
     then obtain D' \<sigma>' l where D'_p: 
-      "D' \<in> ?Ps l" 
+      "D' \<in> ?Ps l \<union> ?Qs l" 
       "D' \<cdot> \<sigma>' = C" 
       "enat l < llength Sts" 
       "is_ground_subst \<sigma>'" (* Do I also need that l is later than i? Probably not. *)
       "\<forall>E \<in> {E. E \<in> (clss_of_state (sup_state Sts)) \<and> subsumes E C}. \<not>properly_subsumes E D'"
       "subsumes D' C"
-      using from_N_to_P[OF deriv fair ns C_p a i_p(1) D_p(2) D_p(3)] by blast
+      using from_N_to_P_or_Q[OF deriv fair ns C_p a i_p(1) D_p(2) D_p(3)] by blast
     then obtain l' where l'_p: "D' \<in> ?Qs l'" "l' < llength Sts" (* Do I also need that l is later than l'? Probably not*)
-      using from_P_to_Q[OF deriv fair ns C_p D'_p(1) D'_p(3) D'_p(6) D'_p(5) ] by blast
+      using from_P_to_Q[OF deriv fair ns C_p _ D'_p(3) D'_p(6) D'_p(5)] by blast
     then have "D' \<in> getQ (limit_state Sts)"
       using from_Q_to_Q_inf[OF deriv fair ns C_p _ l'_p(2)] D'_p by auto
     then have "\<exists>D' \<sigma>'. D' \<in> getQ (limit_state Sts) \<and> D' \<cdot> \<sigma>' = C \<and> is_ground_subst \<sigma>'"
