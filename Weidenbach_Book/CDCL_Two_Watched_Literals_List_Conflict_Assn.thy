4513dac92fdaf25f81c025e429933019fe07948d
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2017-07-09 21:29:36 +0200

    more

--- Weidenbach_Book/CDCL_Two_Watched_Literals_List_Conflict_Assn.thy
+++ Weidenbach_Book/CDCL_Two_Watched_Literals_List_Conflict_Assn.thy
@@ -39,22 +39,53 @@ lemma mset_as_position_in_iff_nth:
     (auto simp: nth_list_update' atm_of_eq_atm_of is_pos_neg_not_is_pos
       dest: mset_as_position_atm_le_length)
 
-term \<open>fold (\<lambda>L xs. xs[atm_of L := Some (is_pos L)]) P xs\<close>
-lemma
+lemma mset_as_position_mset_union:
   fixes P xs
   defines \<open>xs' \<equiv> fold (\<lambda>L xs. xs[atm_of L := Some (is_pos L)]) P xs\<close>
-  assumes \<open>mset_as_position xs P'\<close> and
-    \<open>\<forall>L \<in> set P. atm_of L < length xs\<close> and
-    \<open>\<forall>L \<in> set P. -L \<notin># P'\<close> and
-    \<open>distinct P\<close>
+  assumes
+    mset: \<open>mset_as_position xs P'\<close> and
+    atm_P_xs: \<open>\<forall>L \<in> set P. atm_of L < length xs\<close> and
+    uL_P: \<open>\<forall>L \<in> set P. -L \<notin># P'\<close> and
+    dist: \<open>distinct P\<close> and
+    tauto: \<open>\<not>tautology (mset P)\<close>
   shows \<open>mset_as_position xs' (mset P \<union># P')\<close>
-  using assms unfolding xs'_def
-  apply (induction P)
-  subgoal by auto
-  subgoal for L P
-    apply (cases \<open>L \<in># P'\<close>)
-    apply (auto intro!: mset_as_position.add) 
-  sorry
-  sorry
+  using atm_P_xs uL_P dist tauto unfolding xs'_def
+proof (induction P)
+  case Nil
+  show ?case using mset by auto
+next
+  case (Cons L P) note IH = this(1) and atm_P_xs = this(2) and uL_P = this(3) and dist = this(4) and
+    tauto = this(5)
+  then have mset: \<open>mset_as_position (fold (λL xs. xs[atm_of L := Some (is_pos L)]) P xs) (mset P \<union># P')\<close>
+    by (auto simp: tautology_add_mset)
+  show ?case
+  proof (cases \<open>L \<in># P'\<close>)
+    case True
+    then show ?thesis
+      using mset dist
+      by (metis (no_types, lifting) add_mset_union assms(2) distinct.simps(2) fold_simps(2)
+       insert_DiffM list_update_id mset.simps(2) mset_as_position_nth set_mset_mset sup_union_right1)
+  next
+    case False
+    have [simp]: \<open>length (fold (λL xs. xs[atm_of L := Some (is_pos L)]) P xs) = length xs\<close>
+      by (induction P arbitrary: xs) auto
+    moreover have \<open>- L \<notin> set P\<close>
+      using tauto by (metis list.set_intros(1) list.set_intros(2) set_mset_mset tautology_minus) 
+    moreover have \<open>fold (λL xs. xs[atm_of L := Some (is_pos L)]) P (xs[atm_of L := Some (is_pos L)]) =
+    fold (λL xs. xs[atm_of L := Some (is_pos L)]) P xs [atm_of L := Some (is_pos L)]\<close>
+       using uL_P dist tauto
+      apply (induction P arbitrary: xs)
+      subgoal by auto
+      subgoal for L' P
+        by (cases \<open>atm_of L = atm_of L'\<close>)
+          (auto simp: tautology_add_mset list_update_swap atm_of_eq_atm_of)
+      done
+    ultimately show ?thesis
+      using mset atm_P_xs dist uL_P False by (auto intro!: mset_as_position.add)      
+  qed
+qed
+
+definition conflict_rel :: "(bool option list × nat literal multiset) set" where
+\<open>conflict_rel = p2rel mset_as_position\<close>
 
 end
