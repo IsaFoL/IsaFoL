a3515afe1a66e728eb7d1584893d6424df12856b
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2017-09-12 14:48:01 +0200

    adding invariant

--- Weidenbach_Book/CDCL_Two_Watched_Literals_List.thy
+++ Weidenbach_Book/CDCL_Two_Watched_Literals_List.thy
@@ -1382,7 +1382,8 @@ definition backtrack_l_inv where
       get_conflict_l S \<noteq> None \<and>
       twl_struct_invs (twl_st_of None S) \<and>
       twl_stgy_invs (twl_st_of None S) \<and>
-      additional_WS_invs S
+      additional_WS_invs S \<and>
+      get_conflict_l S \<noteq> None
   \<close>
 
 definition propgate_bt_l :: \<open>'v literal \<Rightarrow> 'v literal \<Rightarrow> 'v twl_st_l \<Rightarrow> 'v twl_st_l nres\<close> where
