84621accf32339549b1a08f7dc4424d6751a3db1
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2017-10-12 22:23:51 +0200

    better code equation for length_aa_u
    avoid conversion to unbounded integer in SML code

--- Weidenbach_Book/Array_UInt.thy
+++ Weidenbach_Book/Array_UInt.thy
@@ -201,6 +201,12 @@ lemma array_set_hnr_u[sepref_fr_rules]:
 definition (in -)length_aa_u :: \<open>('a::heap array_list) array \<Rightarrow> uint32 \<Rightarrow> nat Heap\<close> where
   \<open>length_aa_u xs i = length_aa xs (nat_of_uint32 i)\<close>
 
+lemma length_aa_u_code[code]:
+  \<open>length_aa_u xs i = nth_u_code xs i \<bind> arl_length\<close>
+  unfolding length_aa_u_def length_aa_def  nth_u_def[symmetric] nth_u_code_def
+   Array.nth'_def 
+  by (auto simp: nat_of_uint32_code)
+
 lemma length_aa_u_hnr[sepref_fr_rules]: \<open>(uncurry length_aa_u, uncurry (RETURN \<circ>\<circ> length_ll)) \<in>
      [\<lambda>(xs, i). i < length xs]\<^sub>a (arrayO_assn (arl_assn R))\<^sup>k *\<^sub>a uint32_nat_assn\<^sup>k \<rightarrow> nat_assn\<close>
   by sepref_to_hoare (sep_auto simp: uint32_nat_rel_def length_aa_u_def br_def)
