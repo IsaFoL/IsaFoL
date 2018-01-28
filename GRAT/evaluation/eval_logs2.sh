c9ea958c08d65e673cf2543bc214c352ed497d86
Peter Lammich <lammich@in.tum.de>
2018-01-28 15:36:23 -0500

    Added new evaluations for SAT2017 competition

--- GRAT/evaluation/eval_logs2.sh
+++ GRAT/evaluation/eval_logs2.sh
@@ -3,9 +3,9 @@
 # test -n "$FIELDS" || FIELDS="drat gratgen-j1 gratchk-j1 gratgen-j4 gratchk-j4 gratgen-j8 gratchk-j8 gratgen-j12 gratchk-j12 gratgen-j16 gratchk-j16"
 test -n "$FIELDS" || FIELDS="drat lrat gratgen-j1 gratchk-j1 gratgen-j8 gratchk-j8"
 
-test -n "$PROBLEMS" || PROBLEMS=`cat sc2016_riss6_unsat.list`
+test -n "$PROBLEMS" || PROBLEMS=`cat sc2017_maple_unsat.list`
 
-test -n "$LOGDIR" || LOGDIR="logs/2017-06-combined/riss6"
+test -n "$LOGDIR" || LOGDIR="logs/2018-01-maple"
 
 OLDDIR="$PWD"
 
