fe858112922f1449e647d6e7b3eddf6461ec51c2
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2018-03-19 16:00:57 +0100

    more

--- Weidenbach_Book/Watched_Literals_Algorithm_Enumeration.thy
+++ Weidenbach_Book/Watched_Literals_Algorithm_Enumeration.thy
@@ -22,19 +22,40 @@ context
 begin
 
 definition cdcl_twl_enum :: \<open>'v twl_st \<Rightarrow> 'v twl_st nres\<close>where
-  \<open>cdcl_twl_enum S =
+  \<open>cdcl_twl_enum S = do {
+     S \<leftarrow> conclusive_TWL_run S;
      WHILE\<^sub>T\<^bsup>cdcl_twl_enum_inv\<^esup>
        (\<lambda>S. get_conflict S \<noteq> None \<and> P (lits_of_l (get_trail S)))
        (\<lambda>S. conclusive_TWL_run S)
-       S\<close>
+       S
+    }\<close>
+
+lemma enumerate_model_unless_alt_def:
+  \<open>RETURN (enumerate_model_unless P N) = do {
+     let MN = (N, get_option_model N);
+     (N, M) \<leftarrow> WHILE\<^sub>T (\<lambda>(N, M). M \<noteq> None \<and> \<not>P (set (the M)))
+       (\<lambda>(N, M). do {
+          let C = (image_mset uminus (mset (the M)));
+          RETURN (add_mset C N, get_option_model (add_mset C N))
+        })
+       MN;
+     RETURN M
+   }\<close>
+  apply (induction P N rule: enumerate_model_unless.induct)
+  apply (subst enumerate_model_unless.simps)
+  apply (subst WHILET_unfold)
+  apply (auto split: option.splits)
+  done
 
 lemma
   \<open>(cdcl_twl_enum, RETURN o (enumerate_model_unless P)) \<in> 
     enum_equisatisfiable_st_clss \<rightarrow>\<^sub>f \<langle>enum_model_st\<rangle>nres_rel\<close>
   unfolding cdcl_twl_enum_def enum_equisatisfiable_st_clss_def
-  apply (subst enumerate_model_unless.simps[abs_def])
   apply (intro frefI nres_relI)
+  apply simp
+  apply (subst enumerate_model_unless_alt_def[abs_def])
   apply clarify
+  apply refine_vcg
   oops
 
 end
