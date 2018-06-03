36536c09581043b5b17e0925a2a7ec02fd15ab64
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2018-06-03 22:46:42 +0200

    more

--- Weidenbach_Book/Watched_Literals_Watch_List_Restart.thy
+++ Weidenbach_Book/Watched_Literals_Watch_List_Restart.thy
@@ -561,6 +561,221 @@ definition rewatch_clauses :: \<open>'v twl_st_wl \<Rightarrow> 'v twl_st_wl nre
      (M, N, D, NE, UE, Q) = (M', N', D', NE', UE', Q') \<and>
     correct_watching (M, N', D, NE, UE, Q, W')))\<close>
 
+definition empty_WL :: \<open>('v literal \<Rightarrow> watched) \<Rightarrow> ('v literal \<Rightarrow> watched)\<close>  where
+  \<open>empty_WL W = (\<lambda>_. [])\<close>
+
+definition rewatch_clause 
+  :: \<open>'v clauses_l \<Rightarrow> nat \<Rightarrow> ('v literal \<Rightarrow> watched) \<Rightarrow> ('v literal \<Rightarrow> watched) nres\<close>
+where
+  \<open>rewatch_clause N C W = do {
+    ASSERT(length (N \<propto> C) > 1);
+    let L = N \<propto> C ! 0;
+    let L' = N \<propto> C ! 1;
+    RETURN (W(L := W L @ [C], L' := W L' @ [C]))
+  }\<close>
+
+definition rewatch_clauses_prog_inv where
+  \<open>rewatch_clauses_prog_inv = (\<lambda>(M, N, D, NE, UE, Q, W) xs (i, W).
+    i \<le> length xs \<and>
+      correct_watching (M, fmrestrict_set (set (take i xs)) N, D, NE, UE, Q, W))\<close>
+
+definition rewatch_clauses_prog_pre :: \<open>'v twl_st_wl \<Rightarrow> bool\<close>  where
+  \<open>rewatch_clauses_prog_pre S \<longleftrightarrow>
+     (\<exists>T U.
+        (S, T) \<in> state_wl_l None \<and>
+        (T, U) \<in> twl_st_l None \<and>
+        twl_struct_invs U \<and>
+        partial_correct_watching S)\<close>
+
+definition rewatch_clauses_prog :: \<open>'v twl_st_wl \<Rightarrow> 'v twl_st_wl nres\<close> where
+\<open>rewatch_clauses_prog = (\<lambda>(M, N, D, NE, UE, Q, W0). do {
+  ASSERT(rewatch_clauses_prog_pre (M, N, D, NE, UE, Q, W0));
+  let W0 = empty_WL W0;
+  xs \<leftarrow> SPEC(\<lambda>xs. dom_m N \<subseteq># mset xs \<and> distinct xs);
+  (_, W) \<leftarrow> WHILE\<^sub>T\<^bsup>rewatch_clauses_prog_inv (M, N, D, NE, UE, Q, W0) xs\<^esup>
+    (\<lambda>(i, W). i < length xs)
+    (\<lambda>(i, W). do {
+      ASSERT(i < length xs);
+      if xs!i \<in># dom_m N then do {
+        W \<leftarrow> rewatch_clause N (xs!i) W;
+        RETURN(i+1, W)
+      } else  RETURN(i+1, W)
+    })
+    (0, W0);
+  RETURN (M, N, D, NE, UE, Q, W)
+})\<close>
+
+lemma fmrestrict_set_fmupd:
+  \<open>a \<in> xs \<Longrightarrow> fmrestrict_set xs (fmupd a C N) = fmupd a C (fmrestrict_set xs N)\<close>
+  \<open>a \<notin> xs \<Longrightarrow> fmrestrict_set xs (fmupd a C N) = fmrestrict_set xs N\<close>
+  by (auto simp: fmfilter_alt_defs)
+
+lemma fset_fmdom_fmrestrict_set:
+  \<open>fset (fmdom (fmrestrict_set xs N)) = fset (fmdom N) \<inter> xs\<close>
+  by (auto simp: fmfilter_alt_defs)
+
+lemma finite_mset_set_inter:
+  \<open>finite A \<Longrightarrow> finite B \<Longrightarrow> mset_set (A \<inter> B) = mset_set A \<inter># mset_set B\<close>
+  apply (induction A rule: finite_induct)
+  subgoal by auto
+  subgoal for a A
+    apply (cases \<open>a \<in> B\<close>; cases \<open>a \<in># mset_set B\<close>)
+    using multi_member_split[of a \<open>mset_set B\<close>]
+    by (auto simp: mset_set.insert_remove)
+  done
+
+
+lemma distinct_mset_inter_remdups_mset:
+  assumes dist: \<open>distinct_mset A\<close>
+  shows \<open>A \<inter># remdups_mset B = A \<inter># B\<close>
+proof -
+  have [simp]: \<open>A' \<inter># remove1_mset a (remdups_mset Aa) = A' \<inter># Aa\<close>
+    if 
+      \<open>A' \<inter># remdups_mset Aa = A' \<inter># Aa\<close> and
+      \<open>a \<notin># A'\<close> and
+      \<open>a \<in># Aa\<close>
+    for A' Aa :: \<open>'a multiset\<close> and a
+  by (metis insert_DiffM inter_add_right1 set_mset_remdups_mset that)
+
+  show ?thesis
+    using dist
+    apply (induction A)
+    subgoal by auto
+     subgoal for a A'
+      apply (cases \<open>a \<in># B\<close>)
+      using multi_member_split[of a \<open>B\<close>]  multi_member_split[of a \<open>A\<close>]
+      by (auto simp: mset_set.insert_remove)
+    done
+qed
+
+
+lemma dom_m_fmresctrict_set: \<open>dom_m (fmrestrict_set (set xs) N) =
+  mset xs \<inter># dom_m N\<close>
+  using fset_fmdom_fmrestrict_set[of \<open>set xs\<close> N] distinct_mset_dom[of N]
+  distinct_mset_inter_remdups_mset[of \<open>mset_fset (fmdom N)\<close> \<open>mset xs\<close>]
+  by (auto simp: dom_m_def fset_mset_mset_fset finite_mset_set_inter multiset_inter_commute
+    remdups_mset_def)
+
+lemma
+  assumes
+    ST: \<open>(S, T) \<in> state_wl_l None\<close> and
+    TU: \<open>(T, U) \<in> twl_st_l None\<close> and
+    struct_invs: \<open>twl_struct_invs U\<close> andfir
+    \<open>twl_list_invs T\<close>
+    \<open>partial_correct_watching S\<close>
+  shows
+    \<open>rewatch_clauses_prog S \<le> rewatch_clauses S\<close>
+proof -
+  have pre: \<open>rewatch_clauses_prog_pre S\<close>
+    using assms unfolding rewatch_clauses_prog_pre_def by blast
+  have size_ge2: \<open>1 < length (N \<propto> (xs ! a))\<close>
+    if 
+      N: \<open>N = get_clauses_wl S\<close> and
+      dom: \<open>xs ! a \<in># dom_m N\<close>
+    for N xs a
+  proof -
+    have \<open>twl_st_inv U\<close>
+      using struct_invs unfolding twl_struct_invs_def by fast+
+    then have \<open>Multiset.Ball (get_clauses U) struct_wf_twl_cls\<close>
+      unfolding twl_st_inv_alt_def by fast
+    moreover have \<open>mset (N \<propto> (xs ! a)) \<in># clauses (get_clauses U)\<close>
+      using ST TU N dom by (auto simp: mset_take_mset_drop_mset')
+    ultimately show ?thesis
+      apply (auto dest!: multi_member_split simp del: size_mset
+        simp: size_mset[symmetric])
+      apply (case_tac x)
+      apply auto
+      done
+  qed
+  have \<open>rewatch_clauses_prog_inv (M, N, D, NE, UE, Q, \<lambda>_. []) xs
+        (i + 1, W2
+          (N \<propto> (xs ! i) ! 0 := W2 (N \<propto> (xs ! i) ! 0) @ [xs ! i],
+          N \<propto> (xs ! i) ! 1 := W2 (N \<propto> (xs ! i) ! 1) @ [xs ! i]))\<close>
+    if 
+      \<open>S = (M, N, D, NE, UE, Q, W)\<close> and
+      \<open>rewatch_clauses_prog_pre (M, N, D, NE, UE, Q, W)\<close> and
+      \<open>xs \<in> {xs. dom_m N \<subseteq># mset xs \<and> distinct xs}\<close> and
+      inv: \<open>rewatch_clauses_prog_inv (M, N, D, NE, UE, Q, \<lambda>_. []) xs s\<close> and
+      \<open>case s of (i, W) \<Rightarrow> i < length xs\<close> and
+      s: \<open>s = (i, W2)\<close> and
+      [simp]: \<open>i < length xs\<close> \<open>xs ! i \<in># dom_m N\<close> and
+      \<open>1 < length (N \<propto> (xs ! i))\<close>
+    for M N D NE UE Q W xs i W2 s
+  proof -
+    define N0 where \<open>N0 \<equiv> fmrestrict_set (set (take i xs)) N\<close>
+    define C where \<open>C \<equiv> xs ! i\<close>
+    define D' where \<open>D' \<equiv> the (fmlookup N C)\<close>
+    define N' where \<open>N' \<equiv> fmdrop C N\<close>
+    have D': \<open>D' = (N \<propto> C, irred N C)\<close>
+      unfolding D'_def
+      by auto
+    have N': \<open>N = fmupd C D' N'\<close>
+      unfolding N'_def C_def D'_def
+      by (smt fmap_ext fmlookup_drop fmupd_lookup in_dom_m_lookup_iff option.collapse that(8)) 
+    have [simp]: \<open>xs ! i \<notin> set (take i xs)\<close>
+      by (metis (no_types, lifting) in_set_conv_iff less_not_refl3 mem_Collect_eq
+         nth_eq_iff_index_eq that(3) that(7))
+    have [simp]: \<open>xs ! i \<notin># dom_m N'\<close>
+      using distinct_mset_dom[of N] unfolding N'_def by (auto simp: distinct_mset_remove1_All C_def)
+    then have [simp]: \<open>fmlookup N' (xs ! i) = None\<close>
+      by (simp_all add: C_def N'_def)
+    have \<open>fmfilter (\<lambda>a. a = xs ! i \<or> a \<in> set (take i xs)) N' =
+      fmfilter (\<lambda>a. a \<in> set (take i xs)) N'\<close>
+      by (rule fmfilter_cong) auto
+    then have N1: \<open>fmrestrict_set (set (take (i + 1) xs)) N = fmupd C (N \<propto> C, irred N C) N0\<close>
+      by (auto simp: take_Suc_conv_app_nth N0_def C_def fmfilter_alt_defs N')
+    have [simp]: \<open>C \<notin># dom_m N0\<close>
+      unfolding N0_def
+      by (auto simp: dom_m_fmresctrict_set C_def)
+    have H: \<open>{#Ca \<in># dom_m N0. (C = Ca \<longrightarrow> P Ca) \<and> (C \<noteq> Ca \<longrightarrow> P' Ca)#} =
+       {#Ca \<in># dom_m N0. P' Ca#}\<close> for P P'
+      by (rule filter_mset_cong2) auto
+    have \<open>correct_watching (M, fmrestrict_set (set (take i xs)) N, D, NE, UE, Q, W2)\<close>
+      using inv 
+      unfolding s rewatch_clauses_prog_inv_def prod.case
+      by fast
+    then have \<open>correct_watching
+     (M, fmrestrict_set (set (take (i + 1) xs)) N, D, NE, UE, Q, W2
+      (N \<propto> (xs ! i) ! 0 := W2 (N \<propto> (xs ! i) ! 0) @ [xs ! i],
+       N \<propto> (xs ! i) ! 1 := W2 (N \<propto> (xs ! i) ! 1) @ [xs ! i]))\<close>
+      unfolding N1 N0_def[symmetric] D'[symmetric] C_def[symmetric]
+      apply (auto simp: correct_watching.simps clause_to_update_def H
+        )
+
+     sorry
+    show ?thesis
+      unfolding rewatch_clauses_prog_inv_def prod.case
+       
+     sorry
+  qed
+
+  show ?thesis
+    unfolding rewatch_clauses_prog_def rewatch_clauses_def Let_def empty_WL_def rewatch_clause_def
+    apply (cases S)
+    apply clarify
+    apply (intro ASSERT_leI)
+    subgoal using pre by fast
+    subgoal for M N D NE UE Q W
+      unfolding intro_spec_iff
+      apply (intro ballI)
+      subgoal for xs
+        apply (refine_vcg
+          WHILEIT_rule[where R= \<open>measure (\<lambda>(i::nat, W::_ literal \<Rightarrow> watched). length xs -i)\<close>])
+        subgoal by auto
+        subgoal by (auto simp: rewatch_clauses_prog_inv_def correct_watching.simps clause_to_update_def)
+        subgoal by auto
+        subgoal by (rule size_ge2) auto
+        subgoal for s i W2
+          explore_have
+           apply (auto simp: rewatch_clauses_prog_inv_def correct_watching.simps clause_to_update_def)
+           sorry
+        subgoal by auto
+        subgoal by auto
+        subgoal unfolding rewatch_clauses_prog_inv_def by auto
+        subgoal by auto
+        subgoal unfolding rewatch_clauses_prog_inv_def by auto
+term fmrestrict_set
+
 context twl_restart
 begin
 
