d44b15e4a0633f5821641b4c6e53d37ccf4afc44
Jasmin Blanchette <jasmin.blanchette@gmail.com>
2018-07-01 21:18:16 +0200

    more

--- AVATAR/AVATAR.thy
+++ AVATAR/AVATAR.thy
@@ -5,13 +5,10 @@ theory AVATAR
     Ordered_Resolution_Prover.Inference_System
 begin
 
-(* FIXME: suboptimal for SMT *)
 type_synonym assertion = nat
 
 datatype 'a aclause =
   AClause (clause_of: "'a clause") (asserts_of: "assertion set")
-
-
 
 datatype 'a ainference =
   AInfer (aside_prems_of: "'a aclause multiset") (amain_prem_of: "'a aclause")
@@ -21,7 +18,7 @@ abbreviation aprems_of :: "'a ainference \<Rightarrow> 'a aclause multiset" wher
   "aprems_of \<gamma> \<equiv> aside_prems_of \<gamma> + {#amain_prem_of \<gamma>#}"
 
 definition aconcl_of where
-  "aconcl_of a\<gamma> = AClause (araw_concl_of a\<gamma>) (\<Union>a \<in> set_mset (aprems_of a\<gamma>). asserts_of a)"
+  "aconcl_of \<gamma> = AClause (araw_concl_of \<gamma>) (\<Union>a \<in> set_mset (aprems_of \<gamma>). asserts_of a)"
 
 abbreviation aconcls_of :: "'a ainference set \<Rightarrow> 'a aclause set" where
   "aconcls_of \<Gamma> \<equiv> aconcl_of ` \<Gamma>"
@@ -45,17 +42,19 @@ locale avatar = substitution subst_atm
 begin
 
 definition a\<Gamma> :: "'a ainference set" where
-  "a\<Gamma> = {a\<gamma>. infer_of_ainfer a\<gamma> \<in> \<Gamma>}"
+  "a\<Gamma> = {\<gamma>. infer_of_ainfer \<gamma> \<in> \<Gamma>}"
 
 definition ainferences_from :: "'a aclause set \<Rightarrow> 'a ainference set" where
-  "ainferences_from CC = {a\<gamma>. a\<gamma> \<in> a\<Gamma> \<and> ainfer_from CC a\<gamma>}"
+  "ainferences_from CC = {\<gamma>. \<gamma> \<in> a\<Gamma> \<and> ainfer_from CC \<gamma>}"
 
 definition ainferences_between :: "'a aclause set \<Rightarrow> 'a aclause \<Rightarrow> 'a ainference set" where
-  "ainferences_between CC C = {a\<gamma>. a\<gamma> \<in> a\<Gamma> \<and> ainfer_from (CC \<union> {C}) a\<gamma> \<and> C \<in># aprems_of a\<gamma>}"
+  "ainferences_between CC C = {\<gamma>. \<gamma> \<in> a\<Gamma> \<and> ainfer_from (CC \<union> {C}) \<gamma> \<and> C \<in># aprems_of \<gamma>}"
 
-inductive AV (infix "\<leadsto>\<^sub>a" 50) where
-  inference_computation: "N' = mset_set (ainferences_between (set_mset Q) C) \<Longrightarrow>
+inductive AV :: "('a, 'b) astate \<Rightarrow> ('a, 'b) astate \<Rightarrow> bool" (infix "\<leadsto>\<^sub>a" 50) where
+  inference_computation: "N' = mset_set (aconcls_of (ainferences_between (set_mset Q) C)) \<Longrightarrow>
     (Some N, P + {#C#}, Q, L, X, M) \<leadsto>\<^sub>a (Some (N + N'), P, Q + {#C#}, L, X, M)"
+| forward_subsumption: "D \<in># P + Q \<Longrightarrow> subsumes (clause_of D) (clause_of C) \<Longrightarrow> 
+    (Some (N + {#C#}), P, Q, L, X, M) \<leadsto>\<^sub>a (Some N, P, Q, L, X, M)"
 
 end
 
