d8adb348c5dbbc36435a6b0b535aba83ff2cbeee
Sophie Tourret <sophie.tourret@mpi-inf.mpg.de>
2018-09-07 10:01:09 +0200

    Saturation framework: lemma 7

--- Saturation_Framework/Saturation_Lifting.thy
+++ Saturation_Framework/Saturation_Lifting.thy
@@ -22,8 +22,34 @@ begin
 abbreviation \<G>_set :: \<open>'f formulas \<Rightarrow> 'g formulas\<close> where
   \<open>\<G>_set N \<equiv> \<Union>C \<in> N. \<G>_F C\<close>
 
+lemma \<G>_subset: \<open>N1 \<subseteq> N2 \<Longrightarrow> \<G>_set N1 \<subseteq> \<G>_set N2\<close> by auto
+
 definition entails_\<G>  :: \<open>'f formulas \<Rightarrow> 'f formulas \<Rightarrow> bool\<close> (infix "\<Turnstile>\<G>" 50) where
 \<open>N1 \<Turnstile>\<G> N2 \<equiv> (\<G>_set N1) \<Turnstile>G (\<G>_set N2)\<close>
+
+text \<open>lemma 7 in Uwe's notes\<close>
+interpretation lifted_consequence_relation: consequence_relation  
+  where Bot_F=Bot_F_F and entails=entails_\<G>
+proof
+  fix N
+  show \<open>{Bot_F_F} \<Turnstile>\<G> N\<close> using Bot_F_map bot_implies_all entails_\<G>_def by auto
+next
+  fix N1 N2 :: \<open>'f formulas\<close>
+  assume 
+    incl: \<open>N2 \<subseteq> N1\<close>
+  show \<open>N1 \<Turnstile>\<G> N2\<close> using incl entails_\<G>_def \<G>_subset subset_entailed by auto
+next
+  fix N1 N2
+  assume
+    N1_entails_C: \<open>\<forall>C \<in> N2. N1 \<Turnstile>\<G> {C}\<close>
+  show \<open>N1 \<Turnstile>\<G> N2\<close> using all_formulas_entailed N1_entails_C entails_\<G>_def 
+    by (smt UN_E UN_I easy1 singletonI)
+next
+  fix N1 N2 N3
+  assume
+    trans: \<open>N1 \<Turnstile>\<G> N2 \<and> N2 \<Turnstile>\<G> N3\<close>
+  show \<open>N1 \<Turnstile>\<G> N3\<close> using trans entails_\<G>_def transitive_entails by blast
+qed
 
 end
 
