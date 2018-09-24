5b663361412a2a1bd4f7a2de54d46e26bf938663
Sophie Tourret <sophie.tourret@mpi-inf.mpg.de>
2018-09-24 14:56:40 +0200

    Saturation framework: migrate from total to partial strict well-founded order

--- Saturation_Framework/Saturation_Lifting.thy
+++ Saturation_Framework/Saturation_Lifting.thy
@@ -1,21 +1,18 @@
 theory Saturation_Lifting
-  imports Saturation_Framework "../lib/Explorer"
+  imports Saturation_Framework "../lib/Explorer" Well_Quasi_Orders.Minimal_Elements
 begin
 
-text \<open>In Uwe's notes, a well-founded partial strict ordering is used.
-  For simplicity, I will start by using a total one (wellorder), because there is a type class 
-  already defined and lots of theorems. To go from total to partial order, an example to look at is
-  the locale order_pair from the theory SN_Orders in the AFP\<close>
 locale redundancy_criterion_lifting = inference_system Bot_F_G entails_G I_G Red_I_G Red_F_G
+  + minimal_element Prec_F UNIV
   for
     Bot_F_G :: \<open>'g set\<close> and
     entails_G :: \<open>'g formulas \<Rightarrow> 'g formulas \<Rightarrow> bool\<close> (infix "\<Turnstile>G" 50) and
     I_G :: \<open>'g inference set\<close> and
     Red_I_G :: \<open>'g formulas \<Rightarrow> 'g inference set\<close> and
-    Red_F_G :: \<open>'g formulas \<Rightarrow> 'g formulas\<close>
+    Red_F_G :: \<open>'g formulas \<Rightarrow> 'g formulas\<close> and
+    Prec_F :: \<open>'f \<Rightarrow> 'f \<Rightarrow> bool\<close> (infix "\<sqsubset>" 50)
   + fixes
-    Bot_F_F :: \<open>'f :: {wellorder} set\<close> and
-    (*{wellorder} constrains 'f to have a total strict well-founded order *)
+    Bot_F_F :: \<open>'f set\<close> and
     I_F :: \<open>'f inference set\<close> and
     \<G>_F :: \<open>'f \<Rightarrow> 'g formulas\<close> and
     \<G>_I :: \<open>'f inference \<Rightarrow> 'g inference set\<close>
@@ -78,15 +75,19 @@ next
   show \<open>N1 \<Turnstile>\<G> N3\<close> using trans entails_\<G>_def transitive_entails by blast
 qed
 
-abbreviation sqsubset :: \<open>'f \<Rightarrow> 'f \<Rightarrow> bool\<close>  (infix "\<sqsubset>" 50) where \<open>C1 \<sqsubset> C2 \<equiv> C1 < C2\<close>
-
-abbreviation sqsubseteq ::  \<open>'f \<Rightarrow> 'f \<Rightarrow> bool\<close>  (infix "\<sqsubseteq>" 50) where \<open>C1 \<sqsubseteq> C2 \<equiv> C1 \<le> C2\<close>
-
 definition Red_I_\<G> :: "'f formulas \<Rightarrow> 'f inference set" where
   \<open>Red_I_\<G> N = {\<iota> \<in> I_F. \<G>_I \<iota> \<subseteq> Red_I_G (\<G>_set N)}\<close>
 
 definition Red_F_\<G> :: "'f formulas \<Rightarrow> 'f formulas" where
   \<open>Red_F_\<G> N = {C. \<forall>D \<in> \<G>_F C. D \<in> Red_F_G (\<G>_set N) \<or> (\<exists>E \<in> N. E \<sqsubset> C \<and> D \<in> \<G>_F E)}\<close>
+
+lemma Prec_trans: 
+  assumes 
+    \<open>A \<sqsubset> B\<close> and
+    \<open>B \<sqsubset> C\<close>
+  shows
+    \<open>A \<sqsubset> C\<close>
+  using po assms unfolding po_on_def transp_on_def by blast
 
 text \<open>lemma 8 in Uwe's notes\<close>
 lemma Red_F_\<G>_equiv_def: 
@@ -104,22 +105,28 @@ proof (rule;clarsimp)
   proof (rule ccontr)
     assume contrad: \<open>D \<notin> Red_F_G (\<G>_set N)\<close>
     have non_empty: \<open>\<exists>E\<in>N. E \<sqsubset> C \<and> D \<in> \<G>_F E\<close> using contrad unfol_C_D by auto
-    then obtain F where F: \<open>F = (LEAST E. E \<in> N \<and> E \<sqsubset> C \<and> D \<in> \<G>_F E)\<close> by auto 
-      (* LEAST will certainly break if wellorder is replaced by a partial order, 
-      then it could be useful to look toward conditional total latices (maybe) to fix it*)
-    then have D_in_F: \<open>D \<in> \<G>_F F\<close> using non_empty LeastI by (metis (no_types, lifting))
+    define B where \<open>B = {E \<in> N. E \<sqsubset> C \<and> D \<in> \<G>_F E}\<close>
+    then have B_non_empty: \<open>B \<noteq> {}\<close> using non_empty by auto
+    obtain F where F: \<open>F = min_elt B\<close> by auto
+    then have D_in_F: \<open>D \<in> \<G>_F F\<close> unfolding B_def using non_empty
+      by (smt Sup_UNIV Sup_upper UNIV_I contra_subsetD empty_iff empty_subsetI mem_Collect_eq 
+          min_elt_mem unfol_C_D)
+    have F_prec: \<open>F \<sqsubset> C\<close> using F min_elt_mem[of B, OF _ B_non_empty] unfolding B_def by auto
     have F_not_in: \<open>F \<notin> Red_F_\<G> N\<close>
     proof
       assume F_in: \<open>F \<in> Red_F_\<G> N\<close>
       have unfol_F_D: \<open>D \<in> Red_F_G (\<G>_set N) \<or> (\<exists>G\<in>N. G \<sqsubset> F \<and> D \<in> \<G>_F G)\<close>
         using F_in D_in_F unfolding Red_F_\<G>_def by auto
-    then have \<open>\<exists>G\<in>N. G \<sqsubset> F \<and> D \<in> \<G>_F G\<close> using contrad D_in unfolding Red_F_\<G>_def by auto
-    then show \<open>False\<close> using F by (smt dual_order.strict_trans neqE non_empty not_less_Least)
+      then have \<open>\<exists>G\<in>N. G \<sqsubset> F \<and> D \<in> \<G>_F G\<close> using contrad D_in unfolding Red_F_\<G>_def by auto
+      then obtain G where G_in: \<open>G \<in> N\<close> and G_prec: \<open>G \<sqsubset> F\<close> and G_map: \<open>D \<in> \<G>_F G\<close> by auto
+      have \<open>G \<sqsubset> C\<close> using G_prec F_prec Prec_trans by blast
+      then have \<open>G \<in> B\<close> unfolding B_def using G_in G_map by auto
+      then show \<open>False\<close> using F G_prec min_elt_minimal[of B G, OF _ B_non_empty] by auto
     qed
-    have \<open>F \<in> N\<close> using F by (metis (no_types, lifting) LeastI_ex non_empty)
+    have \<open>F \<in> N\<close> using F by (metis B_def B_non_empty mem_Collect_eq min_elt_mem top_greatest)
     then have \<open>F \<in> N - Red_F_\<G> N\<close> using F_not_in by auto
     then show \<open>False\<close> 
-      using D_in_F neg_not_sec_case by (smt F dual_order.strict_trans neqE non_empty not_less_Least)
+      using D_in_F neg_not_sec_case F_prec by blast
   qed
 next
   fix C
@@ -136,10 +143,12 @@ proof
   have D_in: \<open>D \<in> \<G>_set N\<close> using D_hyp by blast
   have  D_not_in: \<open>D \<notin> Red_F_G (\<G>_set N)\<close> using D_hyp by blast
   have exist_C: \<open>\<exists>C. C \<in> N \<and> D \<in> \<G>_F C\<close> using D_in by auto
-  obtain C where C: \<open>C = (LEAST C. C \<in> N \<and> D \<in> \<G>_F C)\<close> by auto
-  (*same issue as in lemma 8*)
-  have C_in_N: \<open>C \<in> N\<close> using exist_C LeastI by (metis C)
-  have D_in_C: \<open>D \<in> \<G>_F C\<close> using exist_C LeastI by (metis C)
+  define B where \<open>B = {C \<in> N. D \<in> \<G>_F C}\<close>
+  obtain C where C: \<open>C = min_elt B\<close> by auto
+  have C_in_N: \<open>C \<in> N\<close> 
+    using exist_C by (metis B_def C empty_iff mem_Collect_eq min_elt_mem top_greatest)
+  have D_in_C: \<open>D \<in> \<G>_F C\<close> 
+    using exist_C by (metis B_def C empty_iff mem_Collect_eq min_elt_mem top_greatest)
   have C_not_in: \<open>C \<notin> Red_F_\<G> N\<close>
   proof
     assume C_in: \<open>C \<in> Red_F_\<G> N\<close>
@@ -151,7 +160,9 @@ proof
         then show \<open>False\<close> using D_not_in by simp
       next
         assume \<open>\<exists>E\<in>N. E \<sqsubset> C \<and> D \<in> \<G>_F E\<close>
-        then show \<open>False\<close> using C using not_less_Least by blast    
+        then show \<open>False\<close> 
+          using C by (metis (no_types, lifting) B_def UNIV_I empty_iff mem_Collect_eq 
+              min_elt_minimal top_greatest)
       qed
   qed
   show \<open>D \<in> \<G>_set (N - Red_F_\<G> N)\<close> using D_in_C C_not_in C_in_N by blast
@@ -234,8 +245,8 @@ proof
   then have \<open>\<forall>\<iota>' \<in> \<G>_I \<iota>. \<iota>' \<in> Red_I_G (\<G>_set (N - Red_F_\<G> N))\<close>
     using Red_I_of_subset[OF not_red_map_in_map_not_red[of N]] by auto
   then have \<open>\<forall>\<iota>' \<in> \<G>_I \<iota>. \<iota>' \<in> Red_I_G (\<G>_set (N - N'))\<close>
-    using  N'_in_Red_F_N by (smt Diff_iff Red_F_\<G>_def Red_F_\<G>_equiv_def Sup_set_def \<G>_subset 
-      inference_system.Red_I_of_subset inference_system_axioms subset_iff)
+    using  N'_in_Red_F_N by (smt Diff_iff Sup_set_def \<G>_subset inference_system.Red_I_of_subset 
+        inference_system_axioms subset_iff)
   then show \<open>\<iota> \<in> Red_I_\<G> (N - N')\<close> unfolding Red_I_\<G>_def using i_in by blast
 qed
 
