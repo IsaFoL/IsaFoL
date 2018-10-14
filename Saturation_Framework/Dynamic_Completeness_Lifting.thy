4a5b9f94146387e3e33400431b2a18d8955bcf45
Sophie Tourret <stourret@mpi-inf.mpg.de>
2018-10-14 22:51:18 +0200

    Saturation framework: work on lemma 22

--- Saturation_Framework/Dynamic_Completeness_Lifting.thy
+++ Saturation_Framework/Dynamic_Completeness_Lifting.thy
@@ -484,9 +484,55 @@ definition Red_F_\<G>_L :: "('f \<times> 'l) set \<Rightarrow> ('f \<times> 'l) 
 text \<open>Lemma 21 from the technical report\<close> (* TODO: replace with the labeled_lifted_calculus *)
 lemma "NL1 \<Turnstile>\<G>L NL2 \<longleftrightarrow> fst ` NL1 \<Turnstile>\<G> fst ` NL2" unfolding entails_comp_\<G>_L_def \<G>_F_L_def entails_\<G>_def by auto
 
+
+lemma i_in_zip: assumes "length A = length B" "i<length A" shows "(zip A B)!i = (A!i,B!i)" using assms by auto
+
+lemma fst_set_in_zip_is_first_set: assumes len: "length A = length B" shows "fst ` (set (zip A B)) = set A" 
+proof
+  show "fst ` set (zip A B) \<subseteq> set A"
+  proof
+    fix x
+    assume "x \<in> fst ` (set (zip A B))"
+    then have "\<exists>y. (x,y) \<in> set (zip A B)" by fastforce
+    then show "x \<in> set A" by (meson set_zip_leftD)
+  qed
+next
+  show "set A \<subseteq> fst ` set (zip A B)"
+  proof
+    fix x
+    assume "x \<in> set A"
+    then obtain i where x_i: "A!i = x" and len_i: "i < length A" using in_set_conv_nth by (meson in_set_conv_nth)
+    then obtain y where y_i: "B!i = y" by auto (*by (metis Ex_list_of_length in_set_impl_in_set_zip1)*)
+    then have "(x,y) = (zip A B)!i" using assms x_i i_in_zip len_i by simp 
+    then show "x \<in> fst ` set (zip A B)" by (metis fst_conv image_eqI len len_i length_zip min_less_iff_conj nth_mem)
+  qed
+qed
+
+find_theorems name: set_zip_leftD
+find_theorems "fst"
+find_theorems name: in_set_zipE
+
+lemma subset_fst: "A \<subseteq> fst ` AB \<Longrightarrow> \<forall>x \<in> A. \<exists>y. (x,y) \<in> AB" by fastforce
+
+lemma to_lists: "\<forall>x \<in> A. \<exists>y. (x,y) \<in> AB \<Longrightarrow> set Xx \<subseteq> A \<Longrightarrow> \<exists>Yy. set (zip Xx Yy) \<subseteq> AB" by (metis empty_iff list.set(1) set_mp set_zip_rightD subrelI subsetI)
+
 text \<open>lemma 22 from the technical report\<close>
 lemma "labeled_lifted_calculus.lifted_calculus.saturated NL \<longrightarrow> empty_order_lifting.lifted_calculus.saturated (fst ` NL)"
-  unfolding labeled_lifted_calculus.lifted_calculus.saturated_def empty_order_lifting.lifted_calculus.saturated_def
+  unfolding labeled_lifted_calculus.lifted_calculus.saturated_def empty_order_lifting.lifted_calculus.saturated_def labeled_lifted_calculus.empty_order_lifting.lifted_calculus.Inf_from_def empty_order_lifting.lifted_calculus.Inf_from_def
+  (*unfolding labeled_lifted_calculus.lifted_calculus.saturated_def empty_order_lifting.lifted_calculus.saturated_def labeled_lifted_calculus.empty_order_lifting.lifted_calculus.Inf_from_def empty_order_lifting.lifted_calculus.Inf_from_def labeled_lifted_calculus.Red_Inf_\<G>_def Red_Inf_\<G>_def \<G>_Inf_L_def using Inf_FL_to_Inf_F*)
+proof 
+  fix \<iota>
+  assume
+    i_in: "\<iota> \<in> Inf_F" and
+    i_prems: "set (prems_of \<iota>) \<subseteq> fst ` NL"
+  obtain Ll where Ll_length: "length Ll = length (prems_of \<iota>)" and
+    (* "\<exists>L0. Infer (zip (prems_of \<iota>) Ll) (concl_of \<iota>, L0) \<in> Inf_FL" and *)
+    "set (zip (prems_of \<iota>) Ll) \<subseteq> NL"
+    using fst_set_in_zip_is_first_set[of "prems_of \<iota>" Ll] subset_fst[OF i_prems] to_lists[of "set (prems_of \<iota>)" NL "prems_of \<iota>"] apply auto
+  then obtain L0 where "Infer (zip (prems_of \<iota>) Ll) (concl_of \<iota>, L0) \<in> Inf_FL" by fast
+  then obtain \<iota>_FL where "\<iota>_FL = Infer (zip (prems_of \<iota>) Ll) (concl_of \<iota>, L0)" by blast
+  then have "\<iota>_FL \<in> {\<iota> \<in> Inf_FL. set (prems_of \<iota>) \<subseteq> NL}" using i_prems
+  then have "\<exists>\<iota>_FL. to_F \<iota>_FL = \<iota>" using Inf_F_to_Inf_FL
 
 
 end
