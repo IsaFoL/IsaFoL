6817b5f56d57b932d17ceebde194249a7e7c990e
Sophie Tourret <sophie.tourret@mpi-inf.mpg.de>
2018-10-24 17:49:07 +0200

    Saturation framework: create file for section 5.2

---
+++ Saturation_Framework/Given_Clause_Prover_Lifting.thy
@@ -0,0 +1,17 @@
+(*  Title:       Dynamic_Completeness_Lifting
+    Author:      Sophie Tourret <stourret at mpi-inf.mpg.de>, 2018
+*)
+
+section \<open>Lifting a Single-Clause-Set Proving Process to a Given-Clause Prover\<close>
+
+subsection \<open>Application to Bachmair and Ganzinger's Resolution Prover\<close>
+
+theory Given_Clause_Prover_Lifting
+  imports
+    Dynamic_Completeness_Lifting
+    Ordered_Resolution_Prover.FO_Ordered_Resolution_Prover
+    "../lib/Explorer"
+begin
+
+end
+
