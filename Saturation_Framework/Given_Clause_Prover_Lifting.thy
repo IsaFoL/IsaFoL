0d11da5716e86e3fb63c75468037b63c90838fbd
Sophie Tourret <sophie.tourret@mpi-inf.mpg.de>
2018-11-05 15:39:43 +0100

    Saturation framework: workaround type 'g (ground clauses)

--- Saturation_Framework/Given_Clause_Prover_Lifting.thy
+++ Saturation_Framework/Given_Clause_Prover_Lifting.thy
@@ -9,17 +9,48 @@ subsection \<open>Application to Bachmair and Ganzinger's Resolution Prover\<clo
 theory Given_Clause_Prover_Lifting
   imports
     Dynamic_Completeness_Lifting
-    Ordered_Resolution_Prover.Abstract_Substitution
     Ordered_Resolution_Prover.FO_Ordered_Resolution_Prover
     "../lib/Explorer"
 begin
 
-term "substitution_ops.is_ground_cls"
-
-typedef 'a ground_clause = \<open>{C::'a clause. substitution_ops.is_ground_cls C}\<close>
 
 context FO_resolution_prover
 begin
+
+(* This part corresponds to section 5.2 step (1) in the technical report*)
+abbreviation Bot_F :: "'a clause set" where "Bot_F \<equiv> {{#}}"
+abbreviation Bot_G :: "'a clause set" where "Bot_G \<equiv> {{#}}"
+
+definition (in -) list_mset :: "'b multiset \<Rightarrow> 'b list" where
+  "list_mset M = (SOME L. mset L = M)"
+
+lemma (in -) mset_list_mset [simp]: "mset (list_mset M) = M"
+  using someI[of "\<lambda>x. mset x = M"] ex_mset[of M] unfolding list_mset_def by auto
+
+lemma (in -) set_list_mset_set_mset [simp]: "set (list_mset M) = set_mset M"
+  by (metis mset_list_mset set_mset_mset)
+
+definition conv_inf :: "'a inference \<Rightarrow> 'a clause Saturation_Framework_Preliminaries.inference" where
+  "conv_inf \<iota> = Saturation_Framework_Preliminaries.inference.Infer (list_mset (prems_of \<iota>)) (concl_of \<iota>)"
+
+definition Inf_F :: "'a clause Saturation_Framework_Preliminaries.inference set" where
+  "Inf_F = conv_inf ` (ord_FO_\<Gamma> S)"
+
+definition Inf_G :: "'a clause Saturation_Framework_Preliminaries.inference set" where
+  "Inf_G = {\<iota> \<in> Inf_F. filter (\<lambda>C. \<not> (is_ground_cls C)) (Saturation_Framework_Preliminaries.inference.prems_of \<iota>) = []}"
+
+(* This part corresponds to section 5.2 step (2) in the technical report*)
+definition entails_sound_F :: "'a clause set \<Rightarrow> 'a clause set \<Rightarrow> bool" (infix "|\<approx>F" 50)  where
+  "S1 |\<approx>F S2 \<equiv> \<forall>I. I \<Turnstile>s S1 \<longrightarrow> I \<Turnstile>s S2"
+
+definition ground_subset :: "'a clause set \<Rightarrow> 'a clause set" where
+  "ground_subset S' = {C \<in> S'. is_ground_cls C}"
+
+definition entails_sound_G :: "'a clause set \<Rightarrow> 'a clause set \<Rightarrow> bool" (infix "|\<approx>G" 50)  where
+  "S1 |\<approx>G S2 \<equiv> \<forall>I. I \<Turnstile>s ground_subset S1 \<longrightarrow> I \<Turnstile>s ground_subset S2"
+
+definition entails_comp_G :: "'a clause set \<Rightarrow> 'a clause set \<Rightarrow> bool" (infix "\<Turnstile>G" 50) where
+  "S1 \<Turnstile>G S2 \<equiv> \<forall>C2 \<in> ground_subset S2. \<exists>C1 \<in> ground_subset S1. \<forall>I. I \<Turnstile> C1 \<longrightarrow> I \<Turnstile> C2"
 
 end
 
--- Saturation_Framework/Saturation_Framework_Preliminaries.thy
+++ Saturation_Framework/Saturation_Framework_Preliminaries.thy
@@ -39,7 +39,7 @@ lemma entail_union: "N \<Turnstile> N1 \<and> N \<Turnstile> N2 \<longleftrighta
 end
 
 datatype 'f inference =
-  Infer (prems_of: "'f list") (concl_of: "'f ")
+  Infer (prems_of: "'f list") (concl_of: "'f")
 
 locale sound_inference_system = consequence_relation +
   fixes
