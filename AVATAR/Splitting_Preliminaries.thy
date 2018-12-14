42e3ad6bce643abc059d70be8a7e625615cd8270
Jasmin Blanchette <jasmin.blanchette@gmail.com>
2018-12-15 00:20:22 +0100

    more AVATAR

--- AVATAR/Splitting_Preliminaries.thy
+++ AVATAR/Splitting_Preliminaries.thy
@@ -17,11 +17,31 @@ locale clausal_consequence_relation = consequence_relation Bot for Bot :: "'a cl
     entails_subclause: "C \<subseteq># D \<Longrightarrow> {C} \<Turnstile> {D}"
 begin
 
-abbreviation entails_comp_or :: "'a clause set \<Rightarrow> 'a clause set \<Rightarrow> bool" (infix "\<Turnstile>\<^sub>\<squnion>" 50) where
-  "M \<Turnstile>\<^sub>\<squnion> CC \<equiv> (\<forall>N D. N \<supseteq> M \<longrightarrow> (\<forall>C \<in> CC. N \<union> {C} \<Turnstile> {D}) \<longrightarrow> N \<Turnstile> {D})"
+definition entails_comp_sqcup :: "'a clause set \<Rightarrow> 'a clause set \<Rightarrow> bool" (infix "\<Turnstile>\<^sub>\<squnion>" 50) where
+  "M \<Turnstile>\<^sub>\<squnion> CC \<longleftrightarrow> (\<forall>N D. N \<Turnstile> M \<longrightarrow> (\<forall>C \<in> CC. N \<union> {C} \<Turnstile> {D}) \<longrightarrow> N \<Turnstile> {D})"
 
-definition is_propositional :: "'a \<Rightarrow> bool" where
-  "is_propositional A \<longleftrightarrow> (\<forall>L C. atm_of L = A \<longrightarrow> {{#L#} + C} \<Turnstile>\<^sub>\<squnion> {{#L#}, C})"
+lemma entails_comp_sqcup_trans:
+  assumes
+    mn: "M \<Turnstile> N" and
+    ncc: "N \<Turnstile>\<^sub>\<squnion> CC"
+  shows "M \<Turnstile>\<^sub>\<squnion> CC"
+  unfolding entails_comp_sqcup_def
+  apply clarify
+  apply (rename_tac P D)
+  apply (rule ncc[unfolded entails_comp_sqcup_def, rule_format])
+  using entails_trans mn apply blast
+  by blast
+
+definition is_propos :: "'a \<Rightarrow> bool" where
+  "is_propos A \<longleftrightarrow> (\<forall>L \<in> {Pos A, Neg A}. \<forall>C. {{#L#} + C} \<Turnstile>\<^sub>\<squnion> {{#L#}, C})"
+
+lemma is_propos_entails_comp_sqcup_excluded_middle:
+  assumes prp: "is_propos A"
+  shows "{} \<Turnstile>\<^sub>\<squnion> {{#Pos A#}, {#Neg A#}}"
+  apply (rule entails_comp_sqcup_trans)
+   apply (rule entails_excluded_middle)
+  using prp[unfolded is_propos_def, rule_format, of "Pos A" "{#Neg A#}", simplified]
+  by (smt add_mset_commute clausal_consequence_relation.entails_comp_sqcup_trans clausal_consequence_relation_axioms entails_excluded_middle insert_subset order_refl subset_entailed)
 
 end
 
