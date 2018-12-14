d2e58b8ae2fb1d177f15c3c8c5d32925a9352f55
Jasmin Blanchette <jasmin.blanchette@gmail.com>
2018-12-15 00:27:43 +0100

    more AVA

--- AVATAR/Splitting_Preliminaries.thy
+++ AVATAR/Splitting_Preliminaries.thy
@@ -12,8 +12,8 @@ begin
 
 locale clausal_consequence_relation = consequence_relation Bot for Bot :: "'a clause set" +
   assumes
-    entails_excluded_middle: "{} \<Turnstile> {{#Pos A, Neg A#}}" and
-    entails_absurd: "{{#Pos A#}, {#Neg A#}} \<Turnstile> {C}" and
+    entails_excluded_middle: "{} \<Turnstile> {{#Pos a, Neg a#}}" and
+    entails_absurd: "{{#Pos a#}, {#Neg a#}} \<Turnstile> {C}" and
     entails_subclause: "C \<subseteq># D \<Longrightarrow> {C} \<Turnstile> {D}"
 begin
 
@@ -33,15 +33,16 @@ lemma entails_comp_sqcup_trans:
   by blast
 
 definition is_propos :: "'a \<Rightarrow> bool" where
-  "is_propos A \<longleftrightarrow> (\<forall>L \<in> {Pos A, Neg A}. \<forall>C. {{#L#} + C} \<Turnstile>\<^sub>\<squnion> {{#L#}, C})"
+  "is_propos a \<longleftrightarrow> (\<forall>L \<in> {Pos a, Neg a}. \<forall>C. {{#L#} + C} \<Turnstile>\<^sub>\<squnion> {{#L#}, C})"
 
 lemma is_propos_entails_comp_sqcup_excluded_middle:
-  assumes prp: "is_propos A"
-  shows "{} \<Turnstile>\<^sub>\<squnion> {{#Pos A#}, {#Neg A#}}"
+  assumes prp: "is_propos a"
+  shows "{} \<Turnstile>\<^sub>\<squnion> {{#Pos a#}, {#Neg a#}}"
   apply (rule entails_comp_sqcup_trans)
    apply (rule entails_excluded_middle)
-  using prp[unfolded is_propos_def, rule_format, of "Pos A" "{#Neg A#}", simplified]
-  by (smt add_mset_commute clausal_consequence_relation.entails_comp_sqcup_trans clausal_consequence_relation_axioms entails_excluded_middle insert_subset order_refl subset_entailed)
+  using prp[unfolded is_propos_def, rule_format, of "Pos a" "{#Neg a#}", simplified]
+  by (smt add_mset_commute clausal_consequence_relation.entails_comp_sqcup_trans
+      clausal_consequence_relation_axioms entails_excluded_middle insert_subset order_refl subset_entailed)
 
 end
 
