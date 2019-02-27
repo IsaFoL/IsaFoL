5953192f1cf6663525cba11928b96c3ea57ba3d9
anderssch <>
2019-02-27 10:09:04 +0100

    place 'if [] : set R then False else True'

--- Functional_Ordered_Resolution_Prover/Executable_FO_Ordered_Resolution_Prover.thy
+++ Functional_Ordered_Resolution_Prover/Executable_FO_Ordered_Resolution_Prover.thy
@@ -160,7 +160,7 @@ end
 definition prover :: "((nat, nat) Term.term literal list \<times> nat) list \<Rightarrow> bool" where
   "prover N = (case deterministic_RP (St0 N 0) of
       None \<Rightarrow> True
-    | Some R \<Rightarrow> if [] \<in> set R then False else True)"
+    | Some R \<Rightarrow> [] \<notin> set R)"
 
 theorem prover_complete_refutation: "prover N \<longleftrightarrow> satisfiable (RP.grounded_N0 N)"
   unfolding prover_def St0_def
