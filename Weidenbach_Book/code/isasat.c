a280462f691a056a831f38e799f089e3ab54f424
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2019-07-18 15:41:17 +0200

    IsaSAT: start code for execution in C

---
+++ Weidenbach_Book/code/isasat.c
@@ -0,0 +1,209 @@
+#include <stdio.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <stdint.h>
+
+/*the parser is based on the code from lingeling*/
+
+static char * inputname;
+static FILE * inputfile;
+
+static int lineno;
+
+static void perr (const char * fmt, ...) {
+  va_list ap;
+  fprintf (stderr, "%s:%d: ", inputname, lineno);
+  va_start (ap, fmt);
+  vfprintf (stderr, fmt, ap);
+  fputc ('\n', stderr);
+  fflush (stderr);
+  exit (1);
+}
+
+typedef struct CLAUSE {
+  int64_t size;
+  int64_t used;
+  uint32_t* clause;
+} clause, CLAUSE;
+
+void make_room(CLAUSE *cl) {
+  cl->clause = (uint32_t *) realloc(cl->clause, 2*cl->size * sizeof(uint32_t));
+  cl->size *= 2;
+}
+
+CLAUSE new_clause(void) {
+  CLAUSE cl;
+  cl.clause = (uint32_t *) malloc(16 * sizeof(uint32_t));
+  cl.size = 16;
+  cl.used = 0;
+  return cl;
+}
+
+void append_lit(int32_t lit, CLAUSE * cl) {
+  uint32_t ulit = (uint32_t)(lit < 0 ? - 2 * lit +1 : 2 * lit);
+  if(cl->used + 1 >= cl->size)
+    make_room(cl);
+  cl->used++;
+  cl->clause[cl->used] = ulit;
+}
+
+void free_clause (CLAUSE *cl) {
+  free(cl->clause);
+}
+
+typedef struct CLAUSES {
+  int64_t size;
+  CLAUSE* clauses;
+} clauses, CLAUSES;
+
+CLAUSES new_clauses(int64_t size) {
+  CLAUSES clauses;
+  clauses.size = size;
+  clauses.clauses = (CLAUSE *) malloc(size * sizeof(CLAUSE));
+  for (int64_t n = 0; n < size; ++n) {
+    clauses.clauses[n] = new_clause();
+  }
+  return clauses;
+}
+
+
+void free_clauses (CLAUSES *cl) {
+  for (int64_t n = 0; n < cl->size; ++n) {
+    free_clause(&cl->clauses[n]);
+  }
+  free(cl->clauses);
+
+}
+
+static int64_t next (void) {
+  int64_t res;
+  res = getc (inputfile);
+  if (res == '\n') lineno++;
+  return res;
+}
+
+static CLAUSES parse (void) {
+  int ch;
+  int32_t lit;
+  int32_t sign;
+  int64_t num_lits, num_clss;
+HEADER:
+  ch = next ();
+  if (ch == EOF) perr ("unexpected end-of-file in header");
+  if (ch == 'c') {
+    while ((ch = next ()) != '\n')
+      if (ch == EOF)
+	perr ("unexpected end-of-file in header comment");
+    goto HEADER;
+  }
+  if (ch != 'p' ||
+      next () != ' ' ||
+      next () != 'c' ||
+      next () != 'n' ||
+      next () != 'f' ||
+      next () != ' ') perr ("invalid header (expected 'p cnf ')");
+
+  ch = next ();
+
+  num_lits = ch - '0';
+  while (isdigit (ch = next ())) num_lits = 10*num_lits + ch - '0';
+ 
+  ch = next ();
+  num_clss = ch - '0';
+  while (isdigit (ch = next ())) num_clss = 10*num_clss + ch - '0';
+
+  CLAUSES clauses = new_clauses(num_clss);
+
+  int64_t clause_num = 0;
+  CLAUSE* cl = &clauses.clauses[0];
+
+CLAUSES:
+  if (ch == ' ' || ch == '\t' || ch == '\r' || ch == '\n') {
+    ch = next (); goto CLAUSES;
+  }
+  if (ch == 'c') {
+    while ((ch = next ()) != '\n')
+      if (ch == EOF)
+	perr ("unexpected end-of-file in body comment");
+    goto CLAUSES;
+  }
+  if (ch == EOF) goto DONE;
+  if (ch == '0') {
+    cl = NULL;
+    ch = next ();
+    goto CLAUSES;
+  }
+  if (ch == '-') {
+    sign = -1;
+    ch = next ();
+    if (!isdigit (ch)) perr ("expected digit after '-'");
+  } else sign = 1;
+
+  if (!isdigit (ch) || ch == 0) perr ("expected literal");
+
+  if (cl == NULL) {
+    if(++clause_num > num_clss)
+      perr("found too many clauses: clause  number %d, only %d expected", clause_num, num_clss);
+    cl = &clauses.clauses[clause_num];
+  }
+  lit = ch - '0';
+
+  while (isdigit (ch = next ())) lit = 10*lit + ch - '0';
+  lit *= sign;
+
+  if(lit < -num_lits || lit > num_lits || lit == 0)
+    perr("invalid literal %d, max: %d", lit, num_lits);
+
+  append_lit(lit, cl);
+  goto CLAUSES;
+
+DONE:
+  if(clause_num != num_clss - 1)
+    perr("found %d clauses, while %d were expected", clause_num, num_clss);
+
+  return clauses;
+}
+
+typedef struct OPTS {
+  _Bool s1:1;
+  _Bool s2:1;
+  _Bool s3:1;
+} OPTS;
+
+typedef struct STATS {
+  uin64_t s1;
+  uin64_t s2;
+  uin64_t s3;
+  uin64_t s4;
+  uin64_t s5;
+  uin64_t s6;
+  uin64_t s7;
+  uin64_t s8;
+} STATS;
+
+typedef struct R {
+  _Bool finished;
+  _Bool sat;
+  CLAUSE model;
+  structs STATS;
+} R;
+R IsaSAT_No_Restart_LLVM_IsaSAT_code(OPTS,CLAUSES);
+
+int main(void) {
+  inputname = "/home/mfleury/Documents/repos/SPASS/Trunk/SAT/Examples/Easy/np.core.400046.cnf";
+  inputfile = fopen (inputname, "r");
+
+  if(inputfile == NULL) {
+    printf("could not open file %s", inputname);
+    exit(EXIT_FAILURE);
+  }
+
+  CLAUSES clauses = parse();
+
+
+  OPTS opts;
+  (void)IsaSAT_No_Restart_LLVM_IsaSAT_code(opts,);
+  free_clauses(&clauses);
+  return 0;
+}
