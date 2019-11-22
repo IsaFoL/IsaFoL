82e6fd3ed78070a23a8495e884a54c7e0b229546
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2019-11-22 12:17:26 +0100

    IsaSAT: regenerate code

--- Weidenbach_Book/code/isasat.c
+++ Weidenbach_Book/code/isasat.c
@@ -306,6 +306,7 @@ FILE * open_pipe (const char * fmt, const char * path,
   prg[prglen] = 0;
   char cmd[strlen (fmt) + strlen (path)];
   sprintf (cmd, fmt, path);
+  printf("res = %s", cmd);
   FILE * res = fopen (cmd, mode);
   return res;
 }
