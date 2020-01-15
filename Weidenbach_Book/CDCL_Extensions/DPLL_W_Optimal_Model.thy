fb7005743f827ba849bee9209adab742d36ea637
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2020-01-15 10:07:19 +0100

    ODPLL: go for more locales

--- Weidenbach_Book/CDCL_Extensions/DPLL_W_Optimal_Model.thy
+++ Weidenbach_Book/CDCL_Extensions/DPLL_W_Optimal_Model.thy
@@ -4,22 +4,43 @@ imports
   CDCL.DPLL_W
 begin
 
-
-locale bnb_ops =
+locale dpll_ops =
   fixes
-    weight :: \<open>'st \<Rightarrow> 'a\<close> and
-    update_weight_information :: "'v dpll\<^sub>W_ann_lits \<Rightarrow> 'st \<Rightarrow> 'st" and
-    is_improving_int :: "'v  dpll\<^sub>W_ann_lits \<Rightarrow> 'v  dpll\<^sub>W_ann_lits \<Rightarrow> 'v clauses \<Rightarrow> 'a \<Rightarrow> bool" and
     trail :: \<open>'st \<Rightarrow> 'v  dpll\<^sub>W_ann_lits\<close> and
     clauses :: \<open>'st \<Rightarrow> 'v clauses\<close> and
     tl_trail :: \<open>'st \<Rightarrow> 'st\<close> and
     cons_trail :: \<open>'v  dpll\<^sub>W_ann_lit \<Rightarrow> 'st \<Rightarrow> 'st\<close> and
     state_eq  :: \<open>'st \<Rightarrow> 'st \<Rightarrow> bool\<close> (infix "\<sim>" 50) and
+    state :: \<open>'st \<Rightarrow> 'v  dpll\<^sub>W_ann_lits \<times> 'v clauses \<times> 'b\<close> 
+begin
+
+definition additional_info :: \<open>'st \<Rightarrow> 'b\<close> where
+  \<open>additional_info S = (\<lambda>(M, N, w). w) (state S)\<close>
+
+end
+
+locale bnb_ops =
+  fixes
+    trail :: \<open>'st \<Rightarrow> 'v  dpll\<^sub>W_ann_lits\<close> and
+    clauses :: \<open>'st \<Rightarrow> 'v clauses\<close> and
+    tl_trail :: \<open>'st \<Rightarrow> 'st\<close> and
+    cons_trail :: \<open>'v  dpll\<^sub>W_ann_lit \<Rightarrow> 'st \<Rightarrow> 'st\<close> and
+    state_eq  :: \<open>'st \<Rightarrow> 'st \<Rightarrow> bool\<close> (infix "\<sim>" 50) and
+    state :: \<open>'st \<Rightarrow> 'v  dpll\<^sub>W_ann_lits \<times> 'v clauses \<times> 'a \<times> 'b\<close> and
+    weight :: \<open>'st \<Rightarrow> 'a\<close> and
+    update_weight_information :: "'v dpll\<^sub>W_ann_lits \<Rightarrow> 'st \<Rightarrow> 'st" and
+    is_improving_int :: "'v  dpll\<^sub>W_ann_lits \<Rightarrow> 'v  dpll\<^sub>W_ann_lits \<Rightarrow> 'v clauses \<Rightarrow> 'a \<Rightarrow> bool" and
     conflicting_clauses :: "'v clauses \<Rightarrow> 'a \<Rightarrow> 'v clauses"
 begin
 
-definition state :: \<open>'st \<Rightarrow> 'v  dpll\<^sub>W_ann_lits \<times> 'v clauses \<times> 'a\<close> where
-  \<open>state S = (trail S, clauses S, weight S)\<close>
+sublocale dpll: dpll_ops where
+  trail = trail and
+  clauses = clauses and
+  tl_trail = tl_trail and
+  cons_trail = cons_trail and
+  state_eq = state_eq and
+  state = state
+  .
 
 definition conflicting_clss :: \<open>'st \<Rightarrow> 'v literal multiset multiset\<close> where
   \<open>conflicting_clss S = conflicting_clauses (clauses S) (weight S)\<close>
@@ -33,11 +54,44 @@ abbreviation is_improving where
 definition state' :: \<open>'st \<Rightarrow> 'v  dpll\<^sub>W_ann_lits \<times> 'v clauses \<times> 'a \<times> 'v clauses\<close> where
   \<open>state' S = (trail S, clauses S, weight S, conflicting_clss S)\<close>
 
+definition additional_info :: \<open>'st \<Rightarrow> 'b\<close> where
+  \<open>additional_info S = (\<lambda>(M, N, _, w). w) (state S)\<close>
+
+
+end
+
+
+locale dpll\<^sub>W_state =
+  dpll_ops trail clauses
+    tl_trail cons_trail state_eq state
+  for
+    trail :: \<open>'st \<Rightarrow> 'v  dpll\<^sub>W_ann_lits\<close> and
+    clauses :: \<open>'st \<Rightarrow> 'v clauses\<close> and
+    tl_trail :: \<open>'st \<Rightarrow> 'st\<close> and
+    cons_trail :: \<open>'v  dpll\<^sub>W_ann_lit \<Rightarrow> 'st \<Rightarrow> 'st\<close> and
+    state_eq  :: \<open>'st \<Rightarrow> 'st \<Rightarrow> bool\<close> (infix "\<sim>" 50) and
+    state :: \<open>'st \<Rightarrow> 'v  dpll\<^sub>W_ann_lits \<times> 'v clauses \<times> 'b\<close> +
+  assumes
+    state_eq_ref[simp, intro]: \<open>S \<sim> S\<close> and
+    state_eq_sym: \<open>S \<sim> T \<longleftrightarrow> T \<sim> S\<close> and
+    state_eq_trans: \<open>S \<sim> T \<Longrightarrow> T \<sim> U' \<Longrightarrow> S \<sim> U'\<close> and
+    state_eq_state: \<open>S \<sim> T \<Longrightarrow> state S = state T\<close> and
+
+    cons_trail:
+      "\<And>S'. state st = (M, S') \<Longrightarrow>
+        state (cons_trail L st) = (L # M, S')" and
+
+    tl_trail:
+      "\<And>S'. state st = (M, S') \<Longrightarrow> state (tl_trail st) = (tl M, S')" and
+    state:
+       \<open>state S = (trail S, clauses S, additional_info S)\<close>
+begin
+
 end
 
 locale bnb =
-  bnb_ops weight update_weight_information is_improving_int trail clauses
-    tl_trail cons_trail state_eq conflicting_clauses
+  bnb_ops trail clauses
+    tl_trail cons_trail state_eq state weight update_weight_information is_improving_int conflicting_clauses
   for
     weight :: \<open>'st \<Rightarrow> 'a\<close> and
     update_weight_information :: "'v dpll\<^sub>W_ann_lits \<Rightarrow> 'st \<Rightarrow> 'st" and
@@ -47,7 +101,8 @@ locale bnb =
     tl_trail :: \<open>'st \<Rightarrow> 'st\<close> and
     cons_trail :: \<open>'v  dpll\<^sub>W_ann_lit \<Rightarrow> 'st \<Rightarrow> 'st\<close> and
     state_eq  :: \<open>'st \<Rightarrow> 'st \<Rightarrow> bool\<close> (infix "\<sim>" 50) and
-    conflicting_clauses :: "'v clauses \<Rightarrow> 'a \<Rightarrow> 'v clauses" +
+    conflicting_clauses :: "'v clauses \<Rightarrow> 'a \<Rightarrow> 'v clauses"and
+    state :: \<open>'st \<Rightarrow> 'v  dpll\<^sub>W_ann_lits \<times> 'v clauses \<times> 'a \<times> 'b\<close> +
   assumes
     state_eq_ref[simp, intro]: \<open>S \<sim> S\<close> and
     state_eq_sym: \<open>S \<sim> T \<longleftrightarrow> T \<sim> S\<close> and
@@ -61,8 +116,8 @@ locale bnb =
     tl_trail:
       "\<And>S'. state st = (M, S') \<Longrightarrow> state (tl_trail st) = (tl M, S')" and
     update_weight_information:
-       \<open>state S = (M, N, w) \<Longrightarrow>
-          \<exists>w'. state (update_weight_information M' S) = (M, N, w')\<close> and
+       \<open>state S = (M, N, w, oth) \<Longrightarrow>
+          \<exists>w'. state (update_weight_information M' S) = (M, N, w', oth)\<close> and
 
     conflicting_clss_update_weight_information_mono:
       \<open>dpll\<^sub>W_all_inv (abs_state S) \<Longrightarrow> is_improving M M' S \<Longrightarrow>
@@ -70,8 +125,51 @@ locale bnb =
     conflicting_clss_update_weight_information_in:
       \<open>is_improving M M' S \<Longrightarrow> negate_ann_lits M' \<in># conflicting_clss (update_weight_information M' S)\<close> and
     atms_of_conflicting_clss:
-      \<open>atms_of_mm (conflicting_clss S) \<subseteq> atms_of_mm (clauses S)\<close>
+      \<open>atms_of_mm (conflicting_clss S) \<subseteq> atms_of_mm (clauses S)\<close> and
+    state:
+       \<open>state S = (trail S, clauses S, weight S, additional_info S)\<close>
 begin
+
+lemma [simp]: \<open>DPLL_W.clauses (abs_state S) = clauses S + conflicting_clss S\<close>
+  \<open>DPLL_W.trail (abs_state S) = trail S\<close>
+  by (auto simp: abs_state_def)
+
+
+lemma [simp]: \<open>trail (update_weight_information M' S) = trail S\<close>
+  using update_weight_information[of S]
+  by (auto simp: state)
+
+lemma [simp]:
+  \<open>clauses (update_weight_information M' S) = clauses S\<close>
+  \<open>weight (cons_trail uu S) = weight S\<close>
+  \<open>clauses (cons_trail uu S) = clauses S\<close>
+  \<open>conflicting_clss (cons_trail uu S) = conflicting_clss S\<close>
+  \<open>trail (cons_trail uu S) = uu # trail S\<close>
+  \<open>trail (tl_trail S) = tl (trail S)\<close>
+  \<open>clauses (tl_trail S) = clauses (S)\<close>
+  \<open>weight (tl_trail S) = weight (S)\<close>
+  \<open>conflicting_clss (tl_trail S) = conflicting_clss (S)\<close>
+  \<open>additional_info (cons_trail L S) = additional_info S\<close>
+  \<open>additional_info (tl_trail S) = additional_info S\<close>
+  \<open>additional_info (update_weight_information M' S) = additional_info S\<close>
+  using update_weight_information[of S]
+    cons_trail[of S]
+    tl_trail[of S]
+  by (auto simp: state conflicting_clss_def)
+
+lemma state_simp[simp]:
+  \<open>T \<sim> S \<Longrightarrow> trail T = trail S\<close>
+  \<open>T \<sim> S \<Longrightarrow> clauses T = clauses S\<close>
+  \<open>T \<sim> S \<Longrightarrow> weight T = weight S\<close>
+  \<open>T \<sim> S \<Longrightarrow> conflicting_clss T = conflicting_clss S\<close>
+  by (auto dest!: state_eq_state simp: state conflicting_clss_def)
+
+
+sublocale dpll: dpll\<^sub>W_state trail clauses tl_trail cons_trail state_eq state
+  apply standard
+  apply (auto dest: state_eq_sym[THEN iffD1] intro: state_eq_trans dest: state_eq_state)
+  apply (auto simp: state dpll.additional_info_def)
+  done
 
 
 text \<open>
@@ -87,11 +185,11 @@ text \<open>
 \<close>
 inductive dpll\<^sub>W_core :: "'st \<Rightarrow> 'st \<Rightarrow> bool" where
 propagate: "add_mset L C \<in># clauses S \<Longrightarrow> trail S \<Turnstile>as CNot C \<Longrightarrow> undefined_lit (trail S) L \<Longrightarrow>
-  state' T = (Propagated L () # trail S, clauses S, weight S, conflicting_clss S) \<Longrightarrow>
+  T \<sim> cons_trail (Propagated L ()) S \<Longrightarrow>
   dpll\<^sub>W_core S T" |
-decided: "undefined_lit (trail S) L \<Longrightarrow> atm_of L \<in> atms_of_mm (clauses S)
-  \<Longrightarrow> state' T = (Decided L # trail S, clauses S, weight S, conflicting_clss S)
-  \<Longrightarrow> dpll\<^sub>W_core S T " |
+decided: "undefined_lit (trail S) L \<Longrightarrow> atm_of L \<in> atms_of_mm (clauses S) \<Longrightarrow>
+  T \<sim> cons_trail (Decided L) S \<Longrightarrow>
+  dpll\<^sub>W_core S T " |
 backtrack: "backtrack_split (trail S) = (M', L # M) \<Longrightarrow> is_decided L \<Longrightarrow> D \<in># clauses S
   \<Longrightarrow> trail S \<Turnstile>as CNot D
   \<Longrightarrow> state' T = (Propagated (- (lit_of L)) () # M, clauses S, weight S, conflicting_clss S)
@@ -102,7 +200,7 @@ backtrack_opt: "backtrack_split (trail S) = (M', L # M) \<Longrightarrow> is_dec
   \<Longrightarrow> dpll\<^sub>W_core S T"
 
 
-inductive dpll\<^sub>W_branch  :: "'st \<Rightarrow> 'st \<Rightarrow> bool" where
+inductive dpll\<^sub>W_branch :: "'st \<Rightarrow> 'st \<Rightarrow> bool" where
 update_info:
   \<open>is_improving M M' S \<Longrightarrow> state T = state (update_weight_information M' S)
    \<Longrightarrow> dpll\<^sub>W_branch S T\<close>
@@ -117,18 +215,14 @@ bnb:
 
 inductive_cases dpll\<^sub>W_bnbE: \<open>dpll\<^sub>W_bnb S T\<close>
 
-lemma [simp]: \<open>DPLL_W.clauses (abs_state S) = clauses S + conflicting_clss S\<close>
-  \<open>DPLL_W.trail (abs_state S) = trail S\<close>
-  by (auto simp: abs_state_def)
-
 lemma dpll\<^sub>W_core_is_dpll\<^sub>W:
   \<open>dpll\<^sub>W_core S T \<Longrightarrow> dpll\<^sub>W (abs_state S) (abs_state T)\<close>
   supply abs_state_def[simp] state'_def[simp]
   apply (induction rule: dpll\<^sub>W_core.induct)
   subgoal
-    by (auto simp: dpll\<^sub>W.simps)
+    by (auto simp: dpll\<^sub>W.simps dest!: )
   subgoal
-    by (auto simp: dpll\<^sub>W.simps)
+    by (auto simp: dpll\<^sub>W.simps dest!: )
   subgoal
     by (auto simp: dpll\<^sub>W.simps)
   subgoal
@@ -153,25 +247,6 @@ lemma dpll\<^sub>W_core_same_weight:
     by (auto simp: dpll\<^sub>W.simps)
   done
 
-lemma [simp]: \<open>trail (update_weight_information M' S) = trail S\<close>
-  using update_weight_information[of S]
-  by (auto simp: state_def)
-
-lemma [simp]:
-  \<open>clauses (update_weight_information M' S) = clauses S\<close>
-  \<open>weight (cons_trail uu S) = weight S\<close>
-  \<open>clauses (cons_trail uu S) = clauses S\<close>
-  \<open>conflicting_clss (cons_trail uu S) = conflicting_clss S\<close>
-  \<open>trail (cons_trail uu S) = uu # trail S\<close>
-  \<open>trail (tl_trail S) = tl (trail S)\<close>
-  \<open>clauses (tl_trail S) = clauses (S)\<close>
-  \<open>weight (tl_trail S) = weight (S)\<close>
-  \<open>conflicting_clss (tl_trail S) = conflicting_clss (S)\<close>
-  using update_weight_information[of S]
-    cons_trail[of S]
-    tl_trail[of S]
-  by (auto simp: state_def conflicting_clss_def)
-
 lemma dpll\<^sub>W_branch_trail:
     \<open>dpll\<^sub>W_branch S T \<Longrightarrow> trail S = trail T\<close> and
    dpll\<^sub>W_branch_clauses:
@@ -180,13 +255,13 @@ lemma dpll\<^sub>W_branch_trail:
     \<open>dpll\<^sub>W_branch S T \<Longrightarrow> dpll\<^sub>W_all_inv (abs_state S) \<Longrightarrow> conflicting_clss S \<subseteq># conflicting_clss T\<close>
   subgoal
     by (induction rule: dpll\<^sub>W_branch.induct)
-     (auto simp: dpll\<^sub>W_all_inv_def state_def dest!: conflicting_clss_update_weight_information_mono)
+     (auto simp: dpll\<^sub>W_all_inv_def state dest!: conflicting_clss_update_weight_information_mono)
   subgoal
     by (induction rule: dpll\<^sub>W_branch.induct)
-     (auto simp: dpll\<^sub>W_all_inv_def state_def dest!: conflicting_clss_update_weight_information_mono)
+     (auto simp: dpll\<^sub>W_all_inv_def state dest!: conflicting_clss_update_weight_information_mono)
   subgoal
     by (induction rule: dpll\<^sub>W_branch.induct)
-      (auto simp: state_def conflicting_clss_def
+      (auto simp: state conflicting_clss_def
         dest!: conflicting_clss_update_weight_information_mono)
   done
 
@@ -236,12 +311,15 @@ proof -
     by (rule wf_subset)
 qed
 
-lemma state_tl_trail: \<open>state (tl_trail S) = (tl (trail S), clauses S, weight S)\<close>
-  by (auto simp: state_def)
+lemma state_tl_trail: \<open>state (tl_trail S) = (tl (trail S), clauses S, weight S, additional_info S)\<close>
+  by (auto simp: state)
 
-lemma state_tl_trail_comp_pow: \<open>state ((tl_trail ^^ n) S) = ((tl ^^ n) (trail S), clauses S, weight S)\<close>
-  by (induction n)
-   (auto simp: state_tl_trail state_def)
+lemma state_tl_trail_comp_pow: \<open>state ((tl_trail ^^ n) S) = ((tl ^^ n) (trail S), clauses S, weight S, additional_info S)\<close>
+  apply (induction n)
+    using state apply fastforce
+  apply (auto simp: state_tl_trail state)[]
+  done
+
 
 lemma [simp]:
   \<open>weight ((tl_trail ^^ n) S) = weight S\<close>
@@ -249,7 +327,7 @@ lemma [simp]:
   \<open>clauses ((tl_trail ^^ n) S) = clauses S\<close>
   \<open>conflicting_clss ((tl_trail ^^ n) S) = conflicting_clss S\<close>
   using state_tl_trail_comp_pow[of n S]
-  by (auto simp: state_def conflicting_clss_def)
+  by (auto simp: state conflicting_clss_def)
 
 lemma (in -)funpow_tl_append_skip_last:
   \<open>((tl ^^ length M') (M' @ M)) = M\<close>
@@ -270,13 +348,13 @@ lemma dpll\<^sub>W_core_Ex_propagate:
    Ex (dpll\<^sub>W_core S)"
   subgoal
     by (rule exI[of _ \<open>cons_trail (Propagated L ()) S\<close>])
-     (auto simp: dpll\<^sub>W_core.simps state'_def)
+     (fastforce simp: dpll\<^sub>W_core.simps state_eq_ref)
   subgoal
     by (rule exI[of _ \<open>cons_trail (Decided L) S\<close>])
      (auto simp: dpll\<^sub>W_core.simps state'_def)
   subgoal
     using backtrack_split_list_eq[of \<open>trail S\<close>, symmetric] apply -
-    apply (rule exI[of _ \<open>cons_trail (Propagated (-lit_of L') ()) ((tl_trail ^^ (length (L' # M'))) S)\<close>]))
+    apply (rule exI[of _ \<open>cons_trail (Propagated (-lit_of L') ()) ((tl_trail ^^ (length (L' # M'))) S)\<close>])
     apply (auto simp: dpll\<^sub>W_core.simps state'_def state_tl_trail_comp_pow funpow_tl_append_skip_last)
     done
   subgoal
@@ -327,5 +405,80 @@ proof -
     by (auto simp: dpll\<^sub>W.simps no_decide)
 qed
 
+(*TODO MOVE*)
+lemma (in -) backtrack_split_some_is_decided_then_snd_has_hd':
+  \<open>l\<in>set M \<Longrightarrow> is_decided l \<Longrightarrow> \<exists>M' L' M''. backtrack_split M = (M'', L' # M')\<close>
+  by (metis backtrack_snd_empty_not_decided list.exhaust prod.collapse)
+
+lemma (in -) total_over_m_entailed_or_conflict:
+  shows \<open>total_over_m M N \<Longrightarrow> M \<Turnstile>s N \<or> (\<exists>C \<in> N. M \<Turnstile>s CNot C)\<close>
+  by (metis Set.set_insert total_not_true_cls_true_clss_CNot total_over_m_empty total_over_m_insert true_clss_def)
+
+
+context
+  assumes can_always_improve:
+     \<open>\<And>S. trail S \<Turnstile>asm clauses S \<Longrightarrow> (\<forall>C \<in># conflicting_clss S. \<not> trail S \<Turnstile>as CNot C) \<Longrightarrow>
+       dpll\<^sub>W_all_inv (abs_state S) \<Longrightarrow>
+       total_over_m (lits_of_l (trail S)) (set_mset (clauses S)) \<Longrightarrow> Ex (dpll\<^sub>W_branch S)\<close>
+begin
+
+lemma no_step_dpll\<^sub>W_bnb_conflict:
+  assumes
+    ns: \<open>no_step dpll\<^sub>W_bnb S\<close> and
+    invs: \<open>dpll\<^sub>W_all_inv (abs_state S)\<close>
+  shows \<open>\<exists>C \<in># clauses S + conflicting_clss S. trail S \<Turnstile>as CNot C\<close> (is ?A) and
+      \<open>count_decided (trail S) = 0\<close>
+proof (rule ccontr)
+  have no_decide: \<open>atm_of L \<in> atms_of_mm (clauses S) \<Longrightarrow> defined_lit (trail S) L\<close> for L
+    using spec[OF ns, of \<open>cons_trail _ S\<close>]
+    apply (fastforce simp: dpll\<^sub>W_bnb.simps total_over_m_def total_over_set_def
+      dpll\<^sub>W_core.simps state'_def)
+    done
+  have tot: \<open>total_over_m (lits_of_l (trail S)) (set_mset (clauses S))\<close>
+    using no_decide
+    by (force simp: total_over_m_def total_over_set_def state'_def
+      Decided_Propagated_in_iff_in_lits_of_l)
+  have dec0: \<open>count_decided (trail S) = 0\<close> if ent: \<open>?A\<close>
+  proof -
+    obtain C where
+      \<open>C \<in># clauses S + conflicting_clss S\<close> and
+      \<open>trail S \<Turnstile>as CNot C\<close>
+      using ent tot ns invs
+      by (auto simp: dpll\<^sub>W_bnb.simps)
+    then show \<open>count_decided (trail S) = 0\<close>
+      using ns  dpll\<^sub>W_core_Ex_backtrack[of S _ _ _ C]  dpll\<^sub>W_core_Ex_backtrack_opt[of S _ _ _ C]
+      unfolding count_decided_0_iff
+      apply clarify
+      apply (frule backtrack_split_some_is_decided_then_snd_has_hd'[of _ \<open>trail S\<close>], assumption)
+     apply (auto simp: dpll\<^sub>W_bnb.simps count_decided_0_iff)
+     apply (metis backtrack_split_snd_hd_decided list.sel(1) list.simps(3) snd_conv)+
+     done
+   qed
+
+  show False if \<open>\<not>?A\<close>
+  proof -
+    have \<open>trail S \<Turnstile>a C\<close> if \<open>C \<in># clauses S + conflicting_clss S\<close> for C
+    proof -
+      have \<open>\<not> trail S \<Turnstile>as CNot C\<close>
+        using \<open>\<not>?A\<close> that by (auto dest!: multi_member_split)
+      then show \<open>?thesis\<close>
+        using tot that
+        total_not_true_cls_true_clss_CNot[of \<open>lits_of_l (trail S)\<close> C]
+          apply (auto simp: true_annots_def simp del: true_clss_def_iff_negation_in_model  dest!: multi_member_split )
+          using true_annot_def apply blast
+          using true_annot_def apply blast
+        by (metis Decided_Propagated_in_iff_in_lits_of_l atms_of_clauses_conflicting_clss atms_of_ms_union
+          in_m_in_literals no_decide set_mset_union that true_annot_def true_cls_add_mset)
+    qed
+    then have \<open>trail S \<Turnstile>asm clauses S + conflicting_clss S\<close>
+      by (auto simp: true_annots_def  dest!: multi_member_split )
+    then show ?thesis
+      using can_always_improve[of S] \<open>\<not>?A\<close> tot invs ns by (auto simp: dpll\<^sub>W_bnb.simps)
+  qed
+  then show \<open>count_decided (trail S) = 0\<close>
+    using dec0 by blast
+qed
+
+end
 
 end
