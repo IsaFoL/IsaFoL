6ecee26c53e2f0feb22a9de45a4966f89f0ab087
Sophie Tourret <sophie.tourret@mpi-inf.mpg.de>
2020-10-23 16:15:11 +0200

    splitting framework: minor progresses

--- Splitting_Framework/Preliminaries.thy
+++ Splitting_Framework/Preliminaries.thy
@@ -4,7 +4,7 @@
 theory Preliminaries
   imports Saturation_Framework.Calculus
 begin
-
+  
 
 locale consequence_relation =
   fixes
@@ -17,10 +17,13 @@ locale consequence_relation =
     entails_each: "M \<Turnstile> N \<Longrightarrow> (\<forall>D\<in>N. M \<union> {D} \<Turnstile> P) \<Longrightarrow> M \<Turnstile> P"
 begin
 
+lemma entails_cond_reflexive: \<open>N \<noteq> {} \<Longrightarrow> N \<Turnstile> N\<close>
+  using entails_reflexive entails_subsets by (meson bot.extremum from_nat_into insert_subset)
+
 definition entails_conjunctive :: "'f set \<Rightarrow> 'f set \<Rightarrow> bool" (infix "\<Turnstile>\<inter>" 50) where
   "M \<Turnstile>\<inter> N \<equiv> \<forall>C\<in>N. M \<Turnstile> {C}"
 
-  sublocale Calculus.consequence_relation "{bot}" "(\<Turnstile>\<inter>)"
+sublocale Calculus.consequence_relation "{bot}" "(\<Turnstile>\<inter>)"
 proof
   show "{bot} \<noteq> {}" by simp
 next
@@ -28,17 +31,31 @@ next
   assume b_in: "B \<in> {bot}"
   then have b_is: "B = bot" by simp
   show "{B} \<Turnstile>\<inter> N"
-  proof (cases "N = {}")
-    assume n_empty: "N = {}"
-    then show "{B} \<Turnstile>\<inter> N"
-      unfolding entails_conjunctive_def by blast
-  next
-    assume n_full: "N \<noteq> {}"
-    then obtain C where c_in: "C \<in> N" by blast
-    have "{B} \<Turnstile> {C}" sorry
-    then show "{B} \<Turnstile>\<inter> N"
-      unfolding entails_conjunctive_def using c_in b_is bot_entails_empty entails_subsets by blast
+    unfolding entails_conjunctive_def using entails_each[OF bot_entails_empty] b_is by fast
+next
+  fix M N
+  assume m_subs: "(M :: 'f set) \<subseteq> N"
+  show \<open>N \<Turnstile>\<inter> M\<close> unfolding entails_conjunctive_def
+  proof
+    fix C
+    assume "C \<in> M"
+    then have c_subs: \<open>{C} \<subseteq> N\<close> using m_subs by fast
+    show \<open>N \<Turnstile> {C}\<close> using entails_subsets[OF c_subs _ entails_reflexive[of C]] by simp 
   qed
+next
+  fix M N
+  assume \<open>\<forall>C\<in>M. N \<Turnstile>\<inter> {C}\<close>
+  then show \<open>N \<Turnstile>\<inter> M\<close>
+    unfolding entails_conjunctive_def by blast
+next
+  fix M N P
+  assume
+    trans1: \<open>M \<Turnstile>\<inter> N\<close> and
+    trans2: \<open>N \<Turnstile>\<inter> P\<close>
+  show \<open>M \<Turnstile>\<inter> P\<close>
+    unfolding entails_conjunctive_def
+
+
 oops
 
 end
