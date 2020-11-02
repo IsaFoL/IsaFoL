86f0d56c87ee4e926b388702815400e5195a759a
Sophie Tourret <sophie.tourret@mpi-inf.mpg.de>
2020-11-02 17:05:02 +0100

    splitting: new D4

--- Splitting_Framework/Preliminaries.thy
+++ Splitting_Framework/Preliminaries.thy
@@ -2,7 +2,8 @@
 * Author:       Sophie Tourret <stourret at mpi-inf.mpg.de>, 2020 *)
 
 theory Preliminaries
-  imports Saturation_Framework.Calculus
+imports Saturation_Framework.Calculus
+  (* Finite_Set *)
 begin
   
 
@@ -14,14 +15,29 @@ locale consequence_relation =
     bot_entails_empty: "{bot} \<Turnstile> {}" and
     entails_reflexive: "{C} \<Turnstile> {C}" and
     entails_subsets: "M \<subseteq> N \<Longrightarrow> P \<subseteq> Q \<Longrightarrow> M \<Turnstile> P \<Longrightarrow> N \<Turnstile> Q" and
-    entails_each: "M \<Turnstile> N \<Longrightarrow> (\<forall>D\<in>N. M \<union> {D} \<Turnstile> P) \<Longrightarrow> M \<Turnstile> P"
+    entails_each: "M \<Turnstile> P \<Longrightarrow> \<forall>C\<in>M. N \<Turnstile> Q \<union> {C} \<Longrightarrow> \<forall>D\<in>P. N \<union> {D} \<Turnstile> Q \<Longrightarrow> N \<Turnstile> Q"
+    (* this was an earlier version of entails_each: "M \<Turnstile> N \<Longrightarrow> (\<forall>D\<in>N. M \<union> {D} \<Turnstile> P) \<Longrightarrow> M \<Turnstile> P"
+    it was detected to be unsufficient thanks to the forma*)
 begin
 
 lemma entails_cond_reflexive: \<open>N \<noteq> {} \<Longrightarrow> N \<Turnstile> N\<close>
   using entails_reflexive entails_subsets by (meson bot.extremum from_nat_into insert_subset)
+    
+(* This lemma shows that an entailment such that {} \<Turnstile> {} is useless, it may or may not help better
+    understand what this entailment is depending on who you ask ^_^' *)
+lemma entails_empty_reflexive_dangerous: \<open>{} \<Turnstile> {} \<Longrightarrow> M \<Turnstile> N\<close>
+  using entails_subsets[of "{}" M "{}" N] by simp
 
 definition entails_conjunctive :: "'f set \<Rightarrow> 'f set \<Rightarrow> bool" (infix "\<Turnstile>\<inter>" 50) where
   "M \<Turnstile>\<inter> N \<equiv> \<forall>C\<in>N. M \<Turnstile> {C}"
+
+(* lemma \<open>M \<union> N \<Turnstile> {C} \<Longrightarrow> M \<Turnstile>\<inter> N \<Longrightarrow> \<forall>C' \<in> N. M \<union> {C'} \<Turnstile> {C}\<close>
+ * proof (cases "N = {}" "N = {C}")
+ *   assume \<open>N = {}\<close>
+ *   then show \<open>\<forall>C' \<in> N. M \<union> {C'} \<Turnstile> {C}\<close> by simp
+ * next
+ *   assume \<open>N \<noteq> {}\<close>
+ *     then show *)
 
 sublocale Calculus.consequence_relation "{bot}" "(\<Turnstile>\<inter>)"
 proof
@@ -31,7 +47,8 @@ next
   assume b_in: "B \<in> {bot}"
   then have b_is: "B = bot" by simp
   show "{B} \<Turnstile>\<inter> N"
-    unfolding entails_conjunctive_def using entails_each[OF bot_entails_empty] b_is by fast
+    unfolding entails_conjunctive_def
+    using entails_subsets[of "{B}" "{B}" "{}"] b_is bot_entails_empty by blast
 next
   fix M N
   assume m_subs: "(M :: 'f set) \<subseteq> N"
@@ -52,8 +69,14 @@ next
   assume
     trans1: \<open>M \<Turnstile>\<inter> N\<close> and
     trans2: \<open>N \<Turnstile>\<inter> P\<close>
-  show \<open>M \<Turnstile>\<inter> P\<close>
-    unfolding entails_conjunctive_def
+  show \<open>M \<Turnstile>\<inter> P\<close> unfolding entails_conjunctive_def
+  proof
+    fix C
+    assume \<open>C \<in> P\<close>
+    then have n_to_c: \<open>N \<Turnstile> {C}\<close> using trans2 unfolding entails_conjunctive_def by simp
+    have m_c_to_c: "M \<union> {C} \<Turnstile> {C}" using entails_subsets[OF _ _ entails_reflexive[of C], of "M \<union> {C}" "{C}"] by fast
+    then show \<open>M \<Turnstile> {C}\<close>
+      using entails_each[OF n_to_c, of M "{C}"] trans1 unfolding entails_conjunctive_def 
 
 
 oops
