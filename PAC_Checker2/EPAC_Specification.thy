c11b39897366ee1254f0392de0681ed30e239c0e
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2020-11-02 10:01:04 +0100

    start PAC_Checker2

---
+++ PAC_Checker2/EPAC_Specification.thy
@@ -0,0 +1,11 @@
+(*
+  File:         PAC_Specification.thy
+  Author:       Mathias Fleury, Daniela Kaufmann, JKU
+  Maintainer:   Mathias Fleury, JKU
+*)
+theory EPAC_Specification
+imports PAC_Checker.PAC_More_Poly
+  PAC_Checker.PAC_Specification
+begin
+
+end
---
+++ PAC_Checker2/EPAC_Version.thy
@@ -0,0 +1,25 @@
+(*
+  File:         PAC_Version.thy
+  Author:       Mathias Fleury, Daniela Kaufmann, JKU
+  Maintainer:   Mathias Fleury, JKU
+*)
+theory EPAC_Version
+  imports Main
+begin
+
+text \<open>This code was taken from IsaFoR. However, for the AFP, we use the version name \<^text>\<open>AFP\<close>,
+instead of a mercurial version. \<close>
+local_setup \<open>
+  let
+    val version =
+      trim_line (#1 (Isabelle_System.bash_output ("cd $ISAFOL/ && git rev-parse --short HEAD || echo unknown")))
+  in
+    Local_Theory.define
+      ((\<^binding>\<open>version\<close>, NoSyn),
+        ((\<^binding>\<open>version_def\<close>, []), HOLogic.mk_literal version)) #> #2
+  end
+\<close>
+
+declare version_def [code]
+
+end
