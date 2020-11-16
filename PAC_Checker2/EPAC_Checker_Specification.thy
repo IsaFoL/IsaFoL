f3b2856e388b0ecff4fe039127794ae30a80afe2
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2020-11-16 19:41:46 +0100

    PAC: prove LC

--- PAC_Checker2/EPAC_Checker_Specification.thy
+++ PAC_Checker2/EPAC_Checker_Specification.thy
@@ -23,15 +23,105 @@ on ideals and the first version of the loop.
 
 subsection \<open>Algorithm\<close>
 
+datatype ('a, 'b, 'lbls) pac_step =
+  CL (pac_srcs: \<open>('a \<times> 'lbls) list\<close>) (new_id: 'lbls) (pac_res: 'a) |
+  Extension (new_id: 'lbls) (new_var: 'b) (pac_res: 'a) |
+  Del (pac_src1: 'lbls)
+
+definition check_linear_comb :: \<open>(nat, int mpoly) fmap \<Rightarrow> nat set \<Rightarrow> (int mpoly \<times> nat) list \<Rightarrow> nat \<Rightarrow> int mpoly \<Rightarrow> bool nres\<close> where
+  \<open>check_linear_comb \<A> \<V> xs n r = SPEC(\<lambda>b. b \<longrightarrow> (\<forall>i \<in> set xs. snd i \<in># dom_m \<A> \<and> vars (fst i) \<subseteq> \<V>) \<and> n \<notin># dom_m \<A> \<and>
+  vars r \<subseteq> \<V> \<and> xs \<noteq> [] \<and> (\<Sum>(p,n) \<in>#mset xs. the (fmlookup \<A> n) * p) - r \<in> ideal polynomial_bool)\<close>
+
+lemma PAC_Format_LC:
+  assumes
+    i: \<open>((\<V>, A), \<V>\<^sub>B, B) \<in> polys_rel_full\<close> and
+    st: \<open>PAC_Format\<^sup>*\<^sup>* (\<V>\<^sub>0, A\<^sub>0) (\<V>, B)\<close> and
+    vars: \<open>\<forall>i\<in>#x11. snd i \<in># dom_m A \<and> vars (fst i) \<subseteq> \<V>\<close> and
+    AV: \<open>\<Union> (vars ` set_mset (ran_m A)) \<subseteq> \<V>\<close> and
+    fin: \<open>x11 \<noteq> {#}\<close> and
+    r: \<open>(\<Sum>x\<in>#x11. case x of (p, n) \<Rightarrow> the (fmlookup A n) * p) - r \<in> More_Modules.ideal polynomial_bool\<close>
+    \<open>vars r \<subseteq> \<V>\<close>
+  shows \<open>PAC_Format\<^sup>*\<^sup>* (\<V>, B) (\<V>, add_mset r B)\<close>
+proof -
+  have AB: \<open>i \<in># dom_m A \<Longrightarrow> the (fmlookup A i) \<in># B\<close> and BA: \<open>B = ran_m A\<close> for i
+    using i by (auto simp: polys_rel_full_def polys_rel_def)
+  have \<open>PAC_Format\<^sup>*\<^sup>* (\<V>, B) (\<V>, add_mset ((\<Sum>x\<in>#x11. case x of (p, n) \<Rightarrow> the (fmlookup A n) * p)) B)\<close>
+    using fin vars
+  proof (induction x11)
+    case empty
+    then show ?case by auto
+  next
+    case (add x F)
+    then have IH: \<open>F \<noteq> {#} \<Longrightarrow> PAC_Format\<^sup>*\<^sup>* (\<V>, B) (\<V>, add_mset (\<Sum>(p,n)\<in>#F. the (fmlookup A n) * p) B)\<close> and
+      x_A: \<open>snd x \<in># dom_m A\<close> and
+      x_var: \<open>vars (fst x) \<subseteq> \<V>\<close> and
+      x_in: \<open>the (fmlookup A (snd x)) \<in># B\<close>
+      using AB[of \<open>snd x\<close>] by auto
+    have vars_A: \<open>vars (the (fmlookup A (snd x))) \<subseteq> \<V>\<close>
+      using AV x_A
+      by (auto simp: ran_m_def)
+    let ?B = \<open>(add_mset (\<Sum>(p,n)\<in>#F. the (fmlookup A n) * p) B)\<close>
+    let ?p = \<open>(\<Sum>(p,n)\<in>#F. the (fmlookup A n) * p)\<close>
+    let ?q = \<open>(\<Sum>(p,n)\<in>#{#x#}. the (fmlookup A n) * p)\<close>
+    let ?vars = \<open>\<lambda>A. \<Union> (vars ` set_mset (A)) \<subseteq> \<V>\<close>
+    consider
+      (empty) \<open>F = {#}\<close> |
+      (nempty) \<open>F \<noteq> {#}\<close>
+      by blast
+    then show ?case
+    proof cases
+      case empty2: empty
+      have \<open>PAC_Format (\<V>, B) (\<V>, add_mset ((\<Sum>x\<in>#{#x#}. case x of (p, n) \<Rightarrow> the (fmlookup A n) * p)) B)\<close>
+        apply (cases x)
+        apply (rule PAC_Format.intros(2)[OF x_in, of \<open>fst x\<close>])
+        by (use x_var vars_A in \<open>auto simp: ideal.span_zero elim!: vars_unE\<close>)
+      then show ?thesis
+        using empty2 by auto
+    next
+      case nempty
+      then have IH: \<open>PAC_Format\<^sup>*\<^sup>* (\<V>, B) (\<V>, add_mset ?p B)\<close>
+        using IH by auto
+      from rtranclp_PAC_Format_subset_ideal[OF this] have vars2: \<open>?vars ?B\<close>
+        using AV unfolding BA[symmetric] by auto
+      have 1:
+        \<open>PAC_Format (\<V>, ?B) (\<V>, add_mset (the (fmlookup A (snd x)) * fst x) ?B)\<close> (is \<open>PAC_Format _ (_, ?C)\<close>)
+        apply (cases x)
+        apply (rule PAC_Format.intros(2)[of \<open>the (fmlookup A (snd x))\<close> _ \<open>fst x\<close>])
+        by (use x_in x_var vars_A in \<open>auto simp: ideal.span_zero elim!: vars_unE\<close>)
+      from PAC_Format_subset_ideal[OF this] have \<open>?vars (add_mset (the (fmlookup A (snd x)) * fst x) ?B)\<close>
+        using vars2 by auto
+      have 2: \<open>PAC_Format (\<V>, ?C) (\<V>, add_mset (\<Sum>(p,n)\<in>#add_mset x F. the (fmlookup A n) * p) ?C)\<close>  (is \<open>PAC_Format _ (_, ?D)\<close>)
+        apply (cases x)
+        apply (rule PAC_Format.intros(1)[of \<open>?p\<close> _ ?q])
+        by (use insert x_in x_var vars_A vars2 in \<open>auto simp: ideal.span_zero elim!: in_vars_addE vars_unE\<close>)
+      then have 3:  \<open>PAC_Format\<^sup>*\<^sup>* (\<V>, ?D) (\<V>, add_mset (\<Sum>(p,n)\<in># add_mset x F. the (fmlookup A n) * p) B)\<close>
+        using  PAC_Format.del[of ?p ?D \<V>]
+          PAC_Format.del[of \<open>the (fmlookup A (snd x)) * fst x\<close> \<open>remove1_mset ?p ?D\<close> \<V>]
+        by (auto 4 7)
+      show ?thesis
+        using IH 1 2 3 by auto
+    qed
+  qed
+  moreover have \<open>PAC_Format (\<V>, add_mset (\<Sum>(p,n)\<in>#x11. the (fmlookup A n) * p) B)
+    (\<V>, add_mset r (add_mset (\<Sum>(p,n)\<in>#x11. the (fmlookup A n) * p) B))\<close> (is \<open>PAC_Format _ ?E\<close>)
+    by (rule PAC_Format.intros(2)[of \<open>(\<Sum>(p,n)\<in>#x11. the (fmlookup A n) * p)\<close> _ 1])
+      (use r in auto)
+  moreover  have \<open>PAC_Format ?E (\<V>, add_mset r B)\<close>
+    using  PAC_Format.del[of \<open>(\<Sum>(p,n)\<in>#x11. the (fmlookup A n) * p)\<close> \<open>snd ?E\<close> \<open>fst ?E\<close>]
+   by auto
+  ultimately show ?thesis
+    using st by auto
+qed
+
 definition PAC_checker_step
   ::  \<open>int_poly \<Rightarrow> (status \<times> fpac_step) \<Rightarrow> (int_poly, nat, nat) pac_step \<Rightarrow>
     (status \<times> fpac_step) nres\<close>
 where
   \<open>PAC_checker_step = (\<lambda>spec (stat, (\<V>, A)) st. case st of
-     Add _ _ _ _ \<Rightarrow>
+     CL _ _ _ \<Rightarrow>
        do {
          r \<leftarrow> normalize_poly_spec (pac_res st);
-        eq \<leftarrow> check_add A \<V> (pac_src1 st) (pac_src2 st) (new_id st) r;
+        eq \<leftarrow> check_linear_comb A \<V> (pac_srcs st) (new_id st) r;
         st' \<leftarrow> SPEC(\<lambda>st'. (\<not>is_failed st' \<and> is_found st' \<longrightarrow> r - spec \<in> ideal polynomial_bool));
         if eq
         then RETURN (merge_status stat st', \<V>, fmupd (new_id st) r A)
@@ -42,17 +132,6 @@ where
         eq \<leftarrow> check_del A (pac_src1 st);
         if eq
         then RETURN (stat, (\<V>, fmdrop (pac_src1 st) A))
-        else RETURN (FAILED, (\<V>, A))
-   }
-   | Mult _ _ _ _ \<Rightarrow>
-       do {
-         r \<leftarrow> normalize_poly_spec (pac_res st);
-         q \<leftarrow> normalize_poly_spec (pac_mult st);
-        eq \<leftarrow> check_mult A \<V> (pac_src1 st) q (new_id st) r;
-        st' \<leftarrow> SPEC(\<lambda>st'. (\<not>is_failed st' \<and> is_found st' \<longrightarrow> r - spec \<in> ideal polynomial_bool));
-        if eq
-        then RETURN (merge_status stat st',
-          \<V>, fmupd (new_id st) r A)
         else RETURN (FAILED, (\<V>, A))
    }
    | Extension _ _ _ \<Rightarrow>
@@ -66,6 +145,7 @@ where
         else RETURN (FAILED, (\<V>, A))
    }
  )\<close>
+
 lemma PAC_checker_step_PAC_checker_specification2:
   fixes a :: \<open>status\<close>
   assumes AB: \<open>((\<V>, A),(\<V>\<^sub>B, B)) \<in> polys_rel_full\<close> and
@@ -137,30 +217,22 @@ proof -
          intro: diff_in_polynomial_bool_pac_idealI simp: H2')
 
    have H3': \<open>the (fmlookup A x12) * q - r \<in> More_Modules.ideal polynomial_bool \<Longrightarrow>
-          r - spec \<in> More_Modules.ideal polynomial_bool \<Longrightarrow>
+          spec - r \<in> More_Modules.ideal polynomial_bool \<Longrightarrow>
           r \<in> pac_ideal (insert (the (fmlookup A x12)) ((\<lambda>x. the (fmlookup A x)) ` set_mset Aa))\<close>
      for Aa x12 r q
      by (metis (no_types, lifting) ab_semigroup_mult_class.mult.commute diff_in_polynomial_bool_pac_idealI
        ideal.span_base pac_idealI3 set_image_mset set_mset_add_mset_insert union_single_eq_member)
-
-  have H3: \<open>x12 \<in># dom_m A \<Longrightarrow>
-       the (fmlookup A x12) * q - r \<in> More_Modules.ideal polynomial_bool \<Longrightarrow>
-       r - spec \<in> More_Modules.ideal polynomial_bool \<Longrightarrow>
-       spec \<in> pac_ideal (set_mset B)\<close> for x12 r q
-     using \<open>(A,B) \<in> polys_rel\<close>
-      ideal.span_add[OF ideal.span_add[OF ideal.span_neg ideal.span_neg,
-         of \<open>the (fmlookup A x12)\<close> _  \<open>the (fmlookup A x12)\<close>],
-      of \<open>set_mset B \<union> polynomial_bool\<close> \<open>2 * the (fmlookup A x12) - r\<close>]
-     unfolding polys_rel_def
-     by (subgoal_tac \<open>r \<in> pac_ideal (set_mset B)\<close>)
-       (auto dest!: multi_member_split simp: ran_m_def H3'
-         intro: diff_in_polynomial_bool_pac_idealI)
 
   have [intro]: \<open>spec \<in> pac_ideal (set_mset B) \<Longrightarrow> spec \<in> pac_ideal (set_mset A\<^sub>0)\<close> and
     vars_B: \<open>\<Union> (vars ` set_mset B) \<subseteq> \<V>\<close>and
     vars_B: \<open>\<Union> (vars ` set_mset (ran_m A)) \<subseteq> \<V>\<close>
     using rtranclp_PAC_Format_subset_ideal[OF A\<^sub>0B  vars_A\<^sub>0] spec\<^sub>0 \<open>(A, B) \<in> polys_rel\<close>[unfolded polys_rel_def, simplified]
     by (smt in_mono mem_Collect_eq restricted_ideal_to_def)+
+  have spec_found: \<open>PAC_Format\<^sup>*\<^sup>* (\<V>\<^sub>0, A\<^sub>0) (V, add_mset r B) \<Longrightarrow>
+    r - spec \<in> ideal polynomial_bool \<Longrightarrow> spec \<in> pac_ideal (set_mset A\<^sub>0)\<close> for V B r
+    using rtranclp_PAC_Format_subset_ideal[of \<V>\<^sub>0 A\<^sub>0 V \<open>add_mset r B\<close>]  vars_A\<^sub>0 spec\<^sub>0
+    by (smt diff_in_polynomial_bool_pac_idealI2 in_mono mem_Collect_eq restricted_ideal_to_def
+      rtranclp_PAC_Format_subset_ideal union_single_eq_member)
 
   have eq_successI: \<open>st' \<noteq> FAILED \<Longrightarrow>
        st' \<noteq> FOUND \<Longrightarrow> st' = SUCCESS\<close> for st'
@@ -178,33 +250,24 @@ proof -
   note [[goals_limit=1]]
   show ?thesis
     unfolding PAC_checker_step_def PAC_checker_specification_step_spec_def
-      normalize_poly_spec_alt_def check_mult_def check_add_def
-      check_extension_def polys_rel_full_def
+      normalize_poly_spec_alt_def 
+      check_extension_def polys_rel_full_def check_linear_comb_def
     apply (cases st)
     apply clarsimp_all
-    subgoal for x11 x12 x13 x14
+    subgoal for x11 x12 x13
       apply (refine_vcg lhs_step_If)
       subgoal for r eqa st'
-        using assms vars_B apply -
+        using assms vars_B PAC_Format_LC[OF assms(1), of \<V>\<^sub>0 A\<^sub>0 \<open>mset x11\<close> r]
+          spec_found[of \<V> r B] rtranclp_trans[of PAC_Format \<open>(\<V>\<^sub>0, A\<^sub>0)\<close> \<open>(\<V>, B)\<close> \<open>(\<V>, add_mset r B)\<close>]
+        apply -
         apply (rule RETURN_SPEC_refine)
         apply (rule_tac x = \<open>(merge_status a st',\<V>,add_mset r B)\<close> in exI)
-        by (auto simp: polys_rel_update_remove ran_m_mapsto_upd_notin
-          intro: PAC_Format_add_and_remove H2 dest: rtranclp_PAC_Format_subset_ideal)
+        apply (auto simp: polys_rel_update_remove ran_m_mapsto_upd_notin
+          intro: PAC_Format_add_and_remove dest: rtranclp_PAC_Format_subset_ideal)
+        done
       subgoal
         by (rule RETURN_SPEC_refine)
          (auto simp: Ex_status_iff dest: rtranclp_PAC_Format_subset_ideal)
-      done
-    subgoal for x11 x12 x13 x14
-      apply (refine_vcg lhs_step_If)
-      subgoal for r q eqa st'
-        using assms vars_B apply -
-        apply (rule RETURN_SPEC_refine)
-        apply (rule_tac x = \<open>(merge_status a st',\<V>,add_mset r B)\<close> in exI)
-        by (auto intro: polys_rel_update_remove intro: PAC_Format_add_and_remove(3-) H3
-           dest: rtranclp_PAC_Format_subset_ideal)
-      subgoal
-        by (rule RETURN_SPEC_refine)
-          (auto simp: Ex_status_iff)
       done
     subgoal for x31 x32 x34
       apply (refine_vcg lhs_step_If)
@@ -233,7 +296,7 @@ proof -
             dest!: eq_successI
             split: if_splits
             dest: rtranclp_PAC_Format_subset_ideal
-            intro: PAC_Format_add_and_remove H3)
+            intro: PAC_Format_add_and_remove)
           done
         subgoal
           apply (rule_tac x = \<open>(a,\<V>, B)\<close> in exI)
