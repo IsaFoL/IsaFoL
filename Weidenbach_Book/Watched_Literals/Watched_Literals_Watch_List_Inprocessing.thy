c452d291e74fecd957c5b4f2c8eb17ac28f2335e
Mathias Fleury <mfleury@mpi-inf.mpg.de>
2021-04-19 20:00:27 +0200

    more proofs

--- Weidenbach_Book/Watched_Literals/Watched_Literals_Watch_List_Inprocessing.thy
+++ Weidenbach_Book/Watched_Literals/Watched_Literals_Watch_List_Inprocessing.thy
@@ -1,5 +1,6 @@
 theory Watched_Literals_Watch_List_Inprocessing
   imports Watched_Literals_Watch_List Watched_Literals_List_Simp
+    Watched_Literals_Watch_List_Restart
 begin
 
 definition simplify_clause_with_unit_st_wl :: \<open>nat \<Rightarrow> 'v twl_st_wl \<Rightarrow> 'v twl_st_wl nres\<close> where
@@ -26,10 +27,7 @@ definition simplify_clause_with_unit_st_wl :: \<open>nat \<Rightarrow> 'v twl_st
 
 definition simplify_clauses_with_unit_st_wl_pre where
   \<open>simplify_clauses_with_unit_st_wl_pre S \<longleftrightarrow> (\<exists>T.
-  (S, T) \<in> state_wl_l None \<and>
-  set_mset (dom_m (get_clauses_wl S)) \<subseteq>
-    clauses_pointed_to (set_mset (all_init_lits_of_wl S))
-    (get_watched_wl S) \<and>
+  (S, T) \<in> state_wl_l None \<and> no_lost_clause_in_WL S \<and>
   simplify_clauses_with_unit_st_pre T)\<close>
 
 definition simplify_clauses_with_unit_st_wl_inv where
@@ -37,9 +35,7 @@ definition simplify_clauses_with_unit_st_wl_inv where
   (S\<^sub>0, T\<^sub>0) \<in> state_wl_l None \<and>
   (S, T) \<in> state_wl_l None \<and>
   simplify_clauses_with_unit_st_inv T\<^sub>0 it T \<and>
-  set_mset (dom_m (get_clauses_wl S)) \<subseteq>
-  clauses_pointed_to (set_mset (all_init_lits_of_wl S))
-  (get_watched_wl S))\<close>
+  no_lost_clause_in_WL S)\<close>
 
 definition simplify_clauses_with_unit_st_wl :: \<open>'v twl_st_wl \<Rightarrow> 'v twl_st_wl nres\<close>  where
   \<open>simplify_clauses_with_unit_st_wl S =
@@ -61,14 +57,11 @@ lemma clauses_pointed_to_mono: \<open>A \<subseteq> B \<Longrightarrow> clauses_
  
 lemma simplify_clause_with_unit_st_wl_simplify_clause_with_unit_st:
   assumes ST: \<open>(S, T) \<in> state_wl_l None\<close> and \<open>(i,j) \<in> nat_rel\<close> and
-    point: \<open>set_mset (dom_m (get_clauses_wl S)) \<subseteq>
-      clauses_pointed_to (set_mset (all_init_lits_of_wl S))
-      (get_watched_wl S)\<close>
+    point: \<open>no_lost_clause_in_WL S\<close>
   shows
-  \<open>simplify_clause_with_unit_st_wl i S \<le> \<Down> {(S,T). (S,T) \<in> state_wl_l None \<and>
-    set_mset (dom_m (get_clauses_wl S)) \<subseteq>
-    clauses_pointed_to (set_mset (all_init_lits_of_wl S))
-    (get_watched_wl S)}
+  \<open>simplify_clause_with_unit_st_wl i S \<le> \<Down> {(S',T). (S',T) \<in> state_wl_l None \<and>
+    no_lost_clause_in_WL S' \<and>
+    get_watched_wl S' = get_watched_wl S}
   (simplify_clause_with_unit_st j T)\<close>
 proof -
   have Id: \<open>A = B \<Longrightarrow> A \<le> \<Down>Id B\<close> for A B
@@ -81,6 +74,18 @@ proof -
     ({#mset (fst x). x \<in># (init_clss_l b)#} + d + f + h)\<close> for C D d b f h j
     by (auto simp: image_mset_remove1_mset_if ran_m_def
       dest!: multi_member_split)
+  have KK[simp]: \<open>irred b j \<Longrightarrow> j \<in># dom_m b \<Longrightarrow>  C \<subseteq># mset (b \<propto> j) \<Longrightarrow>
+    set_mset (all_lits_of_mm (add_mset (mset (b \<propto> j))
+    (add_mset C
+    (mset `# remove1_mset (b \<propto> j) (init_clss_lf b) + d + f + h)))) =
+    set_mset (all_lits_of_mm (mset `# (init_clss_lf b) + d + f + h))\<close>
+    for b j C d f h
+    using all_lits_of_m_mono[of C \<open>mset (b \<propto> j)\<close>]
+    by (auto simp: image_mset_remove1_mset_if ran_m_def conj_disj_distribR Collect_disj_eq
+      image_Un Collect_conv_if all_lits_of_mm_add_mset
+      simp flip: insert_compr
+      dest!: multi_member_split[of j])
+ 
   have H: \<open>fmdrop j x2 = fmdrop j b \<Longrightarrow>
     mset (x2 \<propto> j) \<subseteq># mset (b \<propto> j) \<Longrightarrow>
     irred x2 j \<Longrightarrow>
@@ -100,8 +105,9 @@ proof -
       dest: all_lits_of_m_mono
       intro!: image_mset_cong2 filter_mset_cong2)
     apply (metis fmdrop_eq_update_eq fmupd_lookup union_single_eq_member)
-    by (metis add_mset_remove_trivial dom_m_fmdrop) 
-
+    by (metis add_mset_remove_trivial dom_m_fmdrop)
+   have [simp]: \<open>mset a \<subseteq># mset b \<Longrightarrow> length a= 1 \<Longrightarrow> a ! 0 \<in> set b\<close> for a b
+     by (cases a, auto)
   show ?thesis
     supply [[goals_limit=1]]
     using ST point
@@ -119,25 +125,27 @@ proof -
       subgoal by auto
       subgoal by auto
       subgoal by (auto simp: all_init_lits_of_wl_def init_clss_l_fmdrop
-        init_clss_l_fmdrop_irrelev
+        init_clss_l_fmdrop_irrelev literals_are_\<L>\<^sub>i\<^sub>n'_def blits_in_\<L>\<^sub>i\<^sub>n'_def
+        no_lost_clause_in_WL_def
         dest: in_diffD)
       subgoal by auto
       subgoal apply (auto simp: all_init_lits_of_wl_def init_clss_l_fmdrop
         init_clss_l_fmdrop_irrelev add_mset_commute
+        no_lost_clause_in_WL_def
         dest: in_diffD)
         apply (subst add_mset_commute)
-        apply (auto simp: all_lits_of_mm_add_mset clauses_pointed_to_union
+        by (auto simp: all_lits_of_mm_add_mset clauses_pointed_to_union
           dest: in_diffD)
-        done
       subgoal by auto
       subgoal by (auto simp: all_init_lits_of_wl_def init_clss_l_fmdrop
         init_clss_l_fmdrop_irrelev all_lits_of_mm_add_mset
+        no_lost_clause_in_WL_def
         dest: in_diffD)
       subgoal for a b c d e f g h i ja k aa ba ca da ea fa ga ha ia jaa ka x x' x1 x2 x1a x2a
         
-        apply
-        (auto simp: all_init_lits_of_wl_def init_clss_l_fmdrop
-        init_clss_l_fmdrop_irrelev H
+        apply (auto simp: all_init_lits_of_wl_def init_clss_l_fmdrop
+          init_clss_l_fmdrop_irrelev H
+          no_lost_clause_in_WL_def
         dest: in_diffD
           intro: )
         apply (metis (no_types, lifting) basic_trans_rules(31) dom_m_fmdrop insert_DiffM) 
@@ -145,47 +153,81 @@ proof -
       done
 qed
 
+lemma [twl_st_wl, simp]:
+  assumes \<open>(\<sigma>, \<sigma>') \<in> state_wl_l None\<close>
+  shows
+    all_learned_lits_of_l_all_learned_lits_of_wl:
+      \<open>all_learned_lits_of_l \<sigma>' = all_learned_lits_of_wl \<sigma>\<close> and
+    all_init_lits_of_l_all_init_lits_of_wl:
+      \<open>all_init_lits_of_l \<sigma>' = all_init_lits_of_wl \<sigma>\<close>
+  using assms by (auto simp: state_wl_l_def all_learned_lits_of_l_def
+    all_learned_lits_of_wl_def all_init_lits_of_l_def
+    all_init_lits_of_wl_def)
+
 
 lemma simplify_clauses_with_unit_st_wl_simplify_clause_with_unit_st:
   assumes ST: \<open>(S, T) \<in> state_wl_l None\<close> and
-    point: \<open>set_mset (dom_m (get_clauses_wl S)) \<subseteq>
-    clauses_pointed_to (set_mset (all_init_lits_of_wl S))
-    (get_watched_wl S)\<close>
+    point: \<open>correct_watching'' S\<close> and
+    lits: \<open>literals_are_\<L>\<^sub>i\<^sub>n' S\<close>
   shows
-    \<open>simplify_clauses_with_unit_st_wl S \<le> \<Down> {(S,T). (S,T) \<in> state_wl_l None \<and>
-    set_mset (dom_m (get_clauses_wl S)) \<subseteq>
-    clauses_pointed_to (set_mset (all_init_lits_of_wl S))
-    (get_watched_wl S)}
+    \<open>simplify_clauses_with_unit_st_wl S \<le> \<Down> {(S',T). (S',T) \<in> state_wl_l None \<and>
+    no_lost_clause_in_WL S' \<and>
+    literals_are_\<L>\<^sub>i\<^sub>n' S' \<and> get_watched_wl S' = get_watched_wl S}
     (simplify_clauses_with_unit_st T)\<close>
 proof -
   have [refine0]: \<open>inj_on id xs\<close> for xs
     by auto
+  have 1: \<open>simplify_clauses_with_unit_st_wl S = do {
+    T \<leftarrow> simplify_clauses_with_unit_st_wl S;
+    RETURN T}\<close>
+    by auto
+  have 2: \<open>simplify_clauses_with_unit_st T = do {
+    T \<leftarrow> simplify_clauses_with_unit_st T;
+    RETURN T}\<close>
+    by auto
+  have ST2: \<open>(S,T) \<in>  {(S',U). (S',U) \<in> state_wl_l None \<and>
+    no_lost_clause_in_WL S' \<and>
+    get_watched_wl S = get_watched_wl S'}\<close>
+    if \<open>simplify_clauses_with_unit_st_pre T\<close>
+    using assms correct_watching''_clauses_pointed_to0[OF ST] that
+    unfolding simplify_clauses_with_unit_st_inv_def
+      simplify_clauses_with_unit_st_pre_def
+    by auto
   show ?thesis
+    apply (subst 1)
     unfolding simplify_clauses_with_unit_st_wl_def simplify_clauses_with_unit_st_def
-    apply (refine_vcg simplify_clause_with_unit_st_wl_simplify_clause_with_unit_st)
+      nres_monad3
+    apply (refine_rcg simplify_clause_with_unit_st_wl_simplify_clause_with_unit_st)
     subgoal
-      using assms unfolding simplify_clauses_with_unit_st_wl_pre_def
+      using assms ST2 unfolding simplify_clauses_with_unit_st_wl_pre_def
       by blast
     subgoal
       using ST by auto
-    subgoal
-      by auto
-    subgoal
-      using assms unfolding simplify_clauses_with_unit_st_wl_pre_def by auto
-    subgoal
-      by auto
+      subgoal by auto
+        apply (rule ST2, assumption)
+    subgoal by auto
     subgoal for xs xsa it \<sigma> it' \<sigma>'
       using assms apply -
       unfolding simplify_clauses_with_unit_st_wl_inv_def
       apply (rule exI[of _ T])
       apply (rule exI[of _ \<sigma>'])
       by auto
+    apply (rule_tac T1=\<sigma>' and j1 = x' in
+        simplify_clause_with_unit_st_wl_simplify_clause_with_unit_st[THEN order_trans])
     subgoal
       by auto
     subgoal
       by auto
     subgoal
       by auto
+    subgoal
+      by (rule conc_fun_R_mono)
+        (use assms(3) in \<open>auto simp: literals_are_\<L>\<^sub>i\<^sub>n'_def
+        blits_in_\<L>\<^sub>i\<^sub>n'_def\<close>)
+    subgoal
+      using ST lits
+      by (auto 4 3 simp: literals_are_\<L>\<^sub>i\<^sub>n'_def watched_by_alt_def
+        blits_in_\<L>\<^sub>i\<^sub>n'_def)
     done
 qed
 
