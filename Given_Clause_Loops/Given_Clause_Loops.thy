018755af4b85385ad9f06f4bdf2da28a7ba088f6
Jasmin Blanchette <j.c.blanchette@vu.nl>
2022-01-26 12:07:48 +0100

    reformatting formalization

--- Given_Clause_Loops/Given_Clause_Loops.thy
+++ Given_Clause_Loops/Given_Clause_Loops.thy
@@ -1,6 +1,8 @@
-(* Title:        Main file importing all given clause loops
- * Author:       Jasmin Blanchette <j.c.blanchette at vu.nl>, 2022
- * Maintainer:   Sophie Tourret <stourret at loria.fr>, 2022 *)
+(* Title:        Given Clause Loops
+   Author:       Jasmin Blanchette <j.c.blanchette at vu.nl>, 2022
+   Maintainer:   Sophie Tourret <stourret at loria.fr>, 2022 *)
+
+section \<open>Given Clause Loops\<close>
 
 theory Given_Clause_Loops
   imports
--- Given_Clause_Loops/More_Lazy_Given_Clause.thy
+++ Given_Clause_Loops/More_Lazy_Given_Clause.thy
@@ -1,103 +1,103 @@
-(* Title:        Extension of the lazy_given_clause locale from Saturation_Framework
- * Author:       Qi Qiu, 2021
- * Maintainer:   Sophie Tourret <stourret at loria.fr>, 2021 *)
+(* Title:        More Lazy Given Clause Lemmas
+   Authors:      Qi Qiu, 2021
+                 Jasmin Blancherte <j.c.blanchette at vu.nl>, 2022
+   Maintainer:   Sophie Tourret <stourret at loria.fr>, 2021 *)
 
-section \<open>6 basic operations covered by lazy_given_clause.process rule\<close>
+section \<open>More Lazy Given Clause Lemmas\<close>
 
 theory More_Lazy_Given_Clause
   imports
     More_Given_Clause_Basis
     Saturation_Framework.Given_Clause_Architectures
-begin (* theory *)
+begin
 
 context lazy_given_clause
-begin (* context lazy_given_clause *)
+begin
 
-subsection \<open>abbreviation, definition , type etc.\<close>
+lemma P0':
+  assumes "(C, l) \<in> Red_F \<N>"
+  shows "(T, \<N> \<union> {(C, l)}) \<leadsto>LGC (T, \<N>)"
+proof -
+  have "{(C, l)} \<subseteq> Red_F (\<N> \<union> \<emptyset>)"
+    using assms by simp
+  moreover have "active_subset \<emptyset> = \<emptyset>"
+    using active_subset_def by simp
+  ultimately show "(T, \<N> \<union> {(C, l)}) \<leadsto>LGC (T, \<N>)"
+    by (metis process sup_bot_right)
+qed
 
-subsection \<open>6 basic operations\<close>
+lemma P1':
+  assumes "C \<in> no_labels.Red_F (fst ` \<N>)"
+  shows "(T, \<N> \<union> {(C, l::'l)}) \<leadsto>LGC (T, \<N>)"
+proof -
+  have "(C, l) \<in> Red_F \<N>"
+    using lemma59point1 assms by simp
+  then show "(T, \<N> \<union> {(C, l::'l)}) \<leadsto>LGC (T, \<N>)"
+    using P0' by auto
+qed
 
-  lemma P0' :
-    assumes "(C,l) \<in> Red_F \<N>" 
-    shows "(T, \<N> \<union> {(C,l)}) \<leadsto>LGC (T, \<N>)"
-  proof-
-    have "{(C,l)} \<subseteq> Red_F (\<N> \<union> \<emptyset>)"
-      using assms by simp
-    moreover have "active_subset \<emptyset> = \<emptyset>" 
-      using active_subset_def by simp
-    ultimately show "(T, \<N> \<union> {(C,l)}) \<leadsto>LGC (T, \<N>)"
-      by (metis process sup_bot_right) 
-  qed
+lemma P2':
+  assumes "l \<noteq> active"
+  shows "(T, \<N>) \<leadsto>LGC (T, \<N> \<union> {(C, l)})"
+proof -
+  have active_subset_C_l: "active_subset {(C, l)} = \<emptyset>"
+    using active_subset_def assms by simp
+  also have "\<emptyset> \<subseteq> Red_F (\<N> \<union> {(C, l)})"
+    by simp
+  finally show "(T, \<N>) \<leadsto>LGC (T, \<N> \<union> {(C, l)})"
+    by (metis active_subset_C_l process sup_bot.right_neutral)
+qed
 
-  lemma P1' : 
-    assumes " C \<in> no_labels.Red_F (fst ` \<N>)"
-    shows   "(T, \<N> \<union> {(C,l::'l)}) \<leadsto>LGC (T, \<N>)"
-  proof-
-    have "(C,l) \<in> Red_F \<N>" 
-      using lemma59point1 assms by simp
-    then show "(T, \<N> \<union> {(C,l::'l)}) \<leadsto>LGC (T, \<N>)" 
-      using P0' by auto
-  qed
-
-  lemma P2' : 
-    assumes "l \<noteq> active"
-    shows   "(T, \<N>) \<leadsto>LGC (T, \<N> \<union> {(C,l)})"
-  proof-
-    have active_subset_C_l : "active_subset {(C,l)} = \<emptyset>" 
-      using active_subset_def assms by simp
-    also have "\<emptyset> \<subseteq> Red_F (\<N> \<union> {(C,l)})" 
-      by simp
-    finally show "(T, \<N>) \<leadsto>LGC (T, \<N> \<union> {(C,l)})"
-      by (metis active_subset_C_l process sup_bot.right_neutral)
-  qed
-
-  lemma P3' : 
-    assumes "(C',l') \<in> \<N>" and 
-            "C' \<prec>\<cdot> C"
-    shows "(T, \<N> \<union> {(C,l)}) \<leadsto>LGC (T, \<N>)"
-  proof-
-    have " C' \<in> fst ` \<N> "
-      by (metis assms(1) fst_conv rev_image_eqI)
-    then have "{(C,l)} \<subseteq> Red_F (\<N>)"
-      using assms lemma59point2 by auto
-    then show ?thesis 
-      using P0' by simp
-  qed
+lemma P3':
+  assumes
+    "(C', l') \<in> \<N>" and
+    "C' \<prec>\<cdot> C"
+  shows "(T, \<N> \<union> {(C, l)}) \<leadsto>LGC (T, \<N>)"
+proof -
+  have " C' \<in> fst ` \<N> "
+    by (metis assms(1) fst_conv rev_image_eqI)
+  then have "{(C, l)} \<subseteq> Red_F (\<N>)"
+    using assms lemma59point2 by auto
+  then show ?thesis
+    using P0' by simp
+qed
 
 
-  lemma P4' : 
-    assumes "(C',l') \<in> \<N>" and
-            "l' \<sqsubset>L l" and
-            "C' \<preceq>\<cdot> C"
-    shows   "(T, \<N> \<union> {(C,l)}) \<leadsto>LGC (T, \<N>)"
-  proof-
-    have " (C,l) \<in> Red_F \<N> "
-      using assms lemma59point3 by auto
-    then show "(T, \<N> \<union> {(C,l)}) \<leadsto>LGC (T, \<N>)" 
-      using P0' by auto
-  qed
+lemma P4':
+  assumes
+    "(C', l') \<in> \<N>" and
+    "l' \<sqsubset>L l" and
+    "C' \<preceq>\<cdot> C"
+  shows "(T, \<N> \<union> {(C, l)}) \<leadsto>LGC (T, \<N>)"
+proof -
+  have " (C, l) \<in> Red_F \<N> "
+    using assms lemma59point3 by auto
+  then show "(T, \<N> \<union> {(C, l)}) \<leadsto>LGC (T, \<N>)"
+    using P0' by auto
+qed
 
-  lemma P5' : 
-    assumes "l' \<sqsubset>L l" and
-            "l' \<noteq> active"
-    shows   "(T, \<N> \<union> {(C,l)}) \<leadsto>LGC (T, \<N> \<union> {(C,l')})"
-  proof-
-    have active_subset_c_l' : "active_subset {(C,l')} = \<emptyset>" 
-      using active_subset_def assms by auto
-    
-    have "C \<doteq> C "
-      by (simp add: equiv_equiv_F equivp_reflp)
-    moreover have "(C,l') \<in> \<N> \<union> {(C,l')} "
-      by auto
-    ultimately have "(C,l) \<in> Red_F (\<N> \<union> {(C,l')})"
-      using assms lemma59point3[of _ _ "\<N> \<union> {(C,l')}"] by auto
-    then have "{(C,l)} \<subseteq> Red_F (\<N> \<union> {(C,l')})" 
-      by auto
+lemma P5':
+  assumes
+    "l' \<sqsubset>L l" and
+    "l' \<noteq> active"
+  shows "(T, \<N> \<union> {(C, l)}) \<leadsto>LGC (T, \<N> \<union> {(C, l')})"
+proof -
+  have active_subset_c_l': "active_subset {(C, l')} = \<emptyset>"
+    using active_subset_def assms by auto
 
-    then show "(T, \<N> \<union> {(C,l)}) \<leadsto>LGC (T, \<N> \<union> {(C,l')})"
-      using active_subset_c_l' process[of _ _ "{(C,l)}" _ "{(C,l')}"] by auto
-  qed
+  have "C \<doteq> C "
+    by (simp add: equiv_equiv_F equivp_reflp)
+  moreover have "(C, l') \<in> \<N> \<union> {(C, l')} "
+    by auto
+  ultimately have "(C, l) \<in> Red_F (\<N> \<union> {(C, l')})"
+    using assms lemma59point3[of _ _ "\<N> \<union> {(C, l')}"] by auto
+  then have "{(C, l)} \<subseteq> Red_F (\<N> \<union> {(C, l')})"
+    by auto
 
-end(* context lazy_given_clause *)
+  then show "(T, \<N> \<union> {(C, l)}) \<leadsto>LGC (T, \<N> \<union> {(C, l')})"
+    using active_subset_c_l' process[of _ _ "{(C, l)}" _ "{(C, l')}"] by auto
+qed
 
-end (* theory *)
+end
+
+end
