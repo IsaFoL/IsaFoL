5c53085cc9b0565040519c6acc2e60eb5f2d2ab5
Jasmin Blanchette <j.c.blanchette@vu.nl>
2022-01-26 11:13:45 +0100

    added main file

---
+++ Given_Clause_Loops/Given_Clause_Loops.thy
@@ -0,0 +1,13 @@
+(* Title:        Main file importing all given clause loops
+ * Author:       Jasmin Blanchette <j.c.blanchette at vu.nl>, 2022
+ * Maintainer:   Sophie Tourret <stourret at loria.fr>, 2022 *)
+
+theory Given_Clause_Loops
+  imports
+    OL_in_GC
+    DL_in_LGC
+    IL_in_GC
+    IL_in_GC_from_OL
+    ZL_in_LGC
+begin
+end
