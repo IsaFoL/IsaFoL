0132a45255467e8a632cfaefb3ed7893a4f2edd5
Jasmin Blanchette <j.c.blanchette@vu.nl>
2022-07-26 14:35:31 +0200

    more ideas

--- Given_Clause_Loops/Loop_Fairness_Basis.thy
+++ Given_Clause_Loops/Loop_Fairness_Basis.thy
@@ -1,5 +1,5 @@
 theory Loop_Fairness_Basis
-  imports Main
+  imports Coinductive.Coinductive_List
 begin
 
 type_synonym 'f strategy = "'f set \<Rightarrow> 'f set"
@@ -7,11 +7,8 @@ type_synonym 'f strategy = "'f set \<Rightarrow> 'f set"
 definition is_strategy_legal :: "'f strategy \<Rightarrow> bool" where
   "is_strategy_legal stgy \<longleftrightarrow> (\<forall>P. stgy P \<subseteq> P) \<and> (\<forall>P. P \<noteq> {} \<longrightarrow> stgy P \<noteq> {})"
 
-inductive strategy_step :: "'f strategy \<Rightarrow> 'f set \<Rightarrow> 'f set \<Rightarrow> 'f set \<Rightarrow> bool"
-    for stgy :: "'f strategy" where
-  "C \<in> stgy P \<Longrightarrow> strategy_step stgy P (P - {C}) ((P - {C}) \<union> N)"
-
 definition is_strategy_fair :: "'f strategy \<Rightarrow> bool" where
-  "is_strategy_fair stgy \<longleftrightarrow> "
+  "is_strategy_fair stgy \<longleftrightarrow>
+   (\<forall>C Ps i. ((\<forall>j \<ge> i. C \<in> lnth Ps j) \<longrightarrow> (\<exists>j \<ge> i. C \<in> stgy (lnth Ps j))))"
 
 end
