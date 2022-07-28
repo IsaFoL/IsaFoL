bb96682c422f71b3757ebb77e02e7360fa05757c
Jasmin Blanchette <j.c.blanchette@vu.nl>
2022-07-28 16:24:01 +0200

    more proof

--- Given_Clause_Loops/Loop_Fairness_Basis.thy
+++ Given_Clause_Loops/Loop_Fairness_Basis.thy
@@ -15,6 +15,11 @@ theory Loop_Fairness_Basis
 begin
 
 
+subsection \<open>Setup\<close>
+
+declare fset_of_list.rep_eq [simp]
+
+
 subsection \<open>Passive Set\<close>
 
 text \<open>The passive set of a given clause prover can be organized in different
@@ -33,11 +38,11 @@ locale passive_struct =
     "formulas (add Cs P) = fset_of_list Cs |\<union>| formulas P"
 begin
 
-text \<open>The assumption that the selected formula is not immediately readded can be fulfilled by
+text \<open>The assumption that the added formulas do not belong to the passive set can be fulfilled by
 annotating formulas with timestamps.\<close>
 
 inductive step :: "'p \<Rightarrow> 'p \<Rightarrow> bool" where
-  stepI: "fst (select P) \<notin> set Cs \<Longrightarrow> step P (add Cs (snd (select P)))"
+  stepI: "set Cs \<inter> fset (formulas P) = {} \<Longrightarrow> step P (add Cs (snd (select P)))"
 
 definition is_struct_fair :: bool where
   "is_struct_fair \<longleftrightarrow> (\<forall>Ps. full_chain step Ps \<longrightarrow> Liminf_llist (lmap (fset \<circ> formulas) Ps) = {})"
@@ -57,11 +62,21 @@ next
     by (simp add: funion_commute)
 qed
 
+lemma chain_fifo_step_distinct_formulas:
+  assumes
+    ps_chain: "chain fifo.step Ps" and
+    i_lt: "enat i < llength Ps"
+  shows "distinct (lnth Ps i)"
+  sorry
+
 lemma fifo_is_struct_fair: "fifo.is_struct_fair TYPE('f)"
   unfolding fifo.is_struct_fair_def
 proof (intro allI impI)
   fix Ps :: "'f list llist"
   assume ps_full: "full_chain fifo.step Ps"
+
+  have ps_chain: "chain fifo.step Ps"
+    by (rule full_chain_imp_chain[OF ps_full])
 
   show "Liminf_llist (lmap (fset \<circ> fset_of_list) Ps) = {}"
   proof (rule ccontr)
@@ -70,7 +85,7 @@ proof (intro allI impI)
     obtain i :: nat where
       i_lt: "enat i < llength Ps" and
       inter_ne: "\<Inter> ((set \<circ> lnth Ps) ` {j. i \<le> j \<and> enat j < llength Ps}) \<noteq> {}"
-      using lim_ne unfolding Liminf_llist_def by (auto simp: fset_of_list.rep_eq)
+      using lim_ne unfolding Liminf_llist_def by auto
 
     from inter_ne obtain C :: 'f where
       c_in: "\<forall>P \<in> lnth Ps ` {j. i \<le> j \<and> enat j < llength Ps}. C \<in> set P"
@@ -100,8 +115,55 @@ proof (intro allI impI)
         by simp
     qed
 
-    show False
+    have c_in'': "\<forall>j. i \<le> j \<longrightarrow> C \<in> set (lnth Ps j)"
+      by (simp add: c_in' ps_inf)
+    then obtain k :: nat where
+      k_lt: "k < length (lnth Ps i)" and
+      at_k: "lnth Ps i ! k = C"
+      by (meson in_set_conv_nth le_refl)
+
+    have
+      ij_bnd: "k - j < length (lnth Ps (i + j))" and
+      at_kmj: "lnth Ps (i + j) ! (k - j) = C"
+      if i_le: "j \<le> k" for j
+      apply -
       sorry
+    have
+      ik_bnd: "length (lnth Ps (i + k)) > 0" and
+      at_0: "lnth Ps (i + k) ! 0 = C"
+      using ij_bnd[of k] at_kmj[of k] by simp+
+    from at_0 have c_at_hd_ik:"hd (lnth Ps (i + k)) = C"
+      using ik_bnd by (simp add: hd_conv_nth)
+
+    have step: "fifo.step (lnth Ps (i + k)) (lnth Ps (i + k + 1))"
+      using full_chain_lnth_rel[OF ps_full]
+      by (metis (full_types) Suc_eq_plus1 enat_ord_code(4) ps_inf)
+
+    have "C \<notin> set (lnth Ps (i + k + 1))"
+    proof -
+      obtain Cs and P where
+        at_ik: "lnth Ps (i + k) = P" and
+        at_sik: "lnth Ps (i + k + 1) = tl P @ Cs" and
+        inter: "set Cs \<inter> fset (fset_of_list P) = {}"
+        using step[simplified fifo.step.simps] by auto
+
+      have "length P > 0"
+        using at_ik ik_bnd by auto
+      moreover have "hd P = C"
+        using c_at_hd_ik at_ik by auto
+      moreover have "distinct (lnth Ps (i + k))"
+        using chain_fifo_step_distinct_formulas[OF ps_chain] by (simp add: ps_inf)
+      ultimately have c_ni_tl: "C \<notin> set (tl P)"
+        using at_ik by (metis distinct.simps(2) hd_Cons_tl length_greater_0_conv)
+
+      have c_ni_cs: "C \<notin> set Cs"
+        using inter at_0 at_ik ik_bnd by auto
+
+      thus ?thesis
+        using c_ni_tl c_ni_cs at_sik by simp
+    qed
+    thus False
+      using c_in'' by auto
   qed
 qed
 
