51536c08c81752e43307ce3b0aa3ae563fd2cd37
Jasmin Blanchette <j.c.blanchette@vu.nl>
2022-08-01 11:26:48 +0200

    compile

--- Given_Clause_Loops/Otter_Loop_Fairness.thy
+++ Given_Clause_Loops/Otter_Loop_Fairness.thy
@@ -5,8 +5,7 @@ begin
 type_synonym ('p, 'f) fair_OL_state = "'f set \<times> 'f set \<times> 'p \<times> 'f set \<times> 'f set"
 
 locale fair_otter_loop =
-  otter_loop Bot_F Inf_F Bot_G Q entails_q Inf_G_q Red_I_q Red_F_q \<G>_F_q \<G>_I_q Equiv_F Prec_F
-    OL_Prec_L Active New XX Passive YY +
+  otter_loop Bot_F Inf_F Bot_G Q entails_q Inf_G_q Red_I_q Red_F_q \<G>_F_q \<G>_I_q Equiv_F Prec_F +
   passive_set empty select add remove fformulas
   for
     Bot_F :: "'f set" and
@@ -25,7 +24,11 @@ locale fair_otter_loop =
     select :: "'p \<Rightarrow> 'f" and
     add :: "'f \<Rightarrow> 'p \<Rightarrow> 'p" and
     remove :: "'f \<Rightarrow> 'p \<Rightarrow> 'p" and
-    fformulas :: "'p \<Rightarrow> 'f fset"
+    fformulas :: "'p \<Rightarrow> 'f fset" +
+  fixes
+    Prec_S :: "'f \<Rightarrow> 'f \<Rightarrow> bool" (infix "\<prec>S" 50)
+  assumes
+    wf_prec_S: "minimal_element (\<prec>S) UNIV"
 begin
 
 
@@ -37,15 +40,15 @@ where
   choose_n: "C \<notin> N \<Longrightarrow> (N \<union> {C}, {}, P, {}, A) \<leadsto>OLf (N, {C}, P, {}, A)"
 | delete_fwd: "C \<in> no_labels.Red_F (formulas P \<union> A) \<or> (\<exists>C' \<in> formulas P \<union> A. C' \<preceq>\<cdot> C) \<Longrightarrow>
     (N, {C}, P, {}, A) \<leadsto>OLf (N, {}, P, {}, A)"
-| simplify_fwd: "C \<in> no_labels.Red_F (formulas P \<union> A \<union> {C'}) \<Longrightarrow>
+| simplify_fwd: "C' \<prec>S S \<Longrightarrow> C \<in> no_labels.Red_F (formulas P \<union> A \<union> {C'}) \<Longrightarrow>
     (N, {C}, P, {}, A) \<leadsto>OLf (N, {C'}, P, {}, A)"
 | delete_bwd_p: "C' \<in> formulas P \<Longrightarrow> C' \<in> no_labels.Red_F {C} \<or> C \<prec>\<cdot> C' \<Longrightarrow>
     (N, {C}, P, {}, A) \<leadsto>OLf (N, {C}, remove C' P, {}, A)"
-| simplify_bwd_p: "C' \<in> formulas P \<Longrightarrow> C' \<in> no_labels.Red_F {C, C''} \<Longrightarrow>
+| simplify_bwd_p: "C' \<prec>S S \<Longrightarrow> C' \<in> formulas P \<Longrightarrow> C' \<in> no_labels.Red_F {C, C''} \<Longrightarrow>
     (N, {C}, P, {}, A) \<leadsto>OLf (N \<union> {C''}, {C}, remove C' P, {}, A)"
 | delete_bwd_a: "C' \<in> no_labels.Red_F {C} \<or> C \<prec>\<cdot> C' \<Longrightarrow>
     (N, {C}, P, {}, A \<union> {C'}) \<leadsto>OLf (N, {C}, P, {}, A)"
-| simplify_bwd_a: "C' \<in> no_labels.Red_F {C, C''} \<Longrightarrow>
+| simplify_bwd_a: "C' \<prec>S S \<Longrightarrow> C' \<in> no_labels.Red_F {C, C''} \<Longrightarrow>
     (N, {C}, P, {}, A \<union> {C'}) \<leadsto>OLf (N \<union> {C''}, {C}, P, {}, A)"
 | transfer: "(N, {C}, P, {}, A) \<leadsto>OLf (N, {}, add C P, {}, A)"
 | choose_p: "({}, {}, P, {}, A) \<leadsto>OLf ({}, {}, remove (select P) P, {select P}, A)"
