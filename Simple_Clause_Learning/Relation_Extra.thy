8b3552fa5f0ca0624912b4ee4481261e2009d5fd
Martin Desharnais <desharnais@mpi-inf.mpg.de>
2022-10-12 11:42:57 +0200

    Add lemmas to Relation_Extra

--- Simple_Clause_Learning/Relation_Extra.thy
+++ Simple_Clause_Learning/Relation_Extra.thy
@@ -2,7 +2,12 @@ theory Relation_Extra
   imports Main
 begin
 
-term totalp
+lemma totalp_on_insert:
+  "totalp_on (insert a A) R \<longleftrightarrow> (\<forall>b \<in> A. a \<noteq> b \<longrightarrow> R a b \<or> R b a) \<and> totalp_on A R"
+  by (auto simp add: totalp_on_def)
+
+lemma reflclp_ident_if_reflp: "reflp R \<Longrightarrow> R\<^sup>=\<^sup>= = R"
+  by (auto dest: reflpD)
 
 lemma reflp_reflclp[simp]: "reflp R\<^sup>=\<^sup>="
   by (simp add: reflp_def)
