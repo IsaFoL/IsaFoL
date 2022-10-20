3f2c5670b04546d8cc5f925ba1705e8d71790ffb
Mathias Fleury <Mathias.Fleury@ens-rennes.fr>
2022-10-20 18:43:49 +0200

    more attempts to do pairing heaps

--- Weidenbach_Book/IsaSAT/IsaSAT_VSIDS.thy
+++ Weidenbach_Book/IsaSAT/IsaSAT_VSIDS.thy
@@ -20,13 +20,17 @@ But maybe pointers are actually better, because by definition in Isabelle no gra
 
 fun mset_nodes :: "('b, 'a) hp \<Rightarrow>'b multiset" where
 "mset_nodes (Hp x _ hs) = {#x#} + sum_mset(mset(map mset_nodes hs))"
+
+datatype 'a pairing_heap = PHeap (ph_score: 'a) (ph_contained: bool) (ph_children: \<open>nat list\<close>)
+
 (*TODO we need a field to say contained or not*)
 inductive encoded_pairheap where
-  empty: \<open>encoded_pairheap [] {#}\<close> |
-  leaf: \<open>encoded_pairheap (arr[n := (x, [])]) (add_mset (Hp n x []) trees)\<close>
+  empty: \<open>encoded_pairheap arr {#}\<close> if \<open>ph_contained ` set arr \<subseteq> {False}\<close> |
+  leaf: \<open>encoded_pairheap (arr[n := PHeap x intree []]) (add_mset (Hp n x []) trees)\<close>
   if  \<open>encoded_pairheap arr trees\<close> \<open>n \<notin># \<Sum>\<^sub># (mset_nodes `# trees)\<close> and
-    \<open>n < length arr\<close>|
-  comb: \<open>encoded_pairheap (arr[n := (fst (arr!n), snd (arr!n)@[m])]) (add_mset (Hp n w\<^sub>n (Hp m w\<^sub>m child\<^sub>m # child\<^sub>n)) trees)\<close>
+    \<open>n < length arr\<close>
+    \<open>intree\<close> |
+  comb: \<open>encoded_pairheap (arr[n := PHeap (ph_score (arr!n)) (ph_contained (arr!n)) (ph_children (arr!n)@[m])]) (add_mset (Hp n w\<^sub>n (Hp m w\<^sub>m child\<^sub>m # child\<^sub>n)) trees)\<close>
   if  \<open>encoded_pairheap arr (add_mset (Hp n w\<^sub>n (child\<^sub>n)) (add_mset (Hp m w\<^sub>m child\<^sub>m) trees))\<close>
 
 lemma encoded_pairheap_distinct_nodes: \<open>encoded_pairheap arr trees \<Longrightarrow> distinct_mset (\<Sum>\<^sub># (mset_nodes `# trees))\<close>
@@ -34,9 +38,11 @@ lemma encoded_pairheap_distinct_nodes: \<open>encoded_pairheap arr trees \<Longr
    (auto simp: ac_simps)
 
 lemma encoded_pairheap_change_irrelevant:
-  \<open>encoded_pairheap arr trees \<Longrightarrow> n \<notin># \<Sum>\<^sub># (mset_nodes `# trees) \<Longrightarrow> encoded_pairheap (arr [n := a]) trees\<close>
+  \<open>encoded_pairheap arr trees \<Longrightarrow> \<not>ph_contained a \<Longrightarrow> n < length arr \<Longrightarrow>
+  n \<notin># \<Sum>\<^sub># (mset_nodes `# trees) \<Longrightarrow> encoded_pairheap (arr [n := a]) trees\<close>
   apply (induction rule: encoded_pairheap.induct)
-  subgoal by (auto intro: encoded_pairheap.intros)
+  subgoal
+    by (rule encoded_pairheap.empty) (auto elim: in_set_upd_cases)
   subgoal for arr trees na x
     using encoded_pairheap.leaf[of \<open>arr[n := a]\<close> trees na x]
     by (auto simp add: list_update_swap)
@@ -89,7 +95,7 @@ lemma in_sum_mset_nodes_iff: \<open>(\<exists>m a. Hp n m a \<in>#  \<Sum>\<^sub
 lemma encoded_pairheap_correct_annot:
   assumes 1: \<open>encoded_pairheap arr trees\<close> and
     \<open>Hp n m a \<in>#  \<Sum>\<^sub># (nodes `# trees)\<close>
-  shows \<open>(arr ! n) = (m, rev (map node a))\<close>
+  shows \<open>(arr ! n) = PHeap m True (rev (map node a))\<close>
 proof -
   have 2: \<open>n \<in># \<Sum>\<^sub># (mset_nodes `# trees)\<close>
     by (rule in_sum_mset_nodes_iff[THEN iffD1])
@@ -111,7 +117,12 @@ proof -
       apply (metis in_sum_mset_nodes_iff sum_image_mset_sum_map)
       apply (metis in_sum_mset_nodes_iff sum_image_mset_sum_map)
       apply (simp add: union_assoc union_lcomm)
+      apply (metis in_sum_mset_nodes_iff sum_image_mset_sum_map)
+      apply (metis in_sum_mset_nodes_iff sum_image_mset_sum_map)
+      apply (metis in_sum_mset_nodes_iff sum_image_mset_sum_map)
       apply (metis (no_types, lifting) fst_conv union_assoc union_lcomm)
+      apply (metis (no_types, lifting) pairing_heap.sel(1) union_assoc union_lcomm)
+      apply (metis pairing_heap.collapse pairing_heap.inject union_commute union_lcomm)
       apply (metis image_mset_single in_sum_mset_nodes_iff sum_mset.singleton)
       by (metis (no_types, lifting) union_assoc union_lcomm)
    done
@@ -121,8 +132,8 @@ definition vsids_link where
   \<open>vsids_link arr m n = do {
      ASSERT (m < length arr);
      ASSERT (n < length arr);
-     if fst (arr ! n) \<le> fst (arr ! m) then RETURN (arr[m := (fst (arr ! m), snd (arr ! m) @ [n])])
-     else RETURN (arr [n := (fst (arr ! n), snd (arr ! n) @ [m])])
+     if ph_score (arr ! n) \<le> ph_score (arr ! m) then RETURN (arr[m := PHeap (ph_score (arr ! m)) True (ph_children (arr ! m) @ [n])])
+     else RETURN (arr [n := PHeap (ph_score (arr ! n)) True (ph_children (arr ! n) @ [m])])
 }\<close>
 
 
@@ -151,15 +162,15 @@ qed
 definition vsids_insert where
   \<open>vsids_insert m n arr = do {
     ASSERT (n < length arr);
-    vsids_link (arr[n:= (fst (arr!n), [])]) m n
+    vsids_link (arr[n:= PHeap (ph_score (arr!n)) True []]) m n
   }\<close>
 
 lemma vsids_insert:
   assumes \<open>encoded_pairheap arr {#Hp m w\<^sub>m children#}\<close> \<open>n \<notin># mset_nodes (Hp m w\<^sub>m children)\<close> \<open>n < length arr\<close>
-  shows \<open>vsids_insert m n arr \<le> SPEC (\<lambda>arr'. encoded_pairheap arr' {#the (VSIDS.insert n (fst (arr!n)) (Some (Hp m w\<^sub>m children)))#}) \<close>
+  shows \<open>vsids_insert m n arr \<le> SPEC (\<lambda>arr'. encoded_pairheap arr' {#the (VSIDS.insert n (ph_score (arr!n)) (Some (Hp m w\<^sub>m children)))#}) \<close>
 proof -
-  have 1: \<open>encoded_pairheap (arr[n:= (fst (arr!n), [])]) {#Hp n (fst (arr!n)) [], Hp m w\<^sub>m children#}\<close>
-    using assms encoded_pairheap.leaf[OF assms(1), of n \<open>(fst (arr!n))\<close>]
+  have 1: \<open>encoded_pairheap (arr[n:= PHeap (ph_score (arr!n)) True []]) {#Hp n (ph_score (arr!n)) [], Hp m w\<^sub>m children#}\<close>
+    using assms encoded_pairheap.leaf[OF assms(1), of n True \<open>ph_score (arr!n)\<close>]
     by (auto intro!: )
   show ?thesis
     unfolding vsids_insert_def
@@ -170,6 +181,162 @@ proof -
 qed
 
 
+definition vsids_link2 where
+  \<open>vsids_link2 arr m n = do {
+     ASSERT (m < length arr);
+     ASSERT (n < length arr);
+     if ph_score (arr ! n) \<le> ph_score (arr ! m) then RETURN (arr[m := PHeap (ph_score (arr ! m)) True (ph_children (arr ! m) @ [n])], m)
+     else RETURN (arr [n := PHeap (ph_score (arr ! n)) True (ph_children (arr ! n) @ [m])], n)
+}\<close>
+
+lemma vsids_link2:
+  assumes vsids: \<open>encoded_pairheap arr (add_mset (Hp n w\<^sub>n child\<^sub>n) (add_mset (Hp m w\<^sub>m child\<^sub>m) trees))\<close>
+  shows \<open>vsids_link2 arr m n \<le> SPEC (\<lambda>(arr', k). k = node (VSIDS.link (Hp n w\<^sub>n child\<^sub>n) (Hp m w\<^sub>m child\<^sub>m))  \<and>
+    encoded_pairheap arr' (add_mset (VSIDS.link (Hp n w\<^sub>n child\<^sub>n) (Hp m w\<^sub>m child\<^sub>m)) trees))\<close>
+proof -
+  show ?thesis
+    unfolding vsids_link2_def
+    apply (refine_vcg if_refine)
+    subgoal using encoded_pairheap_le_lengthI[OF vsids, of m] by auto
+    subgoal using encoded_pairheap_le_lengthI[OF vsids, of n] by auto
+    subgoal
+      using encoded_pairheap.comb[OF vsids[unfolded add_mset_commute[of \<open>Hp n _ _\<close>]]]
+        encoded_pairheap_correct_annot[OF vsids, of n w\<^sub>n child\<^sub>n]
+        encoded_pairheap_correct_annot[OF vsids, of m w\<^sub>m child\<^sub>m]
+      by (auto intro: encoded_pairheap.intros)
+    subgoal
+      using encoded_pairheap.comb[OF vsids[unfolded add_mset_commute[of \<open>Hp n _ _\<close>]]]
+        encoded_pairheap_correct_annot[OF vsids, of n w\<^sub>n child\<^sub>n]
+        encoded_pairheap_correct_annot[OF vsids, of m w\<^sub>m child\<^sub>m]
+      by (auto intro: encoded_pairheap.intros)
+    subgoal
+      using encoded_pairheap.comb[OF vsids]
+        encoded_pairheap_correct_annot[OF vsids, of n w\<^sub>n child\<^sub>n]
+        encoded_pairheap_correct_annot[OF vsids, of m w\<^sub>m child\<^sub>m]
+      by (auto intro: encoded_pairheap.intros)
+    subgoal
+      using encoded_pairheap.comb[OF vsids]
+        encoded_pairheap_correct_annot[OF vsids, of n w\<^sub>n child\<^sub>n]
+        encoded_pairheap_correct_annot[OF vsids, of m w\<^sub>m child\<^sub>m]
+      by (auto intro: encoded_pairheap.intros)
+    done
+qed
+
+inductive_cases encoded_pairheapE: \<open>encoded_pairheap arr trees\<close>
+
+lemma encoded_pairheap_array_cong:
+  assumes \<open>encoded_pairheap arr trees\<close>
+    \<open>length arr = length arr'\<close>
+    \<open>\<forall>m\<in>#\<Sum>\<^sub># (mset_nodes`#trees). arr!m = arr'!m\<close>
+    \<open>\<forall>m< length arr . m\<notin>#\<Sum>\<^sub># (mset_nodes`#trees) \<longrightarrow> \<not>ph_contained (arr'!m)\<close>
+  shows \<open>encoded_pairheap arr' trees\<close>
+  using assms 
+proof (induction arbitrary: arr' rule: encoded_pairheap.induct)
+  case (empty arr)
+  then show ?case apply -
+    apply (rule  encoded_pairheap.empty)
+    apply (auto intro!: simp: )
+    by (metis image_eqI in_set_image_conv_nth)
+next
+  case (leaf arr trees n intree x arr')
+  let ?arr' = \<open>arr' [n := PHeap (ph_score (arr'!n)) False (ph_children (arr'!n))]\<close>
+  have \<open>\<forall>y\<in>#trees. \<forall>m\<in>#mset_nodes y. arr ! m = ?arr' ! m\<close> and
+    \<open>\<forall>m<length arr. m \<notin># \<Sum>\<^sub># (mset_nodes `# trees) \<longrightarrow> \<not> ph_contained (?arr' ! m)\<close>
+    using leaf apply (auto; metis; fail)
+    by (use leaf in \<open>auto split: if_splits\<close>)
+  then show ?case
+    using encoded_pairheap.leaf[of ?arr' trees n intree x] leaf
+    by (auto split: if_splits)
+next
+  case (comb arr n w\<^sub>n child\<^sub>n m w\<^sub>m child\<^sub>m trees)
+  let ?arr' = \<open>arr'[n := PHeap (ph_score (arr ! n)) (ph_contained (arr ! n)) (ph_children (arr ! n))]\<close>
+  have 1: \<open>\<forall>m\<in>#\<Sum>\<^sub># (mset_nodes `# add_mset (Hp n w\<^sub>n child\<^sub>n) (add_mset (Hp m w\<^sub>m child\<^sub>m) trees)).
+    arr ! m = ?arr' ! m\<close>
+    using comb(1,3-) encoded_pairheap_distinct_nodes[OF comb(1)]
+      encoded_pairheap_le_lengthI[OF comb(1), of m]
+      encoded_pairheap_le_lengthI[OF comb(1), of n]
+    apply auto
+    apply (meson Un_iff)
+    apply (meson Un_iff)
+    by (meson Un_iff UnionI image_eqI)
+  have 2: \<open>\<forall>ma<length arr.
+    ma \<notin># \<Sum>\<^sub># (mset_nodes `# add_mset (Hp n w\<^sub>n child\<^sub>n) (add_mset (Hp m w\<^sub>m child\<^sub>m) trees)) \<longrightarrow>
+    \<not> ph_contained (?arr' ! ma)\<close>
+    using comb(1,3-) encoded_pairheap_distinct_nodes[OF comb(1)]
+      encoded_pairheap_le_lengthI[OF comb(1), of m]
+      encoded_pairheap_le_lengthI[OF comb(1), of n]
+    by auto
+  have [simp]: \<open>(arr'
+   [n := PHeap (ph_score (arr'[n := arr ! n] ! n)) (ph_contained (arr'[n := arr ! n] ! n))
+       (ph_children (arr'[n := arr ! n] ! n) @ [m])]) = arr'\<close>
+    using comb(1,3-) encoded_pairheap_distinct_nodes[OF comb(1)]
+      encoded_pairheap_le_lengthI[OF comb(1), of m]
+      encoded_pairheap_le_lengthI[OF comb(1), of n]
+    by auto
+
+  show ?case
+    using comb.IH[of ?arr', OF _ 1 2] comb(3-5)
+    using encoded_pairheap.comb[of ?arr' n w\<^sub>n child\<^sub>n m w\<^sub>m child\<^sub>m trees]
+    by (auto split: if_splits)
+qed
+
+
+definition vsids_merge_pairs where
+  \<open>vsids_merge_pairs arr (xs :: nat list) i j = do {
+    ASSERT (i \<le> j \<and> j < length xs);
+  REC\<^sub>T (\<lambda>f (arr, j). do {
+  if i = j then RETURN (arr, xs!i)
+  else if i = j - 1 then do {
+    ASSERT (j < length xs);
+    ASSERT (j > 0);
+    vsids_link2 arr (xs!j) (xs!(j-1))
+  }
+  else do {
+    ASSERT (j < length xs);
+    ASSERT (j > 0);
+    (arr, m) \<leftarrow> vsids_link2 arr (xs!j) (xs!(j-1));
+    (arr, n) \<leftarrow> f (arr, j-2);
+    arr \<leftarrow> vsids_link arr m n;
+    RETURN (arr, n)}
+  }) (arr, j)
+  }\<close>
+
+lemma
+  assumes \<open>encoded_pairheap arr (mset hp\<^sub>s + trees)\<close>
+    \<open>map node hp\<^sub>s = Misc.slice i j xs\<close>
+    \<open>i \<le> j\<close>
+    \<open>j < length xs\<close>
+  shows \<open>vsids_merge_pairs arr xs i j \<le> \<Down> Id (SPEC (\<lambda>(arr', n). n = node (the (VSIDS.merge_pairs hp\<^sub>s)) \<and> encoded_pairheap arr' (add_mset (the (VSIDS.merge_pairs hp\<^sub>s)) trees)))\<close>
+proof -
+  define pre where
+    \<open>pre = (\<lambda>(arr, j\<^sub>0). encoded_pairheap arr (mset (take (j\<^sub>0 - i) hp\<^sub>s) + trees) \<and> j\<^sub>0 \<le> j)\<close>
+  have pre0: \<open>pre (arr, j)\<close>
+    using assms arg_cong[OF assms(2), of length, simplified, symmetric] unfolding pre_def by auto
+  show ?thesis
+    unfolding vsids_merge_pairs_def
+    apply refine_vcg
+    subgoal using assms by auto
+    subgoal using assms by auto
+    apply (rule RECT_rule[of _ \<open>measure (\<lambda>(arr, j). j)\<close> pre])
+    subgoal
+      unfolding case_prod_beta
+      by (rule refine_mono) (auto intro!: refine_mono dest: le_funD)
+    subgoal
+      by auto
+    subgoal
+      by (rule pre0)
+    subgoal for f x
+      unfolding case_prod_beta
+      apply (refine_vcg)
+      apply (auto simp: )
+        apply (rule mono)
+find_theorems "trimono _"
+find_theorems "REC\<^sub>T _ _ \<le> _"
+  
+definition vsids_del_min where
+  \<open>vsids_del_min arr m =
+    \<close>
+thm VSIDS.merge_pairs.simps
 hide_const (open) NEMonad.ASSERT NEMonad.RETURN
 
 
