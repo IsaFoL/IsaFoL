900f4d07603807c083163a908db95d38d7329159
Mesabloo <ghilain.bergeron@inria.fr>
2023-04-21 11:16:57 +0200

    splitting: add more notes for subsection 7.1

--- Splitting_Framework/Splitting_Without_Backtracking.thy
+++ Splitting_Framework/Splitting_Without_Backtracking.thy
@@ -6,13 +6,14 @@ theory Splitting_Without_Backtracking
   imports
     Main
     Splitting_Calculi
+    Saturation_Framework_Extensions.FO_Ordered_Resolution_Prover_Revisited
 begin
 
 subsection \<open>Splitting without Backtracking\<close>
 
 text \<open>
-  In this section, we show that \<open>O\<^sub>\<bbbP>\<close>, an ordered resolution with parallel selection, is closely
-  related to \<open>LA\<close>, an instance of our Splitting Framework @{locale splitting_calculus}
+  In this section, we show that \<open>O\<^sub>\<bbbP>\<close>, an ordered resolution calculus with parallel selection,
+  is closely related to \<open>LA\<close>, an instance of our Splitting Framework @{locale splitting_calculus}
   augmented with the following simplification rule:
 
   \[ \mprset{fraction={===}
@@ -34,34 +35,18 @@ text \<open>
  * I do not quite like this, but I don't quite see another way. *)
 
 text \<open>
-  We start by defining \<open>LA\<close> as an instance of our locale @{locale splitting_calculus} that we augment
-  with the simplification rule \textsc{BinSplit} (which we show to be sound and to make its premises
-  redundant, Theorem @{thm splitting_calculus_with_simps.SInf_with_simps_sound_wrt_entails_sound}
-  and Theorem @{thm splitting_calculus_with_simps.simplification_to_redundant} respectively).
+  We start by defining \<open>LA\<close> as an instance of our locale @{locale splitting_calculus}.
+  Note that we do not add the simplification rule \textsc{BinSplit} yet, as it complexifies the
+  proofs for lemma 81.
+  See locale \<open>???\<close> for the full calculus with \textsc{BinSplit}. 
 
   By Theorem @{thm splitting_calculus.S_calculus_statically_complete}, we can show that \<open>LA\<close> is
-  statically complete.
+  statically complete, and therefore dynamically complete.
 \<close>
 
-text \<open>
-  The definition of simplification rules follows what we have been doing in
-  @{locale splitting_calculus_with_simps}, i.e. $X\_simp$ indicates the simplification rule itself,
-  while $X\_pre$ contains the precondition which must hold for the simplification rule to be
-  applicable.
-\<close> 
 
 
-
-
-(* Report theorem 14 for the case BinSplit *)
-
-
-
-
-
-(* Report theorem 19 for the case BinSplit *) 
-
-
+(* Report theorem 21 and corollary 22 *)
 
 
 
@@ -107,7 +92,44 @@ text \<open>
  * This leads me to think that \<open>\<Sigma>\<^sub>\<bbbP>\<close> is actually \<^emph>\<open>not\<close> defined in @{theory Clausal_Logic} (only
  * \<open>\<Sigma>\<close>-clauses are through the datatoe @{typ clause}) and that we need to define \<open>\<Sigma>\<^sub>\<bbbP>\<close> clauses
  * ourselves.
- * This needs some more investigation, because the \<open>\<G>\<close> functions are defined on @{typ clauses}s. *)
+ * This needs some more investigation, because the \<open>\<G>\<close> functions are defined on @{typ clauses}s.
+ *
+ *
+ * 
+ * UPDATE: the more I read this section of the paper (the first few paragraphs of the subsection,
+ * until \textsc{BinSplit}), the more I'm starting to believe that we actually need to define
+ * \<open>O\<^sub>\<bbbP>\<close> ourselves, using custom clause datatype along the lines of \<open>
+     datatype ('f, 'v) \<bbbP>_clause =
+       Or \<open>'f\<close> \<open>'v clause\<close> (infix \<open>\<or>\<^sub>\<bbbP>\<close> 60)
+   \<close>, which defines what a \<open>\<Sigma>\<^sub>\<bbbP>\<close>-clause is.
+ * Hence, a \<open>\<bbbP>\<close>-clause may just be represented by the type @{typ clause}.
+ * There is a catch: how do we define \<open>O\<^sub>\<bbbP>\<close> from \<open>O = (FInf, FRed)\<close>?
+ * Moreover, we need to define the parallel selection function, which selects maximal \<bbbP>-literals
+ * in pure \<open>\<bbbP>\<close>-clauses and falls back to the original selection function.
+ * I guess what \<closedblquote>pure\<opendblquote> means in this context is that \<open>C = \<bottom>\<close> in a \<open>\<Sigma>\<^sub>\<bbbP>\<close>-clause \<open>C \<or> L\<^sub>1 \<or> \<dots> \<or> L\<^sub>n\<close>?
+ *
+ * Also note that we need that all \<open>\<bbbP>\<close>-literals be smaller than all \<open>\<Sigma>\<close>-literals.
+ * This is required for the proof of lemma 80.
+ *
+ *
+ *
+ * For all the properties on \<open>O\<^sub>\<bbbP>\<close>, I guess we will need to instantiate (or extend) the locale
+ * @{locale FO_resolution_prover}:
+ * \<^item> \<open>S\<close> is the selection function;
+ * \<^item> \<open>subst_atm\<close> is the application of a substitution to an atom;
+ * \<^item> \<open>subst_id\<close> is the empty substitution;
+ * \<^item> \<open>comp_subst\<close> is the substitution composition operator;
+ * \<^item> \<open>renamings_apart\<close> ?;
+ * \<^item> \<open>atm_of_atms\<close> ?;
+ * \<^item> \<open>mgu\<close> is a function finding the most general unifier between atoms;
+ * \<^item> \<open>less_atm\<close> is a strict partial ordering on atoms.
+ *
+ * If we are extending this locale, then we can use \<open>S\<close> as the \<closedblquote>original selection function\<closedblquote>
+ * (although we have to provide a correct mapping from \<open>\<bbbP>\<close>-clauses to clauses), and extend \<open>less_atm\<close>
+ * so that all \<open>'v\<close> atoms are smaller than any \<open>'f\<close> (\<open>\<Sigma>\<close>-)clause.
+ *
+ * Do we also need to lift the \<closedblquote>\<open>\<G>\<close> functions\<opendblquote> to \<open>\<Sigma>\<^sub>\<bbbP>\<close>-clauses? The paper says that they are defined
+ * on \<open>\<Sigma>\<close> clauses, but later in lemmas 80 and 81, they are used on \<open>\<Sigma>\<^sub>\<bbbP>\<close> clauses. *)
 
 
 
@@ -130,7 +152,10 @@ text \<open>
 
   We also define \<open>\<iota>\<alpha>(\<cdot>)\<close> on inferences such that \<open>\<iota>\<alpha>((C\<^sub>n, \<dots>, C\<^sub>1, C\<^sub>0)) \<equiv> (\<alpha>(C\<^sub>n), \<dots>, \<alpha>(C\<^sub>1), \<alpha>(C\<^sub>0))\<close>.
   As usual, we also implicitly lift \<open>\<alpha>(\<cdot>)\<close> to sets.
-\<close> 
+\<close>
+(* NOTE: to prove the bijectivity of \<open>\<alpha>(\<cdot>)\<close> we can use the predicates @{const inj}, @{const surj}
+ * and @{const bij} from the theory @{theory Fun}.
+ * @{term \<open>bij \<alpha>\<close>} should basically follow from @{term \<open>inj \<alpha>\<close>} and @{term \<open>surj \<alpha>\<close>}. *)
 
 
 
@@ -143,7 +168,7 @@ text \<open>
  * \<^url>\<open>https://www.isa-afp.org/theories/saturation_framework_extensions/#FO_Ordered_Resolution_Prover_Revisited.html#offset_5058..5061\<close>
  * (* Sorry for the 100 characters restriction, I can't shorten nor linebreak the link\<dots> :( *)
  *
- * The function of interest are \<open>\<G>_F\<close>, \<open>\<G>_Fset\<close> and \<open>\<G>_I\<close>.
+ * The function of interest are @{const \<G>_F}, @{const \<G>_Fset} and @{const \<G>_I}.
  *)
 
 
@@ -173,9 +198,13 @@ text \<open>
   Lemma 79 states that if we have some \<open>\<alpha>(\<iota>)\<close> which is a \textsc{Base} inference, and that \<open>\<iota>\<close> only
   contains \<open>\<Sigma>\<^sub>\<bbbP>\<close>-clauses, then \<open>\<iota>\<close> is actually an inference in \<open>FPInf\<close>. 
 \<close>
-(* I think that this proof may not be as simple as described in the paper, but I'll see. *)
 (* Report lemma 79 *)
 
+(* I think that this proof may not be as simple as described in the paper, but I'll see.
+ *
+ * Specifically, the fact that all \<open>C\<^sub>i\<close> (for \<open>i \<in> {1 ,\<dots>, n}\<close>) are not \<open>\<bottom>\<close> is blurry.
+ * Same goes for using the selection function with selected literals: how do we know that the
+ * function will select those exactly? . *)
 
 
 
@@ -219,7 +248,7 @@ text \<open>
   Lemma 81 shows that the notion of saturation is stronger in \<open>LA\<close>.
   If \<open>N\<close> is saturated w.r.t. \<open>O\<^sub>\<bbbP>\<close>, then \<open>\<alpha>(N)\<close> is saturated w.r.t. \<open>LA\<close>. 
 \<close>
-(* Report lemma 81 *)
+(* Report lemma 81 only for SInf (without BinSplit) *)
 
 
 
@@ -239,117 +268,48 @@ text \<open>
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-(*************************************************************)
-(*************************************************************)
-(*************************************************************)
-(* Please, do not read anything past this line. This is an ugly mess but I'm keeping it
- * only to check if I forgot anything up there. *)
-
-text \<open>
-  Currently, these will be only notes that I take while reading the section of the paper.
-  I will later reformulate most of them (if not delete half of them).
-
-
-
-  Basically, instead of using the \textsc{Split} simplification rule introduced in section 3
-  (see the locale @{locale Splitting_Calculi.splitting_calculus_with_simps}), we will be using the
-  following splitting simplification rule:
-
-  \[ \mprset{fraction={===}}
-     \inferrule{C \vee D \leftarrow A}
-               {C \leftarrow A \union \{ p \} \\ D \leftarrow A \union \{ \neg p \}}
-     \;\textsc{BinSplit}
-  \]
-
-  \<^noindent>{\centering\rule{.5\paperwidth}{0.4pt}}
-
-  We then define the \<open>LA\<close> calculus (which stands for \<closedblquote>Lightweight AVATAR\<opendblquote>):
-  Let \<open>O\<close> be \<open>(FInf, FRed)\<close> together with both \<closedblquote>normal\<opendblquote> and sound entailments.
-  \<open>LA\<close> is obtained by instantiating our locale @{locale splitting_calculus} (or
-  @{locale splitting_calculus_extensions} or @{locale splitting_calculus_with_simps})
-  and by adding the simplification rule above, with preconditions \<open>p \<in> asn(C)\<close> and
-  \<open>C \<or> D\<close> is splittable into \<open>C\<close> and \<open>D\<close>.
-
-  (* NOTE: quick problem: we require a disjunction over \<open>'f\<close> formulas, which we don't have since
-   * \<open>'f\<close> is abstract. We could add a disjunction symbol \<open>(\<or>\<^sub>F) :: 'f \<Rightarrow> 'f \<Rightarrow> 'f\<close> to the locale.
-   *
-   * Another possibility would be to fully instantiate \<open>'f\<close> with a datatype representing F-formulas,
-   * although I don't think that this is a good idea if we don't need it at all.
-   *
-   * I don't know about any other good possibility though. We'll see with Sophie, I guess. *)
-
-  (* NOTE: We are relating \<open>LA\<close> and \<open>O\<^sub>\<bbbP>\<close>, but where is \<open>O\<^sub>\<bbbP>\<close> defined?
-   * Should we define it ourselves?
-   * In this case, we'll need to go over [25] and do all the work\<dots>which may actually take some
-   * time.
-   *
-   * NOTE: see the formalization of Bachmair and Ganzinger ordered resolution \<open>O\<close> on the AFP:
-   * \<^url>\<open>https://www.isa-afp.org/entries/Ordered_Resolution_Prover.html\<close> and also maybe
-   * \<^url>\<open>https://www.isa-afp.org/entries/Saturation_Framework_Extensions.html\<close>. These may contain
-   * what we actually want here? *)
-
-  We then define a function \<open>\<alpha>(\<cdot>)\<close> which transforms a \<open>\<Sigma>\<^sub>\<bbbP>\<close>-clause into an A-clause.
-  Formally, \<open>\<alpha>(C \<or> L_1 \<or> \<dots> \<or> L_n) \<equiv> C \<leftarrow> {\<not>L_1, \<dots>, \<not>L_n}\<close>.
-
-  We also overload \<open>\<lfloor>\<cdot>\<rfloor>\<close> on \<open>\<Sigma>\<^sub>\<bbbP>\<close> so that \<open>\<lfloor>C \<or> L_1 \<or> \<dots> \<or> L_n\<rfloor> = \<lfloor>C \<leftarrow> {\<not>L_1, \<dots>, \<not>L_n}\<rfloor> = C\<close>.
-  And also have \<open>\<G>\<close> denote the grounding function.
-
-  (* NOTE: should we define \<open>\<G>\<close> or should it be abstract (as a parameter to a new locale)? *)
-\<close>
-
-
-
-text \<open>
-  Let \<open>M\<close> and \<open>N\<close> be sets of \<open>\<Sigma>\<^sub>\<bbbP>\<close>-clauses.
-  \<open>M\<close> entails \<open>N\<close> iff \<open>\<alpha>(M)\<close> entails \<open>\<alpha>(N)\<close>.
-
-  In this lemma, we show that the entailments on \<open>\<Sigma>\<^sub>\<bbbP>\<close> clauses is equivalent
-  up to \<alpha>-equivalence. 
-\<close>
-(* Report lemma 78 *)
-
-
-
-
-text \<open>
-  This lemma allows us to lift a \textsc{Base} inference \<open>\<alpha>(\<iota>)\<close> to a FP-inference \<open>\<iota>\<close>
-  (if, obviously, \<open>\<iota>\<close> is comprised only of \<open>\<Sigma>\<^sub>\<bbbP>\<close> clauses).
-\<close>
-(* Report lemma 79 *) 
-
-
-
-
-text \<open>
-  Lemma 80 is comprised of two parts:
-  \<^enum> The first part is used to show that redundancy of formulas is lifted up to \<alpha>-equivalence.
-    If \<open>\<alpha>(C) \<in> SRed\<^sub>F(\<alpha>(N))\<close> then \<open>C \<in> FPRed\<^sub>F(N)\<close>.
-  \<^enum> The second part shows that redundancy of inferences is also lifted up to \<alpha>-equivalence.
-    If \<open>\<alpha>(\<iota>) \<in> SRed\<^sub>I(\<alpha>(N))\<close> then \<open>\<iota>  \<in> FPRed\<^sub>I(N)\<close>.
-\<close>
-(* Report lemma 80 (1/2) *)
-
-(* Report lemma 80 (2/2) *)
 
 
 
 
 
 text \<open>
-  Lemma 81 shows that saturation can be lifted up to \<alpha>-equivalence.
-  Basically, if \<open>N\<close> is saturated w.r.t \<open>O\<^sub>\<bbbP>\<close> then \<open>\<alpha>(N)\<close> is saturated w.r.t \<open>LA\<close>. 
+  We now augment the earlier calculus \<open>LA\<close> with the simplification rule \textsc{BinSplit}
+  (which we show to be sound and to make its premises redundant,
+  Theorem @{thm splitting_calculus_with_simps.SInf_with_simps_sound_wrt_entails_sound}
+  and Theorem @{thm splitting_calculus_with_simps.simplification_to_redundant} respectively).
 \<close> 
-(* Report lemma 81 *) 
 
-end 
+
+
+text \<open>
+  The definition of simplification rules follows what we have been doing in
+  @{locale splitting_calculus_with_simps}, i.e. $X\_simp$ indicates the simplification rule itself,
+  while $X\_pre$ contains the precondition which must hold for the simplification rule to be
+  applicable.
+\<close> 
+
+
+
+
+(* Report theorem 14 for the case BinSplit *)
+
+
+
+
+
+(* Report theorem 19 for the case BinSplit *) 
+
+
+
+
+
+(* Report lemma 81 for the full calculus with BinSplit *)
+
+
+
+
+
+
+
+end (* theory Splitting_Without_Backtracking *)
