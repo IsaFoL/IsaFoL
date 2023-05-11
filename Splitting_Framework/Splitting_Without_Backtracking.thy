2e4035ffade37db5626d4b739f0a997c91470eca
Mesabloo <ghilain.bergeron@inria.fr>
2023-05-11 10:42:19 +0200

    splitting: finish proof that non-ground unsatisfiability is compact

--- Splitting_Framework/Splitting_Without_Backtracking.thy
+++ Splitting_Framework/Splitting_Without_Backtracking.thy
@@ -29,6 +29,96 @@ text \<open>
   that saturation w.r.t. \<open>LA\<close> implies saturation w.r.t. \<open>O\<^sub>\<bbbP>\<close>.
 \<close>
 
+(*<*)
+(*! All these should belong in the \<open>List\<close> theory, although these definitions lack a few lemmas
+ *  which aren't useful in our case.
+ *  Note that it would be better to remove calls to the \<open>smt\<close> method as these take a bit long. *)
+primrec zip3 :: \<open>'a list \<Rightarrow> 'b list \<Rightarrow> 'c list \<Rightarrow> ('a \<times> 'b \<times> 'c) list\<close> where
+  \<open>zip3 xs ys [] = []\<close>
+  | \<open>zip3 xs ys (z # zs) =
+  (case xs of [] \<Rightarrow> [] | x # xs \<Rightarrow>
+  (case ys of [] \<Rightarrow> [] | y # ys \<Rightarrow> (x, y, z) # zip3 xs ys zs))\<close>
+
+abbreviation map3 :: \<open>('a \<Rightarrow> 'b \<Rightarrow> 'c \<Rightarrow> 'd) \<Rightarrow> 'a list \<Rightarrow> 'b list \<Rightarrow> 'c list \<Rightarrow> 'd list\<close> where
+  \<open>map3 f xs ys zs \<equiv> map (\<lambda>(x, y, z). f x y z) (zip3 xs ys zs)\<close> 
+
+fun list_all3 :: \<open>('a \<Rightarrow> 'b \<Rightarrow> 'c \<Rightarrow> bool) \<Rightarrow> 'a list \<Rightarrow> 'b list \<Rightarrow> 'c list \<Rightarrow> bool\<close> where
+  \<open>list_all3 p [] [] [] = True\<close>
+| \<open>list_all3 p (x # xs) (y # ys) (z # zs) = (p x y z \<and> list_all3 p xs ys zs)\<close>
+| \<open>list_all3 _ _ _ _ = False\<close> 
+
+lemma list_all3_length_eq1: \<open>list_all3 P xs ys zs \<Longrightarrow> length xs = length ys\<close>
+  by (induction P xs ys zs rule: list_all3.induct, auto)
+
+lemma list_all3_length_eq2: \<open>list_all3 P xs ys zs \<Longrightarrow> length ys = length zs\<close>
+  by (induction P xs ys zs rule: list_all3.induct, auto) 
+
+lemma list_all3_length_eq3: \<open>list_all3 P xs ys zs \<Longrightarrow> length xs = length zs\<close>
+  using list_all3_length_eq1 list_all3_length_eq2
+  by fastforce 
+
+lemma list_all2_ex_to_ex_list_all3:
+  \<open>list_all2 (\<lambda> x y. \<exists> z. P x y z) xs ys \<longleftrightarrow> (\<exists> zs. list_all3 P xs ys zs)\<close>
+proof (intro iffI)
+  assume \<open>list_all2 (\<lambda> x y. \<exists> z. P x y z) xs ys\<close>
+  then show \<open>\<exists> zs. list_all3 P xs ys zs\<close>
+    by (induct xs ys; metis list_all3.simps(1) list_all3.simps(2))
+next
+  assume \<open>\<exists> zs. list_all3 P xs ys zs\<close>
+  then obtain zs where
+    all3_xs_ys_zs: \<open>list_all3 P xs ys zs\<close> 
+    by blast
+  then show \<open>list_all2 (\<lambda> x y. \<exists> z. P x y z) xs ys\<close>
+    using all3_xs_ys_zs
+    by (induction P xs ys zs rule: list_all3.induct, auto)
+qed 
+
+lemma list_all3_conj_distrib: \<open>list_all3 (\<lambda> x y z. P x y z \<and> Q x y z) xs ys zs \<longleftrightarrow>
+  list_all3 P xs ys zs \<and> list_all3 Q xs ys zs\<close>
+  by (induction \<open>\<lambda> x y z. P x y z \<and> Q x y z\<close> xs ys zs rule: list_all3.induct, auto)
+
+lemma list_all3_conv_list_all_3: \<open>list_all3 (\<lambda> x y z. P z) xs ys zs \<Longrightarrow> list_all P zs\<close>
+proof -
+  assume \<open>list_all3 (\<lambda> x y z. P z) xs ys zs\<close> (is \<open>list_all3 ?P xs ys zs\<close>)
+  then show \<open>list_all P zs\<close> 
+    by (induction ?P xs ys zs rule: list_all3.induct, auto)
+qed
+
+lemma list_all3_reorder:
+  \<open>list_all3 (\<lambda> x y z. P x y z) xs ys zs \<longleftrightarrow> list_all3 (\<lambda> x z y. P x y z) xs zs ys\<close>
+  by (induction \<open>\<lambda> x y z. P x y z\<close> xs ys zs rule: list_all3.induct, auto)
+
+lemma list_all3_reorder2:
+  \<open>list_all3 (\<lambda> x y z. P x y z) xs ys zs \<longleftrightarrow> list_all3 (\<lambda> y z x. P x y z) ys zs xs\<close>
+  by (induction \<open>\<lambda> x y z. P x y z\<close> xs ys zs rule: list_all3.induct, auto)
+
+lemma list_all2_conj_distrib:
+  \<open>list_all2 (\<lambda> x y. P x y \<and> Q x y) A B \<longleftrightarrow> list_all2 P A B \<and> list_all2 Q A B\<close>
+  by (smt (verit, ccfv_SIG) list_all2_conv_all_nth)
+
+lemma list_all_bex_ex_list_all2_conv:
+  \<open>list_all (\<lambda> x. \<exists> y \<in> ys. P x y) xs \<longleftrightarrow> (\<exists> ys'. set ys' \<subseteq> ys \<and> list_all2 P xs ys')\<close>
+proof (intro iffI)
+  assume \<open>list_all (\<lambda> x. \<exists> y \<in> ys. P x y) xs\<close>
+  then have \<open>list_all (\<lambda> x. \<exists> y. y \<in> ys \<and> P x y) xs\<close>
+    by meson 
+  then have \<open>\<exists> ys'. list_all2 (\<lambda> x y. y \<in> ys \<and> P x y) xs ys'\<close>
+    by (induct xs, auto)
+  then have \<open>\<exists> ys'. list_all2 (\<lambda> x y. y \<in> ys) xs ys' \<and> list_all2 (\<lambda> x y. P x y) xs ys'\<close>
+    using list_all2_conj_distrib
+    by blast
+  then have \<open>\<exists> ys'. list_all (\<lambda> y. y \<in> ys) ys' \<and> list_all2 P xs ys'\<close>
+    by (metis list_all2_conv_all_nth list_all_length) 
+  then show \<open>\<exists> ys'. set ys' \<subseteq> ys \<and> list_all2 P xs ys'\<close>
+    by (metis list.pred_set subsetI) 
+next
+  assume \<open>\<exists> ys'. set ys' \<subseteq> ys \<and> list_all2 P xs ys'\<close>
+  then show \<open>list_all (\<lambda> x. \<exists> y \<in> ys. P x y) xs\<close>
+    (* /!\ A bit slow /!\ *)
+    by (smt (verit, del_insts) list.pred_set list_all2_find_element subsetD) 
+qed 
+(*>*)
+
 text \<open>
   We start by defining \<open>LA\<close> as an instance of our locale @{locale splitting_calculus}.
 \<close>
@@ -240,10 +330,121 @@ lemma entails_\<G>_iff_unsatisfiable:
   using entails_iff_unsatisfiable
   by (smt (verit, ccfv_threshold) UNIV_I)
 
-lemma unsat_\<G>_compact: \<open>M \<TTurnstile>\<inter>\<G>e {{#}} \<Longrightarrow> \<exists> M' \<subseteq> M. finite M' \<and> M' \<TTurnstile>\<inter>\<G>e {{#}}\<close>
-   
-  sorry 
+lemma list_all3_eq_map2:
+  \<open>length xs = length ys \<Longrightarrow> length ys = length zs \<Longrightarrow>
+    list_all3 (\<lambda> x y z. z = F x y) xs ys zs \<longleftrightarrow> map2 F xs ys = zs\<close>
+proof (intro iffI)
+  assume \<open>list_all3 (\<lambda> x y z. z = F x y) xs ys zs\<close> (is \<open>list_all3 ?P xs ys zs\<close>)
+  then show \<open>map2 F xs ys = zs\<close>
+    by (induction ?P xs ys zs rule: list_all3.induct, auto)
+next
+  assume
+    \<open>length xs = length ys\<close> and
+    \<open>length ys = length zs\<close> and
+    \<open>map2 F xs ys = zs\<close>
+  then show \<open>list_all3 (\<lambda> x y z. z = F x y) xs ys zs\<close>
+  proof (induction \<open>zip xs ys\<close> arbitrary: xs ys zs) 
+    case Nil
+    then show ?case
+      by auto 
+  next
+    case (Cons a as)
 
+    obtain x y where
+      \<open>a = (x, y)\<close>
+      by fastforce
+    then obtain z zs' xs' ys' where
+      zs_eq: \<open>zs = z # zs'\<close> and
+      xs_eq: \<open>xs = x # xs'\<close> and
+      ys_eq: \<open>ys = y # ys'\<close> and
+      z_is: \<open>z = F x y\<close>
+      (* /!\ Very slow /!\ *)
+      by (smt (verit, ccfv_threshold) Cons.hyps(2) Cons.prems(2) Cons.prems(3) Pair_inject
+          list.inject map_eq_Cons_conv old.prod.case zip_eq_ConsE)
+    then have
+      \<open>as = zip xs' ys'\<close> and
+      \<open>length xs' = length ys'\<close> and
+      \<open>length ys' = length zs'\<close>
+      using Cons.prems(1, 2) Cons.hyps(2)
+      by auto 
+    then show ?case
+      using Cons.hyps Cons.prems(3) z_is zs_eq xs_eq ys_eq
+      by auto
+  qed
+qed
+
+lemma ex_finite_subset_M_if_ex_finite_subset_\<G>_F_M:
+  \<open>M\<sigma> \<subseteq> \<G>_Fset M \<Longrightarrow> finite M\<sigma> \<Longrightarrow> M\<sigma> \<TTurnstile>\<inter>e {{#}} \<Longrightarrow> \<exists> M' \<subseteq> M. finite M' \<and> \<G>_Fset M' \<TTurnstile>\<inter>e {{#}}\<close>
+proof -
+  assume
+    M\<sigma>_subset: \<open>M\<sigma> \<subseteq> \<G>_Fset M\<close> and
+    finite_M\<sigma>: \<open>finite M\<sigma>\<close> and
+    M\<sigma>_entails_bot: \<open>M\<sigma> \<TTurnstile>\<inter>e {{#}}\<close>
+
+  have \<open>M\<sigma> \<subseteq> (\<Union> C \<in> M. {C \<cdot> \<sigma> | \<sigma>. is_ground_subst \<sigma>})\<close>
+    using M\<sigma>_subset
+    unfolding \<G>_Fset_def \<G>_F_def
+    by blast      
+  then have \<open>\<forall> C \<in> M\<sigma>. \<exists> C' \<in> M. \<exists> \<sigma>. is_ground_subst \<sigma> \<and> C = C' \<cdot> \<sigma>\<close>
+    by blast
+  moreover have \<open>M\<sigma> \<noteq> {}\<close>
+    using M\<sigma>_entails_bot
+    by blast 
+  then obtain M\<sigma>' where
+    M\<sigma>'_is: \<open>set M\<sigma>' = M\<sigma>\<close> and
+    \<open>M\<sigma>' \<noteq> []\<close> 
+    using finite_M\<sigma> finite_list
+    by (meson sorted_list_of_set.set_sorted_key_list_of_set
+        sorted_list_of_set.sorted_key_list_of_set_eq_Nil_iff) 
+  ultimately have \<open>list_all (\<lambda> C. \<exists> C' \<in> M. \<exists> \<sigma>. is_ground_subst \<sigma> \<and> C = C' \<cdot> \<sigma>) M\<sigma>'\<close>
+    by (simp add: list.pred_set) 
+  then obtain Cs where
+    Cs_in_M: \<open>set Cs \<subseteq> M\<close> and
+    \<open>list_all2 (\<lambda> C C'. \<exists> \<sigma>. is_ground_subst \<sigma> \<and> C = C' \<cdot> \<sigma>) M\<sigma>' Cs\<close>
+    using list_all_bex_ex_list_all2_conv[of M _ M\<sigma>']
+    by blast 
+  then obtain \<sigma>s where
+    \<open>list_all3 (\<lambda> C C' \<sigma>. is_ground_subst \<sigma> \<and> C = C' \<cdot> \<sigma>) M\<sigma>' Cs \<sigma>s\<close>
+    using list_all2_ex_to_ex_list_all3[of _ M\<sigma>' Cs]
+    by blast 
+  then have 
+    all_grounding_in_\<sigma>s: \<open>list_all is_ground_subst \<sigma>s\<close> and
+    \<open>list_all3 (\<lambda> C C' \<sigma>. C = C' \<cdot> \<sigma>) M\<sigma>' Cs \<sigma>s\<close>
+    using list_all3_conj_distrib[of _ _ M\<sigma>' Cs \<sigma>s]
+    using list_all3_conv_list_all_3
+    by fastforce+
+  then have M\<sigma>'_eq_map2: \<open>map2 (\<cdot>) Cs \<sigma>s = M\<sigma>'\<close>
+    using list_all3_reorder2[of _ M\<sigma>' Cs \<sigma>s] list_all3_eq_map2[of Cs \<sigma>s M\<sigma>']
+          list_all3_length_eq1[of _ M\<sigma>' Cs \<sigma>s] list_all3_length_eq2[of _ M\<sigma>' Cs \<sigma>s]
+    by fastforce
+  then have \<open>set M\<sigma>' \<subseteq> \<G>_Fset (set Cs)\<close>
+    unfolding \<G>_Fset_def \<G>_F_def
+    using all_grounding_in_\<sigma>s
+    by auto (metis list.pred_set set_zip_leftD set_zip_rightD) 
+  then have \<open>\<G>_Fset (set Cs) \<TTurnstile>\<inter>e {{#}}\<close>
+    using M\<sigma>_entails_bot M\<sigma>'_is
+    by (meson true_clss_mono) 
+  then show \<open>\<exists> M' \<subseteq> M. finite M' \<and> \<G>_Fset M' \<TTurnstile>\<inter>e {{#}}\<close>
+    using Cs_in_M
+    by blast 
+qed
+
+lemma unsat_\<G>_compact: \<open>M \<TTurnstile>\<inter>\<G>e {{#}} \<Longrightarrow> \<exists> M' \<subseteq> M. finite M' \<and> M' \<TTurnstile>\<inter>\<G>e {{#}}\<close> 
+proof -
+  assume M_entails_bot: \<open>M \<TTurnstile>\<inter>\<G>e {{#}}\<close>
+  then have \<open>\<G>_Fset M \<TTurnstile>\<inter>e {{#}}\<close>
+    using F_entails_\<G>_iff grounding_of_clss_def
+    by fastforce
+  then have \<open>\<exists> M' \<subseteq> \<G>_Fset M. finite M' \<and> M' \<TTurnstile>\<inter>e {{#}}\<close>
+    using Unordered_Ground_Resolution.clausal_logic_compact
+    by auto
+  then have \<open>\<exists> M' \<subseteq> M. finite M' \<and> \<G>_Fset M' \<TTurnstile>\<inter>e {{#}}\<close>
+    by (elim exE conjE, blast intro: ex_finite_subset_M_if_ex_finite_subset_\<G>_F_M)
+  then show \<open>\<exists> M' \<subseteq> M. finite M' \<and> M' \<TTurnstile>\<inter>\<G>e {{#}}\<close>
+    using F_entails_\<G>_iff grounding_of_clss_def
+    by auto
+qed
+      
 lemma neg_of_\<G>_F_lits_is_\<G>_F_of_neg_lits:
   \<open>\<Union> {{{# -L #} | L. L \<in># D' } | D'. D' \<in> \<G>_F D} = \<Union> (\<G>_F ` {{# -L #} | L. L \<in># D})\<close>
 proof -
@@ -295,9 +496,9 @@ next
     using neg_of_\<G>_F_lits_is_\<G>_F_of_neg_lits
     by presburger
   then have \<open>\<G>_Fset M \<union> (\<Union> D' \<in> \<G>_F D. {{# -L #} | L. L \<in># D'}) \<TTurnstile>\<inter>e {{#}}\<close>
-    by auto 
+    by auto
   then have \<open>\<G>_Fset M \<TTurnstile>\<inter>e \<G>_F D\<close>
-    sorry  
+    sorry (* Is this doable? *)
   then show \<open>M \<TTurnstile>\<inter>\<G>e {D}\<close>
     using F_entails_\<G>_iff grounding_of_clss_def
     by force
@@ -313,8 +514,8 @@ lemma entails_\<G>_bot_mono: \<open>M \<subseteq> M' \<Longrightarrow> M \<TTurn
 interpretation entails_\<G>_compact: concl_compact_consequence_relation
   \<open>{{#}} :: ('a :: wellorder) clause set\<close> \<open>(\<TTurnstile>\<inter>\<G>e)\<close>
 (* This is basically the same proof as for the ground level (see file \<open>Clausal_Calculus.thy\<close>).
- * The only big difference is that instead of using \<open>satisfiable\<close>, we use an explicit entailment
- * of bottom. *)
+ * The only big difference is that instead of using @{const satisfiable}, we use an explicit
+ * entailment of bottom. *)
 proof (standard)
   fix M N :: \<open>('a :: wellorder) clause set\<close> 
 
@@ -329,7 +530,7 @@ proof (standard)
     by blast
   then obtain M'_of where
     M'_of: \<open>\<forall> D \<in> N. M'_of D \<subseteq> M \<union> {{# -L #} | L. L \<in># D} \<and> finite (M'_of D) \<and> M'_of D \<TTurnstile>\<inter>\<G>e {{#}}\<close>
-    (* try0 suggests \<open>by meson\<close> but it causes some ugly warnings that I don't understand. *)
+    (* \<open>try0\<close> suggests \<open>by meson\<close> but it causes some ugly warnings that I don't understand. *)
     by moura 
 
   define M' where
@@ -683,9 +884,11 @@ definition mk_bin_split
     let a = (SOME a. a \<in> asn (sign.Pos C)) in
     { AF.Pair C (finsert a A), AF.Pair D (finsert (neg a) A) }\<close> 
 
-(* We use the same naming convention as used in @{locale splitting_calculus_with_simps}, where
- * $X\_pre$ is the condition which must hold for the rule to be applicable, and $X\_simp$ is
- * the simplification rule itself. *)
+text \<open>
+  We use the same naming convention as used in @{locale splitting_calculus_with_simps}, where
+  $X\_pre$ is the condition which must hold for the rule to be applicable, and $X\_simp$ is
+  the simplification rule itself.
+\<close>
 
 abbreviation bin_split_pre :: \<open>'a clause \<Rightarrow> 'a clause \<Rightarrow> 'v sign fset \<Rightarrow>
   ('a clause, 'v) AF set \<Rightarrow> bool\<close> where
