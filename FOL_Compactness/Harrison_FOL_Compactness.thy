2a29b18bbba78f412ba4badb61aa8185d0cb192b
Sophie Tourret <stourret@mpi-inf.mpg.de>
2023-11-23 00:09:23 +0100

    compactness: equiv to prop compactness in progress: must prove countability of terms

--- FOL_Compactness/Harrison_FOL_Compactness.thy
+++ FOL_Compactness/Harrison_FOL_Compactness.thy
@@ -18,14 +18,13 @@ begin
 
 type_synonym nterm = \<open>(nat, nat) term\<close>
 
-
 fun functions_term :: \<open>nterm \<Rightarrow> (nat \<times> nat) set\<close> where
   \<open>functions_term (Var _) = {}\<close>
 | \<open>functions_term (Fun f ts) = {(f, length ts)} \<union> (\<Union> t \<in> set ts. functions_term t)\<close>
 
 datatype form =
   Bot (\<open>\<^bold>\<bottom>\<close>)
-| Atom nat \<open>nterm list\<close>
+| Atom (pred:nat) (args:\<open>nterm list\<close>)
 | Implies form form (infixl \<open>\<^bold>\<longrightarrow>\<close> 85)
 | Forall nat form (\<open>\<^bold>\<forall> _\<^bold>. _\<close> [0, 70] 70)
 
@@ -710,7 +709,32 @@ fun pholds :: \<open>(form \<Rightarrow> bool) \<Rightarrow> form \<Rightarrow> 
 definition psatisfiable :: "form set \<Rightarrow> bool" where
   \<open>psatisfiable S \<equiv> \<exists>I. \<forall>\<phi>\<in>S. I \<Turnstile>\<^sub>p \<phi>\<close>
 
-lemma 
+definition val_to_prop_val :: "(form \<Rightarrow> bool) \<Rightarrow> ((nat \<times> nterm list) \<Rightarrow> bool)" where
+  \<open>val_to_prop_val I = (\<lambda>x. I (Atom (fst x) (snd x)))\<close>
+
+lemma pentails_equiv: \<open>qfree \<phi> \<Longrightarrow> (I \<Turnstile>\<^sub>p \<phi> \<equiv> (val_to_prop_val I) \<Turnstile> (form_to_formula \<phi>))\<close>
+proof (induction \<phi>)
+  case Bot
+  then show ?case
+    unfolding val_to_prop_val_def by simp
+next
+  case (Atom x1 x2)
+  then show ?case
+    unfolding val_to_prop_val_def by simp
+next
+  case (Implies \<phi>1 \<phi>2)
+  then have \<open>qfree \<phi>1\<close> and \<open>qfree \<phi>2\<close> by simp+
+  then have \<open>I \<Turnstile>\<^sub>p \<phi>1 = val_to_prop_val I \<Turnstile> form_to_formula \<phi>1\<close> and
+            \<open>I \<Turnstile>\<^sub>p \<phi>2 = val_to_prop_val I \<Turnstile> form_to_formula \<phi>2\<close>
+    using Implies(1) Implies(2) by simp+
+  then show ?case by simp
+next
+  case (Forall x1 \<phi>)
+  then have False by simp
+  then show ?case by simp
+qed
+
+lemma pentails_equiv_set:
   assumes all_qfree: \<open>\<forall>\<phi>\<in>S. qfree \<phi>\<close>
   shows \<open>psatisfiable S \<equiv> sat (form_to_formula ` S)\<close>
 proof -
@@ -718,36 +742,23 @@ proof -
     assume psat_s: \<open>psatisfiable S\<close>
     then obtain I where I_is: \<open>\<forall>\<phi>\<in>S. I \<Turnstile>\<^sub>p \<phi>\<close>
       unfolding psatisfiable_def by blast
-    define \<A> where \<open>\<A> = (\<lambda>x. I (Atom (fst x) (snd x)))\<close>
-    have \<open>\<forall>\<phi>\<in>S. \<A> \<Turnstile> (form_to_formula \<phi>)\<close>
-    proof
-      fix \<phi>
-      assume phi_in: \<open>\<phi> \<in> S\<close>
-      then show \<open>\<A> \<Turnstile> (form_to_formula \<phi>)\<close>
-      proof (induction \<phi>)
-        case Bot
-        then show ?case
-          using psat_s unfolding psatisfiable_def by auto
-      next
-        case (Atom x1 x2)
-        then show ?case
-          using psat_s I_is \<A>_def unfolding psatisfiable_def
-          by auto
-      next
-        case (Implies \<phi>1 \<phi>2)
-        then show ?case sorry
-      next
-        case (Forall x1 \<phi>)
-        then show ?case sorry
-      qed
-    qed
-  have \<open>sat (form_to_formula ` S)\<close>
-    sorry
+    define \<A> where \<open>\<A> = val_to_prop_val I\<close>
+    then have \<open>\<forall>\<phi>\<in>S. \<A> \<Turnstile> (form_to_formula \<phi>)\<close>
+      using pentails_equiv all_qfree I_is by blast
+    then have \<open>sat (form_to_formula ` S)\<close>
+      unfolding sat_def by blast
   }
   moreover {
     assume \<open>sat (form_to_formula ` S)\<close>
-    have \<open>psatisfiable S\<close>
-      sorry
+    then obtain \<A> where a_is: \<open>\<forall>\<phi>\<in>S. \<A> \<Turnstile> form_to_formula \<phi>\<close>
+      by (meson image_eqI sat_def)
+    define I where i_is: \<open>I = (\<lambda>x. \<A> (pred x, args x))\<close>
+    then have \<open>\<A> = val_to_prop_val I\<close>
+      unfolding val_to_prop_val_def by simp
+    then have \<open>\<forall>\<phi>\<in>S. I \<Turnstile>\<^sub>p \<phi>\<close>
+      using pentails_equiv all_qfree a_is by blast
+    then have \<open>psatisfiable S\<close>
+      unfolding psatisfiable_def by auto
   }
   ultimately show \<open>psatisfiable S \<equiv> sat (form_to_formula ` S)\<close>
     by argo
@@ -755,6 +766,44 @@ qed
 
 definition finsat :: "form set \<Rightarrow> bool" where
   \<open>finsat S \<equiv> \<forall>T\<subseteq>S. finite T \<longrightarrow> psatisfiable T\<close>
+
+lemma finsat_fin_sat_eq:
+  assumes all_qfree: \<open>\<forall>\<phi>\<in>S. qfree \<phi>\<close>
+  shows \<open>finsat S \<equiv> fin_sat (form_to_formula ` S)\<close>
+proof -
+  {
+    assume finsat_s: \<open>finsat S\<close>
+    have \<open>fin_sat (form_to_formula ` S)\<close>
+      unfolding fin_sat_def
+    proof clarsimp
+      fix s
+      assume 
+        s_in: "s \<subseteq> form_to_formula ` S" and
+        fin_s: \<open>finite s\<close>
+      then obtain t where \<open>t \<subseteq> S\<close> and \<open>s = form_to_formula ` t\<close> and \<open>finite t\<close>
+        by (meson finite_subset_image)
+      then show "sat s"
+        using pentails_equiv_set[of t] all_qfree by (meson finsat_s finsat_def subsetD)
+      qed
+  }
+  moreover
+  {
+    assume fin_sat_s: \<open>fin_sat (form_to_formula ` S)\<close>
+    have \<open>finsat S\<close>
+      unfolding finsat_def
+    proof clarsimp
+      fix T
+      assume
+        \<open>T \<subseteq> S\<close> and
+        \<open>finite T\<close>
+      then show \<open>psatisfiable T\<close>
+        using pentails_equiv_set fin_sat_s unfolding fin_sat_def
+        by (meson assms finite_imageI image_mono subset_iff)
+    qed
+  }
+  ultimately show \<open>finsat S \<equiv> fin_sat (form_to_formula ` S)\<close>
+    by argo
+qed
 
 lemma psatisfiable_mono: \<open>psatisfiable S  \<Longrightarrow> T \<subseteq> S \<Longrightarrow> psatisfiable T\<close>
   unfolding psatisfiable_def by blast
@@ -765,11 +814,20 @@ lemma finsat_mono: \<open>finsat S \<Longrightarrow> T \<subseteq> S \<Longright
 lemma finsat_satisfiable: \<open>psatisfiable S \<Longrightarrow> finsat S\<close>
   unfolding psatisfiable_def finsat_def by blast
 
-fun qfree_to_prop :: \<open>form\<close>
-
-lemma prop_compactness: \<open>(\<forall>\<phi> \<in> S. qfree \<phi>) \<Longrightarrow> finsat S \<Longrightarrow> psatisfiable S\<close>
-  unfolding finsat_def psatisfiable_def using compactness
-  sorry
+lemma prop_compactness: \<open>(\<forall>\<phi> \<in> S. qfree \<phi>) \<Longrightarrow> finsat S = psatisfiable S\<close>
+proof -
+  assume all_qfree: \<open>\<forall>\<phi> \<in> S. qfree \<phi>\<close>
+  have \<open>finsat S = fin_sat (form_to_formula ` S)\<close>
+    using finsat_fin_sat_eq[OF all_qfree] by simp
+  also have \<open>... = sat (form_to_formula ` S)\<close>
+    (* using compactness[of "form_to_formula ` S"] *) 
+    (* must prove that the type is countable to invoque compactness *)
+    sorry
+  also have \<open>... = psatisfiable S\<close>
+    using pentails_equiv_set[OF all_qfree] by simp
+  finally show \<open>finsat S = psatisfiable S\<close>
+    . 
+qed
 
 lemma finsat_max: \<open>finsat T \<Longrightarrow> (\<exists>S. T \<subseteq> S \<and> finsat S \<and> (\<forall>P. S \<subseteq> P \<and> finsat P \<longrightarrow> (P = S)))\<close>
   unfolding finsat_def sorry
