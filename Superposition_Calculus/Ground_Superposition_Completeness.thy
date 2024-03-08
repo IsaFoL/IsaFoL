6eb5acf6aa70b5a66df48f773fecadfb896b0a22
Martin Desharnais <desharnais@mpi-inf.mpg.de>
2024-03-08 12:05:38 +0100

    Extract lemma out of sublocale

--- Superposition_Calculus/Ground_Superposition_Completeness.thy
+++ Superposition_Calculus/Ground_Superposition_Completeness.thy
@@ -1769,6 +1769,59 @@ qed
 
 subsection \<open>Static Refutational Completeness\<close>
 
+lemma (in ground_superposition_calculus) statically_complete:
+  fixes N :: "'f gatom clause set"
+  assumes "saturated N" and "G_entails N {{#}}"
+  shows "{#} \<in> N"
+  using \<open>G_entails N {{#}}\<close>
+proof (rule contrapos_pp)
+  assume "{#} \<notin> N"
+
+  define I :: "'f gterm rel" where
+    "I = (rewrite_inside_gctxt (\<Union>D \<in> N. equation N D))\<^sup>\<down>"
+
+  show "\<not> G_entails N G_Bot"
+    unfolding G_entails_def not_all not_imp
+  proof (intro exI conjI)
+    show "refl I"
+      by (simp only: I_def refl_join)
+  next
+    show "trans I"
+      unfolding I_def
+    proof (rule trans_join)
+      have "wf ((rewrite_inside_gctxt (\<Union>D \<in> N. equation N D))\<inverse>)"
+      proof (rule wf_converse_rewrite_inside_gctxt)
+        fix s t
+        assume "(s, t) \<in> (\<Union>D \<in> N. equation N D)"
+        then obtain C where "C \<in> N" "(s, t) \<in> equation N C"
+          by auto
+        thus "t \<prec>\<^sub>t s"
+          by (auto elim: mem_equationE)
+      qed auto
+      thus "SN (rewrite_inside_gctxt (\<Union>D \<in> N. equation N D))"
+        unfolding SN_iff_wf .
+    next
+      show "WCR (rewrite_inside_gctxt (\<Union>D \<in> N. equation N D))"
+        using WCR_Union_rewrite_sys .
+    qed
+  next
+    show "sym I"
+      by (simp only: I_def sym_join)
+  next
+    show "compatible_with_gctxt I"
+      unfolding I_def
+      by (simp only: I_def compatible_with_gctxt_join compatible_with_gctxt_rewrite_inside_gctxt)
+  next
+    show "(\<lambda>(x, y). Upair x y) ` I \<TTurnstile>s N"
+      unfolding I_def
+      using model_construction(2)[OF \<open>saturated N\<close> \<open>{#} \<notin> N\<close>]
+      by (simp add: true_clss_def)
+  next
+    show "\<not> (\<lambda>(x, y). Upair x y) ` I \<TTurnstile>s G_Bot"
+      by simp
+  qed
+qed
+
 sublocale ground_superposition_calculus \<subseteq> statically_complete_calculus where
   Bot = G_Bot and
   Inf = G_Inf and
@@ -1784,53 +1837,7 @@ proof unfold_locales
   assume "G_entails N {B}"
   hence "{#} \<in> N"
     unfolding \<open>B = {#}\<close>
-  proof (rule contrapos_pp)
-    assume "{#} \<notin> N"
-
-    define I :: "'f gterm rel" where
-      "I = (rewrite_inside_gctxt (\<Union>D \<in> N. equation N D))\<^sup>\<down>"
-
-    show "\<not> G_entails N G_Bot"
-      unfolding G_entails_def not_all not_imp
-    proof (intro exI conjI)
-      show "refl I"
-        by (simp only: I_def refl_join)
-    next
-      show "trans I"
-        unfolding I_def
-      proof (rule trans_join)
-        have "wf ((rewrite_inside_gctxt (\<Union>D \<in> N. equation N D))\<inverse>)"
-        proof (rule wf_converse_rewrite_inside_gctxt)
-          fix s t
-          assume "(s, t) \<in> (\<Union>D \<in> N. equation N D)"
-          then obtain C where "C \<in> N" "(s, t) \<in> equation N C"
-            by auto
-          thus "t \<prec>\<^sub>t s"
-            by (auto elim: mem_equationE)
-        qed auto
-        thus "SN (rewrite_inside_gctxt (\<Union>D \<in> N. equation N D))"
-          unfolding SN_iff_wf .
-      next
-        show "WCR (rewrite_inside_gctxt (\<Union>D \<in> N. equation N D))"
-          using WCR_Union_rewrite_sys .
-      qed
-    next
-      show "sym I"
-        by (simp only: I_def sym_join)
-    next
-      show "compatible_with_gctxt I"
-        unfolding I_def
-        by (simp only: I_def compatible_with_gctxt_join compatible_with_gctxt_rewrite_inside_gctxt)
-    next
-      show "(\<lambda>(x, y). Upair x y) ` I \<TTurnstile>s N"
-        unfolding I_def
-        using model_construction(2)[OF \<open>saturated N\<close> \<open>{#} \<notin> N\<close>]
-        by (simp add: true_clss_def)
-    next
-      show "\<not> (\<lambda>(x, y). Upair x y) ` I \<TTurnstile>s G_Bot"
-        by simp
-    qed
-  qed
+    using statically_complete[OF \<open>saturated N\<close>] by argo
   thus "\<exists>B'\<in>G_Bot. B' \<in> N"
     by auto
 qed
