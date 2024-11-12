d49b5549bba00e4180687fd9a0b4d4b044b7699c
Sophie Tourret <stourret@loria.fr>
2024-11-12 16:42:00 +0100

    compactness: statements up to compactness with sorries and commented form_to_formula and prop_of_model v1

--- FOL_Compactness/Canonical_Models.thy
+++ FOL_Compactness/Canonical_Models.thy
@@ -80,40 +80,59 @@ qed
 definition canonical :: "(nat \<times> nat) set \<times> (nat \<times> nat) set \<Rightarrow> nterm intrp \<Rightarrow> bool" where
 \<open>canonical \<L> \<M> \<equiv> (dom \<M> = terms_set (fst \<L>) \<and> (\<forall>f. intrp_fn \<M> f = Fun f))\<close>
 
-
-fun formula_to_form :: \<open>(nat\<times>nterm list) formula \<Rightarrow> form\<close> where
+(* No need to go back to formulas in fact! *)
+(* fun formula_to_form :: \<open>(nat\<times>nterm list) formula \<Rightarrow> form\<close> where
   \<open>formula_to_form (formula.Atom (p,ts)) = Atom p ts\<close>
 | \<open>formula_to_form \<bottom> = \<^bold>\<bottom>\<close>
 | \<open>formula_to_form (formula.Not \<phi>) = \<^bold>\<not> (formula_to_form \<phi>)\<close>
 | \<open>formula_to_form (formula.And \<phi> \<psi>) = (formula_to_form \<phi>) \<^bold>\<and> (formula_to_form \<psi>)\<close>
 | \<open>formula_to_form (formula.Or \<phi> \<psi>) = (formula_to_form \<phi>) \<^bold>\<or> (formula_to_form \<psi>)\<close>
 | \<open>formula_to_form (\<phi> \<^bold>\<rightarrow> \<psi>) = (formula_to_form \<phi>) \<^bold>\<longrightarrow> (formula_to_form \<psi>)\<close>
+*)
 
-(* - slight deviation from the hol-light definition where "Atom p ts" appears on the left, which is
+(* Slight deviation from the hol-light definition where "Atom p ts" appears on the left, which is
    forbidden in Isabelle, I don't see how to define it only for atoms and obtain a propositional
-   interpretation. 
-   - type translation to bridge with prop compactness result from AFP *)
-definition prop_of_model :: "'m intrp \<Rightarrow> (nat \<Rightarrow> 'm) \<Rightarrow> (nat \<times> nterm list) formula \<Rightarrow> bool" where
-  \<open>prop_of_model \<M> \<beta> \<phi> \<equiv> \<M>\<^bold>,\<beta> \<Turnstile> formula_to_form \<phi>\<close>
+   interpretation: *)
+ 
+(*    - type translation to bridge with prop compactness result from AFP *)
+(*definition prop_of_model :: "'m intrp \<Rightarrow> (nat \<Rightarrow> 'm) \<Rightarrow> (nat \<times> nterm list) formula \<Rightarrow> bool" where
+  \<open>prop_of_model \<M> \<beta> \<phi> \<equiv> \<M>\<^bold>,\<beta> \<Turnstile> formula_to_form \<phi>\<close> *)
+(* - It makes things easier to define the interpretation instead. It matches the use of the predicate
+    in hol-light better *)
+definition pintrp_of_intrp :: "'m intrp \<Rightarrow> (nat \<Rightarrow> 'm) \<Rightarrow> (form \<Rightarrow> bool)" where
+  \<open>pintrp_of_intrp \<M> \<beta> = (\<lambda>\<phi>. \<M>\<^bold>,\<beta> \<Turnstile> \<phi>)\<close>
 
 (* the predicates are not defined exactly in the same way here and in hol-light, 
    I replaced the predicate d with the set of all valid atoms and the function returns the list
    of accepted arguments for a given predicate instead of being a Boolean for compatibility.
 *)
-definition canon_of_prop :: "((nat \<times> nat) set \<times> (nat \<times> nat) set) \<Rightarrow> (form \<Rightarrow> bool) \<Rightarrow> nterm intrp" where
+definition 
+  canon_of_prop :: "((nat \<times> nat) set \<times> (nat \<times> nat) set) \<Rightarrow> (form \<Rightarrow> bool) \<Rightarrow> nterm intrp" where
   \<open>canon_of_prop \<L> I \<equiv> Abs_intrp (terms_set (fst \<L>), (\<lambda>f ts. Fun f ts), (\<lambda>p. {ts. I (Atom p ts)}))\<close>
 
+(*
 lemma formula_to_form_to_formula: \<open>qfree \<phi> \<Longrightarrow> formula_to_form (form_to_formula \<phi>) = \<phi>\<close>
   by (induction \<phi>) simp+
-
+*)
 (* PHOLDS_PROP_OF_MODEL in hol-light *)
-lemma pholds_prop_of_model: 
+(* lemma pholds_prop_of_model: 
   \<open>qfree \<phi> \<Longrightarrow> (prop_of_model \<M> \<beta>) (form_to_formula \<phi>) \<longleftrightarrow> \<M>\<^bold>,\<beta> \<Turnstile> \<phi>\<close>
   unfolding prop_of_model_def using formula_to_form_to_formula by simp
+*)
+
+lemma pholds_pintrp_of_intrp:
+  \<open>qfree \<phi> \<Longrightarrow> (pintrp_of_intrp \<M> \<beta>) \<Turnstile>\<^sub>p \<phi> \<longleftrightarrow> \<M>\<^bold>,\<beta> \<Turnstile> \<phi>\<close>
+  unfolding pintrp_of_intrp_def by (induction \<phi>) simp+
 
 (* PROP_OF_CANON_OF_PROP in hol-light *)
-lemma prop_of_canon_of_prop: 
+(* lemma prop_of_canon_of_prop:
   \<open>prop_of_model (canon_of_prop \<L> I) \<beta> (form_to_formula (Atom p ts)) = I (Atom p ts)\<close>
+  unfolding canon_of_prop_def
+  sorry
+*)
+
+lemma intrp_of_canon_of_prop:
+  \<open>pintrp_of_intrp (canon_of_prop \<L> I) \<beta> (Atom p ts) = I (Atom p ts)\<close>
   unfolding canon_of_prop_def
   sorry
 
@@ -134,5 +153,50 @@ lemma holds_canon_of_prop_general: \<open>qfree \<phi> \<Longrightarrow> (canon_
 lemma canonical_canon_of_prop: \<open>canonical \<L> (canon_of_prop \<L> I)\<close>
   unfolding canonical_def canon_of_prop_def
   by (metis dom_Abs_is_fst emptyE intrp_fn_Abs_is_fst_snd struct_def terms_set.vars)
+
+(* INTERPRETATION_CANON_OF_PROP in hol-ligh *)
+lemma interpretation_canon_of_prop: \<open>is_interpretation \<L> (canon_of_prop \<L> I)\<close>
+  unfolding is_interpretation_def canon_of_prop_def
+  by (metis (no_types, lifting) canonical_canon_of_prop canonical_def dom_Abs_is_fst 
+      intrp_fn_Abs_is_fst_snd intrp_is_struct subset_code(1) terms_set.fn)
+
+(* PROP_VALID_IMP_FOL_VALID in hol-light *)
+lemma prop_valid_imp_fol_valid: \<open>qfree \<phi> \<and> (\<forall>I. I \<Turnstile>\<^sub>p \<phi>) \<Longrightarrow> (\<forall>\<M> \<beta>. \<M>\<^bold>,\<beta> \<Turnstile> \<phi>)\<close>
+  using pholds_pintrp_of_intrp by fast
+
+(* FOL_VALID_IMP_PROP_VALID *)
+lemma fol_valid_imp_prop_valid: \<open>qfree \<phi> \<and> (\<forall>\<M> \<beta>. canonical (language {\<phi>}) \<M> \<longrightarrow> \<M>\<^bold>,\<beta> \<Turnstile> \<phi>)
+  \<Longrightarrow> \<forall>I. I \<Turnstile>\<^sub>p \<phi>\<close>
+  using canonical_canon_of_prop holds_canon_of_prop by blast
+
+(* TODO: as a definition included in Ground_FOL_Compactness, where pholds is defined? *)
+abbreviation psatisfies where \<open>psatisfies I \<Phi> \<equiv> \<forall>\<phi>\<in>\<Phi>. I \<phi>\<close>
+
+(* SATISFIES_PSATISFIES *)
+lemma satisfies_psatisfies: \<open>(\<forall>\<phi>. \<phi> \<in> \<Phi> \<longrightarrow> qfree \<phi>) \<and> satisfies \<M> \<Phi> \<and> is_valuation \<M> \<beta> \<Longrightarrow>
+  psatisfies (pintrp_of_intrp \<M> \<beta>) \<Phi>\<close>
+  using pholds_pintrp_of_intrp unfolding satisfies_def by (simp add: pintrp_of_intrp_def)
+
+find_consts name: terms
+
+(* PSATISFIES_INSTANCES in hol-light *)
+lemma psatisfies_instances: \<open>(\<forall>\<phi>. \<phi> \<in> \<Phi> \<longrightarrow> qfree \<phi>) \<and> 
+  psatisfies I {\<phi> \<cdot>\<^sub>f\<^sub>m \<beta> |\<phi> \<beta>. (\<forall>x. \<beta> x \<in> terms_set (fst \<L>)) \<and> \<phi> \<in> \<Phi>} \<Longrightarrow>
+  satisfies (canon_of_prop \<L> I) \<Phi>\<close>
+  sorry
+
+(* SATISFIES_INSTANCES in hol-light *)
+lemma satisfies_instances: \<open>(is_interpretation (language \<Xi>) \<M> \<Longrightarrow> 
+ (satisfies \<M> {\<phi> \<cdot>\<^sub>f\<^sub>m \<sigma> |\<phi> \<sigma>. \<phi> \<in> \<Phi> \<and> (\<forall>x. \<sigma> x \<in> terms_set (fst (language \<Xi>)))}) \<longleftrightarrow>
+  satisfies \<M> \<Phi>)\<close>
+  sorry
+
+(* COMPACT_CANON_QFREE in hol-light *)
+(* I don't see the point of \<open>language \<Xi>\<close> here instead of, e.g., \<L> *)
+lemma compact_canon_qfree: \<open>(\<forall>\<phi>. \<phi> \<in> \<Phi> \<longrightarrow> qfree \<phi>) \<and> 
+  (\<forall>\<Psi>. finite \<Psi> \<and> \<Psi> \<subseteq> \<Phi> \<longrightarrow> (\<exists>\<M>. is_interpretation (language \<Xi>) \<M> \<and> \<not> (dom \<M> = {}) \<and> 
+    satisfies \<M> \<Psi>)) \<Longrightarrow> \<exists>\<C>. is_interpretation (language \<Xi>) \<C> \<and> canonical (language \<Xi>) \<C> \<and>
+    satisfies \<C> \<Phi>\<close>
+  sorry
 
 end
