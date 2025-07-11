fb13f6ffb1bc0d99fc02f3b90b8deda6db327e0f
Martin Desharnais <martin.desharnais@ifi.lmu.de>
2025-07-11 17:00:47 +0200

    Refactor to model λ-terms for the optimistic λ-superposition calculus

--- Locally_Nameless_Lambda_Calculus/LN_HO_Term.thy
+++ Locally_Nameless_Lambda_Calculus/LN_HO_Term.thy
@@ -4,45 +4,43 @@ theory LN_HO_Term
     "HOL-Library.Uprod"
     "HOL-Library.Multiset"
     "HOL-Library.FSet"
-    "HOL-Library.Infinite_Typeclass"
     "HOL-ex.Sketch_and_Explore"
 begin
 
 declare foldl_inject[simp]
 
-datatype (funs_term: 'f, vars_term: 'v) "term" =
-  is_Const: Const 'f |
-  is_Free: Free 'v |
+datatype ('\<tau>, 'c, free_vars: 'f) preterm =
+  is_Const: Const 'c "'\<tau> list" "('\<tau>, 'c, 'f) preterm list" |
+  is_Free: Free 'f |
   is_Bound: Bound nat |
-  is_App: App "('f, 'v) term" "('f, 'v) term" |
-  is_Abs: Abs "('f, 'v) term"
+  is_App: App "('\<tau>, 'c, 'f) preterm" "('\<tau>, 'c, 'f) preterm" |
+  is_Abs: Abs "'\<tau>" "('\<tau>, 'c, 'f) preterm"
 
-lemma finite_funs_term: "finite (funs_term t)"
+lemma finite_vars_term: "finite (free_vars t)"
   by (induction t) simp_all
 
-lemma finite_vars_term: "finite (vars_term t)"
-  by (induction t) simp_all
-
-primrec subst_bound :: "nat \<Rightarrow> ('c, 'f) term \<Rightarrow> ('c, 'f) term \<Rightarrow> ('c, 'f) term" where
-  "subst_bound n t (Const c) = Const c"|
+primrec subst_bound :: "nat \<Rightarrow> ('\<tau>, 'c, 'f) preterm \<Rightarrow> ('\<tau>, 'c, 'f) preterm \<Rightarrow> ('\<tau>, 'c, 'f) preterm" where
+  "subst_bound n t (Const c \<tau>s ts) = Const c \<tau>s ts"|
   "subst_bound n t (Free f) = Free f" |
   "subst_bound n t (Bound k) = (if k = n then t else Bound k)" |
   "subst_bound n t (App t\<^sub>1 t\<^sub>2) = App (subst_bound n t t\<^sub>1) (subst_bound n t t\<^sub>2)" |
-  "subst_bound n t (Abs t\<^sub>1) = Abs (subst_bound (Suc n) t t\<^sub>1)"
+  "subst_bound n t (Abs \<tau> t\<^sub>1) = Abs \<tau> (subst_bound (Suc n) t t\<^sub>1)"
 
-primrec subst_free :: "'f \<Rightarrow> ('c, 'f) term \<Rightarrow> ('c, 'f) term \<Rightarrow> ('c, 'f) term" where
-  "subst_free x u (Const c) = Const c"|
+primrec subst_free
+  :: "'f \<Rightarrow> ('\<tau>, 'c, 'f) preterm \<Rightarrow> ('\<tau>, 'c, 'f) preterm \<Rightarrow> ('\<tau>, 'c, 'f) preterm" where
+  "subst_free x u (Const c \<tau>s ts) = Const c \<tau>s ts"|
   "subst_free x u (Free y) = (if x = y then u else Free y)" |
   "subst_free x u (Bound k) = Bound k" |
   "subst_free x u (App t\<^sub>1 t\<^sub>2) = App (subst_free x u t\<^sub>1) (subst_free x u t\<^sub>2)" |
-  "subst_free x u (Abs t) = Abs (subst_free x u t)"
+  "subst_free x u (Abs \<tau> t) = Abs \<tau> (subst_free x u t)"
 
-inductive locally_closed :: "('c, 'f) term \<Rightarrow> bool" where
-  Const: "locally_closed (Const c)" |
+inductive locally_closed :: "('\<tau>, 'c, 'f) preterm \<Rightarrow> bool" where
+  Const: "locally_closed (Const c \<tau>s ts)"
+    if "list_all locally_closed ts" for c \<tau>s ts |
   Free: "locally_closed (Free f)" |
   App: "locally_closed (App t\<^sub>1 t\<^sub>2)"
     if "locally_closed t\<^sub>1" and "locally_closed t\<^sub>2" |
-  Abs: "locally_closed (Abs t)"
+  Abs: "locally_closed (Abs \<tau> t)"
     if "\<And>x. x |\<notin>| \<X> \<Longrightarrow> locally_closed (subst_bound 0 (Free x) t)"
 
 definition body where
@@ -51,21 +49,21 @@ definition body where
 lemma locally_closed_App_iff: "locally_closed (App t\<^sub>1 t\<^sub>2) \<longleftrightarrow> locally_closed t\<^sub>1 \<and> locally_closed t\<^sub>2"
   by (auto elim: locally_closed.cases intro: locally_closed.App)
 
-lemma locall_closed_Abs_iff_body: "locally_closed (Abs t) \<longleftrightarrow> body t"
-  by (metis Abs LN_HO_Term.term.simps(13,19,25) body_def locally_closed.cases term.sel(6))
+lemma locall_closed_Abs_iff_body: "locally_closed (Abs \<tau> t) \<longleftrightarrow> body t"
+  unfolding locally_closed.simps[of "Abs _ _", simplified] body_def ..
 
 lemma subst_bound_subst_bound_Free_idem[simp]:
   "subst_bound n u (subst_bound n (Free x) t) = subst_bound n (Free x) t"
-  by (induction t arbitrary: n) simp_all
+  by (induction t arbitrary: n rule: preterm.induct) simp_all
 
-lemma subst_free_ident_if_not_in_vars[simp]: "x \<notin> vars_term t \<Longrightarrow> subst_free x u t = t"
+lemma subst_free_ident_if_not_in_vars[simp]: "x \<notin> free_vars t \<Longrightarrow> subst_free x u t = t"
   by (induction t) simp_all
 
 lemma subst_bound_idem_if_subst_bound_subst_bound_idem:
   "i \<noteq> j \<Longrightarrow>
   subst_bound i (Free u) (subst_bound j (Free v) t) = subst_bound j (Free v) t \<Longrightarrow>
   subst_bound i (Free u) t = t"
-proof (induction t arbitrary: i j)
+proof (induction t arbitrary: i j rule: preterm.induct)
   case (Bound x)
   then show ?case
     by auto
@@ -77,13 +75,13 @@ next
 qed simp_all
 
 lemma subst_bound_ident_if_locally_closed:
-  fixes t :: "('f, 'v) term"
-  assumes inf_vars: "infinite (UNIV :: 'v set)"
+  fixes t :: "('\<tau>, 'c, 'f) preterm"
+  assumes inf_vars: "infinite (UNIV :: 'f set)"
   assumes "locally_closed t"
   shows "subst_bound n u t = t"
   using \<open>locally_closed t\<close>
 proof (induction t arbitrary: n u rule: locally_closed.induct)
-  case (Const c)
+  case (Const c \<tau>s ts)
   then show ?case by simp
 next
   case (Free c)
@@ -93,7 +91,7 @@ next
   then show ?case by simp
 next
   case (Abs \<X> t)
-  obtain x where "x |\<notin>| \<X>"
+  obtain x :: 'f where "x |\<notin>| \<X>"
     using inf_vars
     by (metis assms(1) finite_fset ex_new_if_finite)
 
@@ -106,11 +104,11 @@ next
 qed
 
 lemma subst_free_term_subst_bound_Free_eq_subst_bound_term:
-  assumes "x \<notin> vars_term t"
+  assumes "x \<notin> free_vars t"
   shows "subst_free x u (subst_bound n (Free x) t) = subst_bound n u t"
   using assms
-proof (induction t arbitrary: n)
-  case (Const x)
+proof (induction t arbitrary: n rule: preterm.induct)
+  case (Const c \<tau>s ts)
   then show ?case
     by simp
 next
@@ -128,8 +126,8 @@ next
   then show ?case
     by simp
 next
-  case (Abs t)
-  have "x \<notin> vars_term t"
+  case (Abs \<tau> t)
+  have "x \<notin> free_vars t"
     using Abs.prems by simp
   then have "\<And>n. subst_free x u (subst_bound n (Free x) t) = subst_bound n u t"
     using Abs.IH by simp
@@ -138,12 +136,12 @@ next
 qed
 
 lemma subst_free_commutes_with_subst_bound_Free:
-  fixes t :: "('f, 'v) term"
-  assumes inf_vars: "infinite (UNIV :: 'v set)"
+  fixes t :: "('\<tau>, 'c, 'f) preterm"
+  assumes inf_vars: "infinite (UNIV :: 'f set)"
   assumes "x \<noteq> y" and "locally_closed u"
   shows "subst_free x u (subst_bound n (Free y) t) = subst_bound n (Free y) (subst_free x u t)"
-proof (induction t arbitrary: n)
-  case (Const x)
+proof (induction t arbitrary: n rule: preterm.induct)
+  case (Const c \<tau>s ts)
   then show ?case
     by simp
 next
@@ -167,16 +165,20 @@ next
 qed
 
 lemma locally_closed_subst_free:
-  fixes t u :: "('f, 'v) term"
+  fixes t u :: "('\<tau>, 'c, 'f) preterm"
   assumes
-    inf_vars: "infinite (UNIV :: 'v set)" and
+    inf_vars: "infinite (UNIV :: 'f set)" and
     "locally_closed t" and "locally_closed u"
   shows "locally_closed (subst_free x u t)"
   using \<open>locally_closed t\<close>
 proof (induction t rule: locally_closed.induct)
-  case (Const c)
-  then show ?case
-    by (simp add: locally_closed.Const)
+  case (Const c \<tau>s ts)
+  show ?case
+    unfolding subst_free.simps
+  proof (rule locally_closed.Const)
+    show "list_all locally_closed ts"
+      using list.pred_mono_strong local.Const by auto
+  qed
 next
   case (Free f)
   then show ?case
@@ -191,7 +193,7 @@ next
   show ?case
     unfolding subst_free.simps
   proof (rule locally_closed.Abs)
-    fix y
+    fix y :: 'f
     assume "y |\<notin>| finsert x \<X>"
     hence "x \<noteq> y" and "y |\<notin>| \<X>"
       by auto
@@ -203,15 +205,15 @@ next
 qed
 
 lemma body_subst_free:
-  fixes t u :: "('f, 'v) term"
+  fixes t u :: "('\<tau>, 'c, 'f) preterm"
   assumes
-    inf_vars: "infinite (UNIV :: 'v set)" and
+    inf_vars: "infinite (UNIV :: 'f set)" and
     "body t" and "locally_closed u"
   shows "body (subst_free x u t)"
 proof -
-  have "locally_closed (subst_free x u (Abs t))"
+  have "locally_closed (subst_free x u (Abs \<tau> t))" for \<tau>
   proof (intro locally_closed_subst_free[OF inf_vars])
-    show "locally_closed (Abs t)"
+    show "locally_closed (Abs \<tau> t)"
       using \<open>body t\<close> by (simp add: locall_closed_Abs_iff_body)
   next
     show "locally_closed u"
@@ -223,9 +225,9 @@ proof -
 qed
 
 lemma body_Abs_if_body:
-  fixes t :: "('f, 'v) term"
-  assumes inf_vars: "infinite (UNIV :: 'v set)"
-  shows "body t \<Longrightarrow> body (Abs t)"
+  fixes t :: "('\<tau>, 'c, 'f) preterm"
+  assumes inf_vars: "infinite (UNIV :: 'f set)"
+  shows "body t \<Longrightarrow> body (Abs \<tau> t)"
   unfolding body_def
   apply (induction t)
       apply (simp_all add: locally_closed.intros)
@@ -236,16 +238,17 @@ lemma body_Abs_if_body:
   by (metis Abs subst_bound.simps(5))
 
 lemma locally_closed_subst_bound:
-  fixes t u :: "('f, 'v) term"
-  assumes inf_vars: "infinite (UNIV :: 'v set)"
+  fixes t u :: "('\<tau>, 'c, 'f) preterm"
+  assumes inf_vars: "infinite (UNIV :: 'f set)"
   assumes "body t" and "locally_closed u"
   shows "locally_closed (subst_bound 0 u t)"
 proof -
-  obtain \<X> where lc_substb_0_t: "\<And>x. x |\<notin>| \<X> \<Longrightarrow> locally_closed (subst_bound 0 (Free x) t)"
+  obtain \<X> :: "'f fset" where
+    lc_substb_0_t: "\<And>x. x |\<notin>| \<X> \<Longrightarrow> locally_closed (subst_bound 0 (Free x) t)"
     using \<open>body t\<close>
     by (metis body_def)
 
-  obtain x where "x \<notin> vars_term t" and "x \<notin> fset \<X>"
+  obtain x :: 'f where "x \<notin> free_vars t" and "x \<notin> fset \<X>"
     by (metis Un_iff ex_new_if_finite finite_Un finite_fset finite_vars_term inf_vars)
 
   then obtain "subst_free x u (subst_bound 0 (Free x) t) = subst_bound 0 u t"
@@ -264,74 +267,86 @@ proof -
     by metis
 qed
 
-primrec shift_bound :: "nat \<Rightarrow> ('c, 'f) term \<Rightarrow> ('c, 'f) term" where
-  "shift_bound n (Const c) = Const c" |
+primrec shift_bound :: "nat \<Rightarrow> ('\<tau>, 'c, 'f) preterm \<Rightarrow> ('\<tau>, 'c, 'f) preterm" where
+  "shift_bound n (Const c \<tau>s ts) = Const c \<tau>s ts" |
   "shift_bound n (Free f) = Free f" |
   "shift_bound n (Bound k) = Bound (if k < n then k else Suc k)" |
   "shift_bound n (App t\<^sub>1 t\<^sub>2) = App (shift_bound n t\<^sub>1) (shift_bound n t\<^sub>2)" |
-  "shift_bound n (Abs t) = Abs (shift_bound (Suc n) t)"
+  "shift_bound n (Abs \<tau> t) = Abs \<tau> (shift_bound (Suc n) t)"
 
 primrec head where
-  "head (Const f) = Const f" |
+  "head (Const c \<tau>s ts) = Const c \<tau>s ts" |
   "head (Free x) = Free x" |
   "head (Bound n) = Bound n" |
   "head (App t\<^sub>1 t\<^sub>2) = head t\<^sub>1" |
-  "head (Abs t) = head t"
+  "head (Abs \<tau> t) = head t"
 
 primrec subterms where
-  "subterms (Const f) = {#Const f#}" |
+  "subterms (Const c \<tau>s ts) = {#Const c \<tau>s ts#}" |
   "subterms (Free x) = {#Free x#}" |
   "subterms (Bound n) = {#Bound n#}" |
   "subterms (App t\<^sub>1 t\<^sub>2) = add_mset (App t\<^sub>1 t\<^sub>2) (subterms t\<^sub>1 + subterms t\<^sub>2)" |
-  "subterms (Abs t) = add_mset (Abs t) (subterms t)"
+  "subterms (Abs \<tau> t) = add_mset (Abs \<tau> t) (subterms t)"
 
 fun beta_reduce where
-  "beta_reduce (App (Abs t\<^sub>1) t\<^sub>2) = subst_bound 0 t\<^sub>2 t\<^sub>1" |
+  "beta_reduce (App (Abs \<tau> t\<^sub>1) t\<^sub>2) = subst_bound 0 t\<^sub>2 t\<^sub>1" |
   "beta_reduce (App t\<^sub>1 t\<^sub>2) = (App (beta_reduce t\<^sub>1) t\<^sub>2)" |
   "beta_reduce t = t"
 
 primrec is_hnf_App where                                  
-  "is_hnf_App (Const _) \<longleftrightarrow> True" |
+  "is_hnf_App (Const _ _ _) \<longleftrightarrow> True" |
   "is_hnf_App (Free _) \<longleftrightarrow> True" |
   "is_hnf_App (Bound _) \<longleftrightarrow> True" |
   "is_hnf_App (App t _) \<longleftrightarrow> is_hnf_App t" |
-  "is_hnf_App (Abs t) \<longleftrightarrow> False"
+  "is_hnf_App (Abs _ _) \<longleftrightarrow> False"
 
 primrec is_hnf where                                  
-  "is_hnf (Const _) \<longleftrightarrow> True" |
+  "is_hnf (Const _ _ _) \<longleftrightarrow> True" |
   "is_hnf (Free _) \<longleftrightarrow> True" |
   "is_hnf (Bound _) \<longleftrightarrow> True" |
   "is_hnf (App t _) \<longleftrightarrow> is_hnf_App t" |
-  "is_hnf (Abs t) \<longleftrightarrow> is_hnf t"
+  "is_hnf (Abs _ t) \<longleftrightarrow> is_hnf t"
 
 experiment begin
 
-lemma "is_hnf (App (Const c\<^sub>1) (Const c\<^sub>2))"
+term "Const c\<^sub>1 [] []"
+
+lemma "is_hnf (App (Const c\<^sub>1 [] []) (Const c\<^sub>2 [] []))"
   by simp
 
-lemma "is_hnf (App (App (Const c\<^sub>1) (Const c\<^sub>2)) (Const c\<^sub>3))"
+lemma "is_hnf (App (App (Const c\<^sub>1 [] []) (Const c\<^sub>2 [] [])) (Const c\<^sub>3 [] []))"
   by simp
 
-lemma "is_hnf (App (Const c\<^sub>1) (App (Const c\<^sub>2) (Const c\<^sub>3)))"
+lemma "is_hnf (App (Const c\<^sub>1 [] []) (App (Const c\<^sub>2 [] []) (Const c\<^sub>3 [] [])))"
   by simp
 
-lemma "\<not> is_hnf (App (Abs (Const c\<^sub>1)) (Const c\<^sub>2))"
+lemma "\<not> is_hnf (App (Abs \<tau> (Const c\<^sub>1 [] [])) (Const c\<^sub>2 [] []))"
   by simp
 
-lemma "is_hnf (Abs (App (Const c\<^sub>1) (Bound 0)))"
+lemma "is_hnf (Abs \<tau> (App (Const c\<^sub>1 [] []) (Bound 0)))"
   by simp
 
-lemma "is_hnf (Abs (Abs (App (App (Const c\<^sub>1) (Bound 1)) (Bound 0))))"
+lemma "is_hnf (Abs \<tau>\<^sub>1 (Abs \<tau>\<^sub>2 (App (App (Const c\<^sub>1 [] []) (Bound 1)) (Bound 0))))"
   by simp
 
 end
  
 lemma "is_hnf t \<Longrightarrow> beta_reduce t = t"
 proof (induction t)
-  case (App t1 t2)
-  then show ?case
-    by (smt (verit, best) beta_reduce.simps(2,3,4,5,6,7,8) is_hnf.simps(4) is_hnf_App.simps(4,5)
-        term.rel_cases term.rel_eq)
+  case (App t\<^sub>1 t\<^sub>2)
+  have "\<not> is_Abs t\<^sub>1"
+    using \<open>is_hnf (App t\<^sub>1 t\<^sub>2)\<close>
+    by (cases t\<^sub>1) simp_all
+  then have "beta_reduce (App t\<^sub>1 t\<^sub>2) = (App (beta_reduce t\<^sub>1) t\<^sub>2)"
+    by (cases t\<^sub>1) simp_all
+  moreover have "beta_reduce t\<^sub>1 = t\<^sub>1"
+    using App.IH
+    by (metis App.prems \<open>\<not> is_Abs t\<^sub>1\<close>
+        beta_reduce.simps(7,8) is_hnf.simps(1,4)
+        is_hnf_App.simps(4) preterm.disc(25)
+        preterm.exhaust_sel)
+  ultimately show ?case
+    by metis
 qed simp_all
 
 end
--- Locally_Nameless_Lambda_Calculus/LN_HO_Typing.thy
+++ Locally_Nameless_Lambda_Calculus/LN_HO_Typing.thy
@@ -1,32 +1,50 @@
 theory LN_HO_Typing
-  imports LN_HO_Term
+  imports
+    LN_HO_Term
 begin
 
-datatype 'ty ty = Base 'ty | Fun "'ty ty" "'ty ty"
-                               
-inductive has_type :: "('f \<rightharpoonup> 'ty ty) \<Rightarrow> ('v \<rightharpoonup> 'ty ty) \<Rightarrow> ('f, 'v) term \<Rightarrow> 'ty ty \<Rightarrow> bool"
+datatype ('\<alpha>, '\<kappa>) ty =
+  TyVar '\<alpha> |
+  TyCtr '\<kappa> "('\<alpha>, '\<kappa>) ty list" |
+  TyBool |
+  TyFun "('\<alpha>, '\<kappa>) ty" "('\<alpha>, '\<kappa>) ty"
+
+type_synonym ('\<alpha>, '\<kappa>) const_ty = "'\<alpha> list \<times> ('\<alpha>, '\<kappa>) ty list \<times> ('\<alpha>, '\<kappa>) ty"
+
+inductive has_type ::
+  "('c \<rightharpoonup> ('\<alpha>, '\<kappa>) const_ty) \<Rightarrow> ('f \<rightharpoonup> ('\<alpha>, '\<kappa>) ty) \<Rightarrow>
+    (('\<alpha>, '\<kappa>) ty, 'c, 'f) preterm \<Rightarrow> ('\<alpha>, '\<kappa>) ty \<Rightarrow> bool"
   for \<C> where
-  Const: "has_type \<C> \<F> (Const c) \<tau>"
-    if "\<C> c = Some \<tau>"
-    for \<F> c \<tau> |
+  Const: "has_type \<C> \<F> (Const c \<tau>\<^sub>1s ts) \<tau>\<^sub>3"
+    if "\<C> c = Some (\<alpha>s, \<tau>\<^sub>2s, \<tau>\<^sub>3)" and "length \<tau>\<^sub>1s = length \<alpha>s" and "list_all2 (has_type \<C> \<F>) ts \<tau>\<^sub>2s"
+    for \<F> c \<tau>\<^sub>1s ts \<tau>\<^sub>3 |
   Free: "has_type \<C> \<F> (Free f) \<tau>"
     if "\<F> f = Some \<tau>"
     for \<F> f \<tau> |
   App: "has_type \<C> \<F> (App t\<^sub>1 t\<^sub>2) \<tau>\<^sub>2"
     if "has_type \<C> \<F> t\<^sub>1 (Fun \<tau>\<^sub>1 \<tau>\<^sub>2)" and "has_type \<C> \<F> t\<^sub>2 \<tau>\<^sub>1"
     for \<F> t\<^sub>1 t\<^sub>2 \<tau>\<^sub>1 \<tau>\<^sub>2 |
-  Abs: "has_type \<C> \<F> (Abs t) (Fun \<tau>\<^sub>1 \<tau>\<^sub>2)"
+  Abs: "has_type \<C> \<F> (Abs \<tau>\<^sub>1 t) (TyFun \<tau>\<^sub>1 \<tau>\<^sub>2)"
     if "\<And>x. x |\<notin>| \<X> \<Longrightarrow> has_type \<C> (\<F>(x \<mapsto> \<tau>\<^sub>1)) (subst_bound 0 (Free x) t) \<tau>\<^sub>2"
     for \<F> t \<tau>\<^sub>1 \<tau>\<^sub>2 \<X>
 
 lemma locally_closed_if_has_type[intro]: "has_type \<C> \<F> t \<tau> \<Longrightarrow> locally_closed t"
-  by (induction \<F> t \<tau> rule: has_type.induct) (auto intro: locally_closed.intros)
+proof (induction \<F> t \<tau> rule: has_type.induct)
+  case (Const \<F> c \<tau>\<^sub>1s ts \<tau>\<^sub>3 \<alpha>s \<tau>\<^sub>2s)
+  show ?case
+  proof (rule locally_closed.Const)
+    show "list_all locally_closed ts"
+      using Const.IH
+      by (simp add: list_all2_conv_all_nth list_all_length)
+  qed
+qed (auto intro: locally_closed.intros)
+
 
 lemma has_type_weaken_funenv: "has_type \<C> \<F> t \<tau>\<^sub>1 \<Longrightarrow> x \<notin> dom \<F> \<Longrightarrow> has_type \<C> (\<F>(x \<mapsto> \<tau>\<^sub>2)) t \<tau>\<^sub>1"
 proof (induction \<F> t \<tau>\<^sub>1 rule: has_type.induct)
-  case (Const \<F> c \<tau>)
+  case (Const \<F> c \<tau>\<^sub>1s ts \<tau>\<^sub>3 \<alpha>s \<tau>\<^sub>2s)
   then show ?case
-    by (auto intro: has_type.intros)
+    by (simp add: has_type.Const list_all2_mono)
 next
   case (Free \<F> f \<tau>)
   then show ?case
@@ -44,16 +62,21 @@ next
     show "has_type \<C> (\<F>(x \<mapsto> \<tau>\<^sub>2, y \<mapsto> \<tau>\<^sub>1)) (subst_bound 0 (Free y) t) \<tau>\<^sub>3"
     proof (cases "x = y")
       case True
+
       then have "\<F>(x \<mapsto> \<tau>\<^sub>2, y \<mapsto> \<tau>\<^sub>1) = \<F>(y \<mapsto> \<tau>\<^sub>1)"
         by simp
+
       then show ?thesis
         using Abs.hyps \<open>y |\<notin>| \<X>\<close> by metis
     next
       case False
+
       then have "\<F>(x \<mapsto> \<tau>\<^sub>2, y \<mapsto> \<tau>\<^sub>1) = \<F>(y \<mapsto> \<tau>\<^sub>1, x \<mapsto> \<tau>\<^sub>2)"
         by auto
+
       moreover have "x \<notin> dom (\<F>(y \<mapsto> \<tau>\<^sub>1))"
         using \<open>x \<notin> dom \<F>\<close> \<open>x \<noteq> y\<close> by simp
+
       ultimately show ?thesis
         using Abs.IH \<open>y |\<notin>| \<X>\<close> by metis
     qed
@@ -61,15 +84,15 @@ next
 qed
 
 lemma has_type_subst_free:
-  fixes t :: "('f, 'v) term"
-  assumes inf_vars: "infinite (UNIV :: 'v set)"
+  fixes t :: "(('\<alpha>, '\<kappa>) ty, 'c, 'f) preterm"
+  assumes inf_vars: "infinite (UNIV :: 'f set)"
   assumes "has_type \<C> \<F> t \<tau>\<^sub>1" and "finite (dom \<F>)" and "\<F> x = Some \<tau>\<^sub>2" and "has_type \<C> \<F> u \<tau>\<^sub>2"
   shows "has_type \<C> \<F> (subst_free x u t) \<tau>\<^sub>1"
   using assms(2-5)
 proof (induction \<F> t \<tau>\<^sub>1 rule: has_type.induct)
-  case (Const \<F> c \<tau>)
+  case (Const \<F> c \<tau>\<^sub>1s ts \<tau>\<^sub>3 \<alpha>s \<tau>\<^sub>2s)
   then show ?case
-    by (auto intro: has_type.intros)
+    by (simp add: has_type.Const list_all2_mono)
 next
   case (Free \<F> f \<tau>)
   then show ?case
