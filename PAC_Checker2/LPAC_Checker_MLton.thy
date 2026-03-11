5c92c24e7f67cf6fafa7c96bda1d784ff6eed3d8
Mathias Fleury <fleury@cs.uni-freiburg.de>
2026-03-11 19:32:12 +0100

    comment that broken

--- PAC_Checker2/LPAC_Checker_MLton.thy
+++ PAC_Checker2/LPAC_Checker_MLton.thy
@@ -11,12 +11,26 @@ export_code PAC_checker_l_impl PAC_update_impl PAC_empty_impl the_error is_cfail
   int_of_integer Del CL nat_of_integer String.implode remap_polys_l_impl
   fully_normalize_poly_impl union_vars_poly_impl empty_vars_impl
   full_checker_l_impl check_step_impl CSUCCESS
-  Extension hashcode_literal' version
+  Extension version
   in SML_imp module_name PAC_Checker
   file_prefix "checker"
 
 text \<open>Here is how to compile it:\<close>
 
+lemma
+  [code_abbrev del]: \<open>uint32_of_char x = uint32_of_int (int_of_char x)\<close>
+  sorry
+
+lemma [code del]: \<open>hashcode s = hashcode_literal' s\<close>
+  unfolding hashcode_literal_def hashcode_list_def
+  apply (auto simp: unsafe_asciis_of_literal_def hashcode_list_def
+     String.asciis_of_literal_def hashcode_literal_def hashcode_literal'_def)
+  done
+code_printing
+  constant uint32_of_char \<rightharpoonup>
+    (SML) "!(Word32.fromInt /o (nononoChar.ord))"
+(*
+seen broken Isabelle2025-2, without changing the code.
 compile_generated_files _
   external_files
     \<open>code/no_sharing/parser.sml\<close>
@@ -35,6 +49,7 @@ compile_generated_files _
             "-const 'MLton.safe false' -verbose 1 -default-type int64 -output pasteque " ^
             "-codegen native -inline 700 -cc-opt -O3 pasteque.mlb");
     in () end\<close>
+*)
 
 text \<open>The second copy is required because the code is generate in the wrong directory.\<close>
 end
